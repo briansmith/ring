5b8f104ee8304f535f1ae9ebe4c749143eb4e8c5
David Benjamin <davidben@chromium.org>
2014-08-25 23:28:44 -0400

    Revise hash management for reading the Finished message.
    Upstream originally sampled the Finished message's hash at ChangeCipherSpec,
    but our patches to add messages between the two complicated this. Move DTLS to
    this path, but use the new SSL_GET_MESSAGE_DONT_HASH_MESSAGE flag to avoid
    special-casing message types in ssl3_get_message.
    
    Change-Id: I9c8ddd9cc500c94dff2ec2f696f89d50ab01b3ad
    Reviewed-on: https://boringssl-review.googlesource.com/1632
    Reviewed-by: Adam Langley <agl@google.com>

--- ssl/s3_pkt.c
+++ ssl/s3_pkt.c
@@ -1426,8 +1426,6 @@ err:
 int ssl3_do_change_cipher_spec(SSL *s)
 	{
 	int i;
-	const char *sender;
-	int slen;
 
 	if (s->state & SSL_ST_ACCEPT)
 		i=SSL3_CHANGE_CIPHER_SERVER_READ;
@@ -1449,29 +1447,6 @@ int ssl3_do_change_cipher_spec(SSL *s)
 
 	if (!s->method->ssl3_enc->change_cipher_state(s,i))
 		return(0);
-
-	/* we have to record the message digest at
-	 * this point so we can get it before we read
-	 * the finished message */
-	if (s->state & SSL_ST_CONNECT)
-		{
-		sender=s->method->ssl3_enc->server_finished_label;
-		slen=s->method->ssl3_enc->server_finished_label_len;
-		}
-	else
-		{
-		sender=s->method->ssl3_enc->client_finished_label;
-		slen=s->method->ssl3_enc->client_finished_label_len;
-		}
-
-	i = s->method->ssl3_enc->final_finish_mac(s,
-		sender,slen,s->s3->tmp.peer_finish_md);
-	if (i == 0)
-		{
-		OPENSSL_PUT_ERROR(SSL, ssl3_do_change_cipher_spec, ERR_R_INTERNAL_ERROR);
-		return 0;
-		}
-	s->s3->tmp.peer_finish_md_len = i;
 
 	return(1);
 	}
