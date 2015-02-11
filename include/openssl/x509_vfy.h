589963f79e114256d895173d7edba9adae1978bd
Adam Langley <agl@chromium.org>
2015-02-11 12:18:56 -0800

    Multiple verifier reference identities.
    (Imported from upstream's 8abffa4a73fcbf6536e0a42d736ed9211a8204ea,
    9624b50d51de25bb2e3a72e81fe45032d80ea5c2 and
    41e3ebd5abacfdf98461cdeb6fa97a4175b7aad3.)
    
    Change-Id: Ic9099eb5704b19b4500229e89351371cc6184f9d

--- include/openssl/x509_vfy.h
+++ include/openssl/x509_vfy.h
@@ -555,6 +555,9 @@ OPENSSL_EXPORT int X509_VERIFY_PARAM_set1_policies(X509_VERIFY_PARAM *param,
 
 OPENSSL_EXPORT int X509_VERIFY_PARAM_set1_host(X509_VERIFY_PARAM *param,
 				const unsigned char *name, size_t namelen);
+OPENSSL_EXPORT int X509_VERIFY_PARAM_add1_host(X509_VERIFY_PARAM *param,
+					       const unsigned char *name,
+					       size_t namelen);
 OPENSSL_EXPORT void X509_VERIFY_PARAM_set_hostflags(X509_VERIFY_PARAM *param,
 					unsigned int flags);
 OPENSSL_EXPORT int X509_VERIFY_PARAM_set1_email(X509_VERIFY_PARAM *param,
