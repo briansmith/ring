799b5ddd577b7cc23b2edfd5019fe350a35eca26
Brian Smith <brian@briansmith.org>
2015-02-28 11:14:12 -0800

    Remove crypto/conf.

--- include/openssl/base.h
+++ include/openssl/base.h
@@ -182,8 +182,6 @@ typedef struct buf_mem_st BUF_MEM;
 typedef struct cbb_st CBB;
 typedef struct cbs_st CBS;
 typedef struct cmac_ctx_st CMAC_CTX;
-typedef struct conf_st CONF;
-typedef struct conf_value_st CONF_VALUE;
 typedef struct dh_method DH_METHOD;
 typedef struct dh_st DH;
 typedef struct ec_key_st EC_KEY;
