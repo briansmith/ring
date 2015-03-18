e5f5df9329e88329f09b4f371b4d60ce117901bd
Brian Smith <brian@briansmith.org>
2015-03-17 21:41:03 -1000

    Remove BN_clear_bit.

--- crypto/bn/shift.c
+++ crypto/bn/shift.c
@@ -242,24 +242,6 @@ int BN_set_bit(BIGNUM *a, int n) {
   return 1;
 }
 
-int BN_clear_bit(BIGNUM *a, int n) {
-  int i, j;
-
-  if (n < 0) {
-    return 0;
-  }
-
-  i = n / BN_BITS2;
-  j = n % BN_BITS2;
-  if (a->top <= i) {
-    return 0;
-  }
-
-  a->d[i] &= (~(((BN_ULONG)1) << j));
-  bn_correct_top(a);
-  return 1;
-}
-
 int BN_is_bit_set(const BIGNUM *a, int n) {
   int i, j;
 
