a1283f75f1cc4771616a6de5ce615bce78646c36
David Benjamin <davidben@chromium.org>
2015-03-26 01:46:52 -0400

    Convert err_test to C++.
    Another easy one. Doesn't actually buy us much.
    
    Change-Id: I166ae08e61c69bedea4de0a74ddd4dfc4699577d
    Reviewed-on: https://boringssl-review.googlesource.com/4129
    Reviewed-by: Adam Langley <agl@google.com>

--- crypto/err/err_test.cc
+++ crypto/err/err_test.cc
@@ -20,55 +20,52 @@
 #include <openssl/mem.h>
 
 
-static int test_overflow(void) {
-  unsigned i;
-
-  for (i = 0; i < ERR_NUM_ERRORS*2; i++) {
+static bool TestOverflow() {
+  for (unsigned i = 0; i < ERR_NUM_ERRORS*2; i++) {
     ERR_put_error(1, 2, i+1, "test", 1);
   }
 
-  for (i = 0; i < ERR_NUM_ERRORS - 1; i++) {
+  for (unsigned i = 0; i < ERR_NUM_ERRORS - 1; i++) {
     uint32_t err = ERR_get_error();
     /* Errors are returned in order they were pushed, with the least recent ones
      * removed, up to |ERR_NUM_ERRORS - 1| errors. So the errors returned are
      * |ERR_NUM_ERRORS + 2| through |ERR_NUM_ERRORS * 2|, inclusive. */
     if (err == 0 || ERR_GET_REASON(err) != i + ERR_NUM_ERRORS + 2) {
       fprintf(stderr, "ERR_get_error failed at %u\n", i);
-      return 0;
+      return false;
     }
   }
 
   if (ERR_get_error() != 0) {
     fprintf(stderr, "ERR_get_error more than the expected number of values.\n");
-    return 0;
+    return false;
   }
 
-  return 1;
+  return true;
 }
 
-static int test_put_error(void) {
-  uint32_t peeked_packed_error, packed_error;
-  int peeked_line, line, peeked_flags, flags;
-  const char *peeked_file, *file, *peeked_data, *data;
-
+static bool TestPutError() {
   if (ERR_get_error() != 0) {
     fprintf(stderr, "ERR_get_error returned value before an error was added.\n");
-    return 0;
+    return false;
   }
 
   ERR_put_error(1, 2, 3, "test", 4);
   ERR_add_error_data(1, "testing");
 
-  peeked_packed_error = ERR_peek_error_line_data(&peeked_file, &peeked_line,
-                                                 &peeked_data, &peeked_flags);
-  packed_error = ERR_get_error_line_data(&file, &line, &data, &flags);
+  int peeked_line, line, peeked_flags, flags;
+  const char *peeked_file, *file, *peeked_data, *data;
+  uint32_t peeked_packed_error =
+      ERR_peek_error_line_data(&peeked_file, &peeked_line, &peeked_data,
+                               &peeked_flags);
+  uint32_t packed_error = ERR_get_error_line_data(&file, &line, &data, &flags);
 
   if (peeked_packed_error != packed_error ||
       peeked_file != file ||
       peeked_data != data ||
       peeked_flags != flags) {
     fprintf(stderr, "Bad peeked error data returned.\n");
-    return 0;
+    return false;
   }
 
   if (strcmp(file, "test") != 0 ||
@@ -79,16 +76,16 @@ static int test_put_error(void) {
       ERR_GET_REASON(packed_error) != 3 ||
       strcmp(data, "testing") != 0) {
     fprintf(stderr, "Bad error data returned.\n");
-    return 0;
+    return false;
   }
 
-  return 1;
+  return true;
 }
 
-static int test_clear_error(void) {
+static bool TestClearError() {
   if (ERR_get_error() != 0) {
     fprintf(stderr, "ERR_get_error returned value before an error was added.\n");
-    return 0;
+    return false;
   }
 
   ERR_put_error(1, 2, 3, "test", 4);
@@ -96,42 +93,39 @@ static int test_clear_error(void) {
 
   if (ERR_get_error() != 0) {
     fprintf(stderr, "Error remained after clearing.\n");
-    return 0;
+    return false;
   }
 
-  return 1;
+  return true;
 }
 
-static int test_print(void) {
-  size_t i;
-  char buf[256];
-  uint32_t packed_error;
-
+static bool TestPrint() {
   ERR_put_error(1, 2, 3, "test", 4);
   ERR_add_error_data(1, "testing");
-  packed_error = ERR_get_error();
+  uint32_t packed_error = ERR_get_error();
 
-  for (i = 0; i <= sizeof(buf); i++) {
+  char buf[256];
+  for (size_t i = 0; i <= sizeof(buf); i++) {
     ERR_error_string_n(packed_error, buf, i);
   }
 
-  return 1;
+  return true;
 }
 
-static int test_release(void) {
+static bool TestRelease() {
   ERR_put_error(1, 2, 3, "test", 4);
   ERR_remove_thread_state(NULL);
-  return 1;
+  return true;
 }
 
-int main(void) {
+int main() {
   CRYPTO_library_init();
 
-  if (!test_overflow() ||
-      !test_put_error() ||
-      !test_clear_error() ||
-      !test_print() ||
-      !test_release()) {
+  if (!TestOverflow() ||
+      !TestPutError() ||
+      !TestClearError() ||
+      !TestPrint() ||
+      !TestRelease()) {
     return 1;
   }
 
