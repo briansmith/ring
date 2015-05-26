ff81e10313fd77f906e09516022c2b25a9c8587c
David Benjamin <davidben@chromium.org>
2015-05-26 17:46:47 -0400

    Add OPENSSL_PUT_ERROR line to X509V3_parse_list.
    See upstream's 344c271eb339fc2982e9a3584a94e51112d84584. We had the error check
    already. But, for consistency with the rest of that function's error paths,
    pushing an error on the error queue would be prudent.
    
    Change-Id: I8b702abc679dc94dffa79c19a9b7c3d0adc0638b
    Reviewed-on: https://boringssl-review.googlesource.com/4889
    Reviewed-by: Adam Langley <agl@google.com>

--- crypto/x509v3/v3_utl.c
+++ crypto/x509v3/v3_utl.c
@@ -263,7 +263,10 @@ STACK_OF(CONF_VALUE) *X509V3_parse_list(const char *line)
 	/* We are going to modify the line so copy it first */
 	linebuf = BUF_strdup(line);
 	if (linebuf == NULL)
+		{
+		OPENSSL_PUT_ERROR(X509V3, X509V3_parse_list, ERR_R_MALLOC_FAILURE);
 		goto err;
+		}
 	state = HDR_NAME;
 	ntmp = NULL;
 	/* Go through all characters */
