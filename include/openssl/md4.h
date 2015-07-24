820731a2b0645a0696bbc61ccd7d4fc29d9be3bb
David Benjamin <davidben@chromium.org>
2015-07-23 20:01:51 -0400

    Fix some typos in license headers.
    These are not in upstream and were probably introduced on accident by stray vim
    keystrokes.
    
    Change-Id: I35f51f81fc37e75702e7d8ffc6f040ce71321b54
    Reviewed-on: https://boringssl-review.googlesource.com/5490
    Reviewed-by: Adam Langley <agl@google.com>

--- include/openssl/md4.h
+++ include/openssl/md4.h
@@ -1,4 +1,5 @@
-/* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com) * All rights reserved.
+/* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
+ * All rights reserved.
  *
  * This package is an SSL implementation written
  * by Eric Young (eay@cryptsoft.com).
