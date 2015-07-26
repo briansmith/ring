72e2935ff24c855581fab9ed81a0d7c30e481cdb
Brian Smith <brian@briansmith.org>
2015-07-26 15:49:48 -0400

    Create a 1-to-1 mapping of perlasm scripts to outputs.
    Previously, sha-x86_64.pl (was sha512-x86_64.pl) was used to generate
    both sha256-x86_64.S and sha512-x86_64.S. This required special build
    logic in ring.mk and also would have required special logic in the
    msbuild project files. This commit adds dedicated perlasm source files
    for both, and there's now a 1-to-1 correspondence between perlasm
    scripts and output files, which avoids any need for special-case logic
    in the build system.
    
    The same goes for sha-armv8.pl (was sha512-armv8.pl).

---
+++ crypto/sha/asm/sha256-armv8.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+#
+# ====================================================================
+# Written by Andy Polyakov <appro@openssl.org> for the OpenSSL
+# project. Rights for redistribution and usage in source and binary
+# forms are granted according to the OpenSSL license.
+# ====================================================================
+
+push @ARGV, 'sha256';
+
+$0 =~ m/(.*[\/\\])[^\/\\]+$/; $dir=$1;
+push(@INC,"${dir}");
+
+do "sha-x86_64.pl" or die "failed to run sha-armv8.pl: $@";
