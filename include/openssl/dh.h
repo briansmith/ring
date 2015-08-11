5f4de9aa8f55f2fd29ff8e1e4b3b85f410390725
David Benjamin <davidben@chromium.org>
2015-08-11 18:57:49 -0400

    Fix pub_key comment in dh.h.
    (Imported from upstream's 602af7d01da82dc6c45946572f531bcbe9ebbf47.)
    
    Change-Id: I17cc0bfaf2d9954aafae9da30d2d37a97101f330
    Reviewed-on: https://boringssl-review.googlesource.com/5691
    Reviewed-by: Adam Langley <agl@google.com>

--- include/openssl/dh.h
+++ include/openssl/dh.h
@@ -235,7 +235,7 @@ struct dh_st {
 
   BIGNUM *p;
   BIGNUM *g;
-  BIGNUM *pub_key;  /* g^x */
+  BIGNUM *pub_key;  /* g^x mod p */
   BIGNUM *priv_key; /* x */
 
   /* priv_length contains the length, in bits, of the private value. If zero,
