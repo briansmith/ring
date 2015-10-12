0d8a8661a67bfa7024ec4926c14df7750167db5c
Brian Smith <brian@briansmith.org>
2015-10-11 20:22:27 -1000

    Silence MSVC warning C4210.
    The warning is:
    
        C4210: nonstandard extension used : function given file scope.
    
    It is caused by function declarations that aren't at the top level in a
    file.

--- crypto/bn/exponentiation.c
+++ crypto/bn/exponentiation.c
@@ -123,6 +123,19 @@
 #define RSAZ_ENABLED
 
 #include "rsaz_exp.h"
+
+void bn_mul_mont_gather5(BN_ULONG * rp, const BN_ULONG * ap,
+                         const void * table, const BN_ULONG * np,
+                         const BN_ULONG * n0, int num, int power);
+void bn_scatter5(const BN_ULONG * inp, size_t num, void * table,
+                 size_t power);
+void bn_gather5(BN_ULONG * out, size_t num, void * table, size_t power);
+void bn_power5(BN_ULONG * rp, const BN_ULONG * ap, const void * table,
+               const BN_ULONG * np, const BN_ULONG * n0, int num,
+               int power);
+int bn_from_montgomery(BN_ULONG * rp, const BN_ULONG * ap,
+                       const BN_ULONG * not_used, const BN_ULONG * np,
+                       const BN_ULONG * n0, int num);
 #endif
 
 int BN_exp(BIGNUM *r, const BIGNUM *a, const BIGNUM *p, BN_CTX *ctx) {
@@ -984,19 +997,6 @@ int BN_mod_exp_mont_consttime(BIGNUM *rr, const BIGNUM *a, const BIGNUM *p,
   /* Dedicated window==4 case improves 512-bit RSA sign by ~15%, but as
    * 512-bit RSA is hardly relevant, we omit it to spare size... */
   if (window == 5 && top > 1) {
-    void bn_mul_mont_gather5(BN_ULONG * rp, const BN_ULONG * ap,
-                             const void * table, const BN_ULONG * np,
-                             const BN_ULONG * n0, int num, int power);
-    void bn_scatter5(const BN_ULONG * inp, size_t num, void * table,
-                     size_t power);
-    void bn_gather5(BN_ULONG * out, size_t num, void * table, size_t power);
-    void bn_power5(BN_ULONG * rp, const BN_ULONG * ap, const void * table,
-                   const BN_ULONG * np, const BN_ULONG * n0, int num,
-                   int power);
-    int bn_from_montgomery(BN_ULONG * rp, const BN_ULONG * ap,
-                           const BN_ULONG * not_used, const BN_ULONG * np,
-                           const BN_ULONG * n0, int num);
-
     BN_ULONG *np = mont->N.d, *n0 = mont->n0, *np2;
 
     /* BN_to_montgomery can contaminate words above .top
