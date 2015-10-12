e825b0b48ca8f3165215eb1463fd2902cc5b3342
Brian Smith <brian@briansmith.org>
2015-10-12 13:12:38 -1000

    Fix documentation for |ring::pbkdf2| and add an example.

--- src/pbkdf2.rs
+++ src/pbkdf2.rs
@@ -22,11 +22,92 @@
 //! with test vectors given in [RFC 6070](https://tools.ietf.org/html/rfc6070).
 //! See also [NIST Special Publication
 //! 800-132](http://csrc.nist.gov/publications/nistpubs/800-132/nist-sp800-132.pdf).
+//!
+//! # Examples
+//!
+//! ## Password Database Example
+//!
+//! ```
+//! use ring::pbkdf2;
+//! use std::collections::HashMap;
+//!
+//! static PBKDF2_PRF: &'static pbkdf2::PRF = &pbkdf2::HMAC_SHA256;
+//! const CREDENTIAL_LEN: usize = 32; // digest::SHA256.digest_len()
+//! pub type Credential = [u8; CREDENTIAL_LEN];
+//!
+//! struct PasswordDatabase {
+//!     pbkdf2_iterations: usize,
+//!     db_salt_component: [u8; 16],
+//!
+//!     // Normally this would be a persistent database.
+//!     storage: HashMap<String, Credential>,
+//! }
+//!
+//! impl PasswordDatabase {
+//!     pub fn store_password(&mut self, username: &str, password: &str) {
+//!         let salt = self.salt(username);
+//!         let mut to_store: Credential = [0u8; CREDENTIAL_LEN];
+//!         pbkdf2::derive(PBKDF2_PRF, self.pbkdf2_iterations, &salt,
+//!                        password.as_bytes(), &mut to_store);
+//!         self.storage.insert(String::from(username), to_store);
+//!     }
+//!
+//!     pub fn verify_password(&self, username: &str, attempted_password: &str)
+//!                            -> Result<(), ()> {
+//!         match self.storage.get(username) {
+//!            Some(actual_password) => {
+//!                let salt = self.salt(username);
+//!                pbkdf2::verify(PBKDF2_PRF, self.pbkdf2_iterations, &salt,
+//!                               attempted_password.as_bytes(),
+//!                               actual_password)
+//!            },
+//!
+//!            None => Err(())
+//!         }
+//!     }
+//!
+//!     // The salt should have a user-specific component so that an attacker
+//!     // cannot crack one password for multiple users in the database. It
+//!     // should have a database-unique component so that an attacker cannot
+//!     // crack the same user's password across databases in the unfortunate
+//!     // but common case that the user has used the same password for
+//!     // multiple systems.
+//!     fn salt(&self, username: &str) -> Vec<u8> {
+//!         let mut salt = Vec::with_capacity(self.db_salt_component.len() +
+//!                                           username.as_bytes().len());
+//!         salt.extend(self.db_salt_component.as_ref());
+//!         salt.extend(username.as_bytes());
+//!         salt
+//!     }
+//! }
+//!
+//! fn main() {
+//!     // Normally these parameters would be loaded from a configuration file.
+//!     let mut db = PasswordDatabase {
+//!         pbkdf2_iterations: 100_000,
+//!         db_salt_component: [
+//!             // This value was generated from a secure PRNG.
+//!             0xd6, 0x26, 0x98, 0xda, 0xf4, 0xdc, 0x50, 0x52,
+//!             0x24, 0xf2, 0x27, 0xd1, 0xfe, 0x39, 0x01, 0x8a
+//!         ],
+//!         storage: HashMap::new(),
+//!     };
+//!
+//!     db.store_password("alice", "@74d7]404j|W}6u");
+//!
+//!     // An attempt to log in with the wrong password fails.
+//!     assert!(db.verify_password("alice", "wrong password").is_err());
+//!
+//!     // Normally there should be an expoentially-increasing delay between
+//!     // attempts to further protect against online attacks.
+//!
+//!     // An attempt to log in with the right password succeeds.
+//!     assert!(db.verify_password("alice", "@74d7]404j|W}6u").is_ok());
+//! }
 
 use super::{digest, ffi, hmac};
 
-/// Fills `out` with the key derived using PBKDF2 with the given inputs,
-/// using HMAC with the given digest algorithm as the PRF.
+/// Fills `out` with the key derived using PBKDF2 with the given inputs.
 ///
 /// Do not use `derive` as part of verifying a secret; use `verify` instead, to
 /// minimize the effectiveness of timing attacks.
@@ -40,9 +121,9 @@ use super::{digest, ffi, hmac};
 /// | Parameter   | RFC 2898 Section 5.2 Term
 /// |-------------|---------------------------------------
 /// | prf         | PRF
-/// | secret      | P (password)
-/// | salt        | S (salt)
 /// | iterations  | c (iteration count)
+/// | salt        | S (salt)
+/// | secret      | P (password)
 /// | out         | dk (derived key)
 /// | out.len()   | dkLen (derived key length)
 ///
@@ -54,7 +135,7 @@ use super::{digest, ffi, hmac};
 ///
 /// `derive` panics if `out.len()` is larger than the output length of the
 /// digest function used by the PRF algorithm.
-pub fn derive(prf: &'static PRF, iterations: usize, secret: &[u8], salt: &[u8],
+pub fn derive(prf: &'static PRF, iterations: usize, salt: &[u8], secret: &[u8],
               out: &mut [u8]) {
     assert!(iterations >= 1);
     assert!(out.len() <= prf.digest_alg.digest_len);
@@ -99,9 +180,9 @@ pub fn derive(prf: &'static PRF, iterations: usize, secret: &[u8], salt: &[u8],
 /// | Parameter                | RFC 2898 Section 5.2 Term
 /// |--------------------------|---------------------------------------
 /// | prf                      | PRF
-/// | secret                   | P (password)
-/// | salt                     | S (salt)
 /// | iterations               | c (iteration count)
+/// | salt                     | S (salt)
+/// | secret                   | P (password)
 /// | previously_derived       | dk (derived key)
 /// | previously_derived.len() | dkLen (derived key length)
 ///
@@ -113,14 +194,14 @@ pub fn derive(prf: &'static PRF, iterations: usize, secret: &[u8], salt: &[u8],
 ///
 /// `verify` panics if `out.len()` is larger than the output length of the
 /// digest function used by the PRF algorithm.
-pub fn verify(prf: &'static PRF, iterations: usize, secret: &[u8], salt: &[u8],
+pub fn verify(prf: &'static PRF, iterations: usize, salt: &[u8], secret: &[u8],
               previously_derived: &[u8]) -> Result<(), ()> {
     let mut derived_buf = [0u8; digest::MAX_DIGEST_LEN];
     if previously_derived.len() > derived_buf.len() {
         return Err(());
     }
     let derived = &mut derived_buf[0..previously_derived.len()];
-    derive(prf, iterations, secret, salt, derived);
+    derive(prf, iterations, salt, secret, derived);
     ffi::verify_slices_are_equal_ct(derived, previously_derived)
 }
 
@@ -174,9 +255,10 @@ mod tests {
             };
 
             let mut out = vec![0u8; dk.len()];
-            pbkdf2::derive(prf, iterations, &secret, &salt, &mut out);
+            pbkdf2::derive(prf, iterations, &salt, &secret, &mut out);
             assert_eq!(dk, out);
-            assert!(pbkdf2::verify(prf, iterations, &secret, &salt,&out).is_ok());
+            assert!(pbkdf2::verify(prf, iterations, &salt, &secret, &out)
+                        .is_ok());
         }
 
         file_test::run("src/pbkdf2_tests.txt", test_case);
