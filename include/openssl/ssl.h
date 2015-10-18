449c3c7b7b09ed28dbb3986ca54414d102e0ba3f
David Benjamin <davidben@chromium.org>
2015-10-17 23:21:35 -0400

    Take some definitions out of the Android compatibility layer.
    They were since added to crypto.h and implemented in the library proper.
    
    Change-Id: Idaa2fe2d9b213e67cf7ef61ff8bfc636dfa1ef1f
    Reviewed-on: https://boringssl-review.googlesource.com/6309
    Reviewed-by: Adam Langley <alangley@gmail.com>

--- include/openssl/ssl.h
+++ include/openssl/ssl.h
@@ -4104,14 +4104,6 @@ OPENSSL_EXPORT int SSL_set_session_secret_cb(SSL *s, void *cb, void *arg);
 OPENSSL_EXPORT int SSL_set_session_ticket_ext_cb(SSL *s, void *cb, void *arg);
 OPENSSL_EXPORT int SSL_set_ssl_method(SSL *s, const SSL_METHOD *method);
 
-#define OPENSSL_VERSION_TEXT "BoringSSL"
-
-#define SSLEAY_VERSION 0
-
-/* SSLeay_version is a compatibility function that returns the string
- * "BoringSSL". */
-OPENSSL_EXPORT const char *SSLeay_version(int unused);
-
 
 /* Preprocessor compatibility section.
  *
