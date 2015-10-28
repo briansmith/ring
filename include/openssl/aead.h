7dd3f77256083a38102657be318dd4901e34f34e
David Benjamin <davidben@chromium.org>
2015-10-28 13:47:39 -0400

    Fix aead.h header typo.
    EVP_aead_chacha20_poly1305_old is listed twice instead of
    EVP_aead_chacha20_poly1305.
    
    Change-Id: I281eee7a8359cd2a2b04047c829ef351ea4a7b82
    Reviewed-on: https://boringssl-review.googlesource.com/6381
    Reviewed-by: Adam Langley <agl@google.com>

--- include/openssl/aead.h
+++ include/openssl/aead.h
@@ -106,7 +106,7 @@ OPENSSL_EXPORT const EVP_AEAD *EVP_aead_chacha20_poly1305_old(void);
 /* EVP_aead_chacha20_poly1305 is currently an alias for
  * |EVP_aead_chacha20_poly1305_old|. In the future, the RFC 7539 version will
  * take this name. */
-OPENSSL_EXPORT const EVP_AEAD *EVP_aead_chacha20_poly1305_old(void);
+OPENSSL_EXPORT const EVP_AEAD *EVP_aead_chacha20_poly1305(void);
 
 /* EVP_aead_chacha20_poly1305_rfc7539 is the AEAD built from ChaCha20 and
  * Poly1305 as described in RFC 7539.
