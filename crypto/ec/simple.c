7308aaa9b47012428c282b6399ba67a0f26d0aee
Brian Smith <brian@briansmith.org>
2015-11-08 10:39:51 -1000

    Remove `EC_GFp_simple_method` (dead code).
    Change-Id: I1820bd5412313e00a69123370178c0fe3e12b5ef
    Reviewed-on: https://boringssl-review.googlesource.com/6482
    Reviewed-by: Adam Langley <agl@google.com>

--- crypto/ec/simple.c
+++ crypto/ec/simple.c
@@ -76,25 +76,6 @@
 #include "internal.h"
 
 
-const EC_METHOD *EC_GFp_simple_method(void) {
-  static const EC_METHOD ret = {ec_GFp_simple_group_init,
-                                ec_GFp_simple_group_finish,
-                                ec_GFp_simple_group_clear_finish,
-                                ec_GFp_simple_group_copy,
-                                ec_GFp_simple_group_set_curve,
-                                ec_GFp_simple_point_get_affine_coordinates,
-                                ec_wNAF_mul /* XXX: Not constant time. */,
-                                ec_wNAF_precompute_mult,
-                                ec_GFp_simple_field_mul,
-                                ec_GFp_simple_field_sqr,
-                                0 /* field_encode */,
-                                0 /* field_decode */,
-                                0 /* field_set_to_one */};
-
-  return &ret;
-}
-
-
 /* Most method functions in this file are designed to work with non-trivial
  * representations of field elements if necessary (see ecp_mont.c): while
  * standard modular addition and subtraction are used, the field_mul and
