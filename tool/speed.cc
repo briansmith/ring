df571631ccac1ef7dc5d93023bc08d0f83a2c933
David Benjamin <davidben@chromium.org>
2015-12-07 19:48:16 -0500

    Add RC4-SHA1 and DES-EDE3-CBC-SHA1 to bssl speed.
    For completeness. In so far as we care about legacy ciphers' performance at
    all, we should have the others too.
    
    Change-Id: Idd2d93345f3af8b6ac5772a1cb3c201f84fe3197
    Reviewed-on: https://boringssl-review.googlesource.com/6750
    Reviewed-by: Adam Langley <agl@google.com>

--- tool/speed.cc
+++ tool/speed.cc
@@ -535,6 +535,9 @@ bool Speed(const std::vector<std::string> &args) {
       !SpeedAEAD(EVP_aead_chacha20_poly1305_old(), "ChaCha20-Poly1305-Old",
                  kTLSADLen, selected) ||
       !SpeedAEAD(EVP_aead_rc4_md5_tls(), "RC4-MD5", kLegacyADLen, selected) ||
+      !SpeedAEAD(EVP_aead_rc4_sha1_tls(), "RC4-SHA1", kLegacyADLen, selected) ||
+      !SpeedAEAD(EVP_aead_des_ede3_cbc_sha1_tls(), "DES-EDE3-CBC-SHA1",
+                 kLegacyADLen, selected) ||
       !SpeedAEAD(EVP_aead_aes_128_cbc_sha1_tls(), "AES-128-CBC-SHA1",
                  kLegacyADLen, selected) ||
       !SpeedAEAD(EVP_aead_aes_256_cbc_sha1_tls(), "AES-256-CBC-SHA1",
