e021a245bf2d6ab692220b14f680d597d078f149
Matt Braithwaite <mab@google.com>
2016-01-14 13:41:46 -0800

    Add curve25519/asm/x25519-asm-x86_64.S.
    Change-Id: I5feff96d8d80981e72a8b3aa3fd90e3202dff39e
    Reviewed-on: https://boringssl-review.googlesource.com/6903
    Reviewed-by: Adam Langley <agl@google.com>

--- util/generate_build_files.py
+++ util/generate_build_files.py
@@ -44,6 +44,9 @@ NON_PERL_FILES = {
         'src/crypto/curve25519/asm/x25519-asm-arm.S',
         'src/crypto/poly1305/poly1305_arm_asm.S',
     ],
+    ('linux', 'x86_64'): [
+        'src/crypto/curve25519/asm/x25519-asm-x86_64.S',
+    ],
 }
 
 
