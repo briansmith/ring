0be04e8f6ceaed41224cec79e062dce8b7c10fad
Corey Farwell <coreyf@rwell.org>
2016-01-18 18:44:30 -0500

    Rephrase from_be_u32 utility function comment

--- crypto/internal.h
+++ crypto/internal.h
@@ -488,8 +488,7 @@ extern void SHA512_5(uint8_t *out, size_t out_len,
 #define SHA512_DIGEST_LENGTH 64
 
 
-/* Given a pointer to 32-bit big-endian data, return (by value) the data with
- * the same byte order as the host CPU */
+/* from_be_u32 returns the 32-bit big-endian-encoded value at |data|. */
 static inline uint32_t from_be_u32(const uint8_t *data) {
   return (data[3] << 0) | (data[2] << 8) | (data[1] << 16) | (data[0] << 24);
 }
