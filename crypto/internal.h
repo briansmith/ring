8b2e71740c175d1133e043b6f218cf204ebb4644
Corey Farwell <coreyf@rwell.org>
2016-01-21 17:25:18 -0500

    not dealing pointers -> don't check STRICT_ALIGNMENT

--- crypto/internal.h
+++ crypto/internal.h
@@ -502,10 +502,9 @@ static inline uint32_t from_be_u32_ptr(const uint8_t *data) {
 /* from_be_u64 returns the native representation of the 64-bit
  * big-endian-encoded value |data|. */
 static inline uint64_t from_be_u64(const uint64_t data) {
-#if STRICT_ALIGNMENT == 0 && OPENSSL_ENDIAN == OPENSSL_LITTLE_ENDIAN && \
-    defined(bswap_u64)
+#if OPENSSL_ENDIAN == OPENSSL_LITTLE_ENDIAN && defined(bswap_u64)
   return bswap_u64(data);
-#elif STRICT_ALIGNMENT == 0 && OPENSSL_ENDIAN == OPENSSL_BIG_ENDIAN
+#elif OPENSSL_ENDIAN == OPENSSL_BIG_ENDIAN
   return data;
 #else
   return (data[0] << 56) |
