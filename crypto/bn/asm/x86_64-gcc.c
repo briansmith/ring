b724a00d5f95f91c52d2454725b88323c81c5a9f
Corey Farwell <coreyf@rwell.org>
2016-01-22 08:44:52 -0500

    Remove '#define asm __asm__' everywhere
    Closes https://github.com/briansmith/ring/issues/69
    
    I agree to license my contributions to each file under the same terms
    given at the top of each file I changed.

--- crypto/bn/asm/x86_64-gcc.c
+++ crypto/bn/asm/x86_64-gcc.c
@@ -61,7 +61,6 @@
 #undef mul
 #undef mul_add
 
-#define asm __asm__
 
 /*
  * "m"(a), "+m"(r)	is the way to favor DirectPath µ-code;
@@ -71,12 +70,12 @@
 #define mul_add(r, a, word, carry)                                     \
   do {                                                                 \
     register BN_ULONG high, low;                                       \
-    asm("mulq %3" : "=a"(low), "=d"(high) : "a"(word), "m"(a) : "cc"); \
-    asm("addq %2,%0; adcq %3,%1"                                       \
+    __asm__("mulq %3" : "=a"(low), "=d"(high) : "a"(word), "m"(a) : "cc"); \
+    __asm__("addq %2,%0; adcq %3,%1"                                       \
         : "+r"(carry), "+d"(high)                                      \
         : "a"(low), "g"(0)                                             \
         : "cc");                                                       \
-    asm("addq %2,%0; adcq %3,%1"                                       \
+    __asm__("addq %2,%0; adcq %3,%1"                                       \
         : "+m"(r), "+d"(high)                                          \
         : "r"(carry), "g"(0)                                           \
         : "cc");                                                       \
@@ -86,15 +85,15 @@
 #define mul(r, a, word, carry)                                         \
   do {                                                                 \
     register BN_ULONG high, low;                                       \
-    asm("mulq %3" : "=a"(low), "=d"(high) : "a"(word), "g"(a) : "cc"); \
-    asm("addq %2,%0; adcq %3,%1"                                       \
+    __asm__("mulq %3" : "=a"(low), "=d"(high) : "a"(word), "g"(a) : "cc"); \
+    __asm__("addq %2,%0; adcq %3,%1"                                       \
         : "+r"(carry), "+d"(high)                                      \
         : "a"(low), "g"(0)                                             \
         : "cc");                                                       \
     (r) = carry, carry = high;                                         \
   } while (0)
 #undef sqr
-#define sqr(r0, r1, a) asm("mulq %2" : "=a"(r0), "=d"(r1) : "a"(a) : "cc");
+#define sqr(r0, r1, a) __asm__("mulq %2" : "=a"(r0), "=d"(r1) : "a"(a) : "cc");
 
 BN_ULONG bn_mul_add_words(BN_ULONG *rp, const BN_ULONG *ap, int num,
                           BN_ULONG w) {
@@ -189,7 +188,7 @@ void bn_sqr_words(BN_ULONG *r, const BN_ULONG *a, int n) {
 BN_ULONG bn_div_words(BN_ULONG h, BN_ULONG l, BN_ULONG d) {
   BN_ULONG ret, waste;
 
-  asm("divq	%4" : "=a"(ret), "=d"(waste) : "a"(l), "d"(h), "g"(d) : "cc");
+  __asm__("divq	%4" : "=a"(ret), "=d"(waste) : "a"(l), "d"(h), "g"(d) : "cc");
 
   return ret;
 }
@@ -203,7 +202,7 @@ BN_ULONG bn_add_words(BN_ULONG *rp, const BN_ULONG *ap, const BN_ULONG *bp,
     return 0;
   }
 
-  asm volatile (
+  __asm__ volatile (
       "	subq	%0,%0		\n" /* clear carry */
       "	jmp	1f		\n"
       ".p2align 4			\n"
@@ -230,7 +229,7 @@ BN_ULONG bn_sub_words(BN_ULONG *rp, const BN_ULONG *ap, const BN_ULONG *bp,
     return 0;
   }
 
-  asm volatile (
+  __asm__ volatile (
       "	subq	%0,%0		\n" /* clear borrow */
       "	jmp	1f		\n"
       ".p2align 4			\n"
@@ -317,8 +316,8 @@ BN_ULONG bn_sub_words(BN_ULONG *r, BN_ULONG *a, BN_ULONG *b, int n) {
 #define mul_add_c(a, b, c0, c1, c2)          \
   do {                                       \
     BN_ULONG t1, t2;                \
-    asm("mulq %3" : "=a"(t1), "=d"(t2) : "a"(a), "m"(b) : "cc"); \
-    asm("addq %3,%0; adcq %4,%1; adcq %5,%2" \
+    __asm__("mulq %3" : "=a"(t1), "=d"(t2) : "a"(a), "m"(b) : "cc"); \
+    __asm__("addq %3,%0; adcq %4,%1; adcq %5,%2" \
         : "+r"(c0), "+r"(c1), "+r"(c2)       \
         : "r"(t1), "r"(t2), "g"(0)           \
         : "cc");                             \
@@ -327,8 +326,8 @@ BN_ULONG bn_sub_words(BN_ULONG *r, BN_ULONG *a, BN_ULONG *b, int n) {
 #define sqr_add_c(a, i, c0, c1, c2)          \
   do {                                       \
     BN_ULONG t1, t2;                         \
-    asm("mulq %2" : "=a"(t1), "=d"(t2) : "a"(a[i]) : "cc"); \
-    asm("addq %3,%0; adcq %4,%1; adcq %5,%2" \
+    __asm__("mulq %2" : "=a"(t1), "=d"(t2) : "a"(a[i]) : "cc"); \
+    __asm__("addq %3,%0; adcq %4,%1; adcq %5,%2" \
         : "+r"(c0), "+r"(c1), "+r"(c2)       \
         : "r"(t1), "r"(t2), "g"(0)           \
         : "cc");                             \
@@ -337,12 +336,12 @@ BN_ULONG bn_sub_words(BN_ULONG *r, BN_ULONG *a, BN_ULONG *b, int n) {
 #define mul_add_c2(a, b, c0, c1, c2)         \
   do {                                       \
     BN_ULONG t1, t2;                                                    \
-    asm("mulq %3" : "=a"(t1), "=d"(t2) : "a"(a), "m"(b) : "cc");        \
-    asm("addq %3,%0; adcq %4,%1; adcq %5,%2" \
+    __asm__("mulq %3" : "=a"(t1), "=d"(t2) : "a"(a), "m"(b) : "cc");        \
+    __asm__("addq %3,%0; adcq %4,%1; adcq %5,%2" \
         : "+r"(c0), "+r"(c1), "+r"(c2)       \
         : "r"(t1), "r"(t2), "g"(0)           \
         : "cc");                             \
-    asm("addq %3,%0; adcq %4,%1; adcq %5,%2" \
+    __asm__("addq %3,%0; adcq %4,%1; adcq %5,%2" \
         : "+r"(c0), "+r"(c1), "+r"(c2)       \
         : "r"(t1), "r"(t2), "g"(0)           \
         : "cc");                             \
