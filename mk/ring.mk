0c8d4214a27e0a34e65dbac13547c03cc0e70ccb
Brian Smith <brian@briansmith.org>
2016-01-21 14:56:56 -1000

    Remove -Wno-pedantic from C/C++ compiler command line.

--- mk/ring.mk
+++ mk/ring.mk
@@ -227,7 +227,6 @@ $(NULL): CPPFLAGS += $(RING_CPPFLAGS) \
                      -fno-strict-aliasing \
                      -fvisibility=hidden \
                      -Wno-cast-align \
-                     -Wno-pedantic \
                      -Wno-unused-parameter \
                      $(NULL)
 $(RING_OBJS) \
