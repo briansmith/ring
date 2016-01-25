75a64c08fc63ee9619214be5607a48ae9c97fdcc
Adam Langley <agl@google.com>
2016-01-25 14:43:01 -0800

    Remove some mingw support cruft.
    This was needed for Android, but “the new version of mingw has moved all
    of time_s.h into time.h” [1].
    
    [1] https://android-review.googlesource.com/#/c/196597/
    
    Change-Id: I17e66ed93606f3e6a774af3290c15b5ca151449f
    Reviewed-on: https://boringssl-review.googlesource.com/6971
    Reviewed-by: David Benjamin <davidben@google.com>

--- crypto/time_support.c
+++ crypto/time_support.c
@@ -59,12 +59,6 @@
 #define _POSIX_C_SOURCE 201410L  /* for gmtime_r */
 #endif
 
-#if defined(__MINGW32__)
-#define MINGW_HAS_SECURE_API 1  /* supplied by libmingwex */
-#include <sec_api/time_s.h>  /* for correct definition of gmtime_s */
-#undef MINGW_HAS_SECURE_API
-#endif
-
 #include <openssl/time_support.h>
 
 #include <time.h>
