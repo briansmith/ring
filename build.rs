17393444ef073c2e35505509cfe72b447673fe7d
Brian Smith <brian@briansmith.org>
2016-02-04 14:40:20 -1000

    Enable more lints and make them fatal.
    Basically, all the lints output by `rustc -W help` were added to
    `#![deny(...)]` lists in build.rs and lib.rs. Lints that currently
    fail because we do not want to follow the pattern they advise were
    added to `#![allow(...)]` lists. Lints that currently fail due to
    (minor) problems with the code were also added to the `#![allow(...)]`
    lists with `// TODO` comments. Each of those TODOs will be addressed in
    the following commits.

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-// Copyright 2015 Brian Smith.
+// Copyright 2015-2016 Brian Smith.
 //
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
@@ -11,6 +11,60 @@
 // WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
 // OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
 // CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#![allow(
+    box_pointers, // TODO
+    missing_docs)]
+#![deny(
+    const_err,
+    dead_code,
+    deprecated,
+    drop_with_repr_extern,
+    exceeding_bitshifts,
+    fat_ptr_transmutes,
+    improper_ctypes,
+    match_of_unit_variant_via_paren_dotdot,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    mutable_transmutes,
+    no_mangle_const_items,
+    non_camel_case_types,
+    non_shorthand_field_patterns,
+    non_snake_case,
+    non_upper_case_globals,
+    overflowing_literals,
+    path_statements,
+    plugin_as_library,
+    private_no_mangle_fns,
+    private_no_mangle_statics,
+    stable_features,
+    trivial_casts,
+    trivial_numeric_casts,
+    unconditional_recursion,
+    unknown_crate_types,
+    unknown_lints,
+    unreachable_code,
+    unsafe_code,
+    unstable_features,
+    unused_allocation,
+    unused_assignments,
+    unused_attributes,
+    unused_comparisons,
+    unused_extern_crates,
+    unused_features,
+    unused_import_braces,
+    unused_imports,
+    unused_must_use,
+    unused_mut,
+    unused_parens,
+    unused_qualifications,
+    unused_results,
+    unused_unsafe,
+    unused_variables,
+    variant_size_differences,
+    warnings,
+    while_true,
+)]
 
 extern crate num;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2015 Brian Smith.
+// Copyright 2015-2016 Brian Smith.
 //
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
@@ -26,6 +26,60 @@
 //!         <code>no_heap</code> mode, but for now some RSA, ECDH, and ECDSA
 //!         functionality still uses the heap.
 //! </table>
+
+#![allow(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs, // TODO
+    trivial_numeric_casts, // TODO
+    unsafe_code, // TODO
+    unused_import_braces, // TODO
+    unused_qualifications, // TODO
+    unused_results, // TODO
+)]
+#![deny(
+    const_err,
+    dead_code,
+    deprecated,
+    drop_with_repr_extern,
+    exceeding_bitshifts,
+    fat_ptr_transmutes,
+    improper_ctypes,
+    match_of_unit_variant_via_paren_dotdot,
+    mutable_transmutes,
+    no_mangle_const_items,
+    non_camel_case_types,
+    non_shorthand_field_patterns,
+    non_snake_case,
+    non_upper_case_globals,
+    overflowing_literals,
+    path_statements,
+    plugin_as_library,
+    private_no_mangle_fns,
+    private_no_mangle_statics,
+    stable_features,
+    trivial_casts,
+    unconditional_recursion,
+    unknown_crate_types,
+    unknown_lints,
+    unreachable_code,
+    unstable_features,
+    unused_allocation,
+    unused_assignments,
+    unused_attributes,
+    unused_comparisons,
+    unused_extern_crates,
+    unused_features,
+    unused_imports,
+    unused_must_use,
+    unused_mut,
+    unused_parens,
+    unused_unsafe,
+    unused_variables,
+    variant_size_differences,
+    warnings,
+    while_true,
+)]
 
 #![no_std]
 
