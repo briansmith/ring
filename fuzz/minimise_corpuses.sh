1d34e3c644cdafea4187002a8409895bbda31289
David Benjamin <davidben@google.com>
2016-03-04 12:30:51 -0500

    Add an option to pick a different build directory in minimise_corpuses.sh.
    Also pass set -e instead of chaining things with &&. (One line was missing the
    &&.)
    
    Change-Id: Ia04e7f40f46688c9664101efefef1d1ea069de71
    Reviewed-on: https://boringssl-review.googlesource.com/7300
    Reviewed-by: Adam Langley <agl@google.com>

--- fuzz/minimise_corpuses.sh
+++ fuzz/minimise_corpuses.sh
@@ -1,6 +1,13 @@
+set -e
+
+buildDir=../build
+if [ $# -gt 0 ]; then
+  buildDir=$1
+fi
+
 for testSource in $(ls -1 *.cc); do
   test=$(echo $testSource | sed -e 's/\.cc$//')
-  if [ ! -x ../build/fuzz/$test ] ; then
+  if [ ! -x $buildDir/fuzz/$test ] ; then
     echo "Failed to find binary for $test"
     exit 1
   fi
@@ -16,8 +23,9 @@ done
 
 for testSource in $(ls -1 *.cc); do
   test=$(echo $testSource | sed -e 's/\.cc$//')
-  mv ${test}_corpus ${test}_corpus_old && \
-    mkdir ${test}_corpus
-    ../build/fuzz/$test -max_len=50000 -runs=0 -save_minimized_corpus=1 ${test}_corpus ${test}_corpus_old &&
-    rm -Rf ${test}_corpus_old
+  mv ${test}_corpus ${test}_corpus_old
+  mkdir ${test}_corpus
+  $buildDir/fuzz/$test -max_len=50000 -runs=0 -save_minimized_corpus=1 \
+    ${test}_corpus ${test}_corpus_old
+  rm -Rf ${test}_corpus_old
 done
