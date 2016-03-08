4a9d0b0af5542b0b799a145a7a72521c98ed182f
Brian Smith <brian@briansmith.org>
2016-03-08 11:52:06 -1000

    Remove unneeded /dev/unrandom-related features.
    This is a prerequisite to simplifying some of the threading code.

--- crypto/rand/urandom.c
+++ crypto/rand/urandom.c
@@ -19,11 +19,8 @@
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
-
-#include <openssl/thread.h>
-#include <openssl/mem.h>
 
 #include "internal.h"
 #include "../internal.h"
@@ -41,23 +38,8 @@ struct rand_buffer {
   uint8_t rand[BUF_SIZE];
 };
 
-/* requested_lock is used to protect the |*_requested| variables. */
-static struct CRYPTO_STATIC_MUTEX requested_lock = CRYPTO_STATIC_MUTEX_INIT;
-
-/* urandom_fd_requested is set by |RAND_set_urandom_fd|.  It's protected by
- * |requested_lock|. */
-static int urandom_fd_requested = -2;
-
 /* urandom_fd is a file descriptor to /dev/urandom. It's protected by |once|. */
 static int urandom_fd = -2;
-
-/* urandom_buffering_requested is set by |RAND_enable_fork_unsafe_buffering|.
- * It's protected by |requested_lock|. */
-static int urandom_buffering_requested = 0;
-
-/* urandom_buffering controls whether buffering is enabled (1) or not (0). This
- * is protected by |once|. */
-static int urandom_buffering = 0;
 
 static CRYPTO_once_t once = CRYPTO_ONCE_INIT;
 
@@ -66,16 +48,11 @@ static CRYPTO_once_t once = CRYPTO_ONCE_INIT;
  * |urandom_buffering|, whose values may be read safely after calling the
  * once. */
 static void init_once(void) {
-  CRYPTO_STATIC_MUTEX_lock_read(&requested_lock);
-  urandom_buffering = urandom_buffering_requested;
-  int fd = urandom_fd_requested;
-  CRYPTO_STATIC_MUTEX_unlock(&requested_lock);
+  int fd;
 
-  if (fd == -2) {
-    do {
-      fd = open("/dev/urandom", O_RDONLY);
-    } while (fd == -1 && errno == EINTR);
-  }
+  do {
+    fd = open("/dev/urandom", O_RDONLY);
+  } while (fd == -1 && errno == EINTR);
 
   if (fd < 0) {
     abort();
@@ -98,64 +75,6 @@ static void init_once(void) {
 
 void RAND_cleanup(void) {}
 
-void RAND_set_urandom_fd(int fd) {
-  fd = dup(fd);
-  if (fd < 0) {
-    abort();
-  }
-
-  CRYPTO_STATIC_MUTEX_lock_write(&requested_lock);
-  urandom_fd_requested = fd;
-  CRYPTO_STATIC_MUTEX_unlock(&requested_lock);
-
-  CRYPTO_once(&once, init_once);
-  if (urandom_fd != fd) {
-    abort();  // Already initialized.
-  }
-}
-
-void RAND_enable_fork_unsafe_buffering(int fd) {
-  if (fd >= 0) {
-    fd = dup(fd);
-    if (fd < 0) {
-      abort();
-    }
-  } else {
-    fd = -2;
-  }
-
-  CRYPTO_STATIC_MUTEX_lock_write(&requested_lock);
-  urandom_buffering_requested = 1;
-  urandom_fd_requested = fd;
-  CRYPTO_STATIC_MUTEX_unlock(&requested_lock);
-
-  CRYPTO_once(&once, init_once);
-  if (urandom_buffering != 1 || (fd >= 0 && urandom_fd != fd)) {
-    abort();  // Already initialized.
-  }
-}
-
-static struct rand_buffer *get_thread_local_buffer(void) {
-  struct rand_buffer *buf =
-      CRYPTO_get_thread_local(OPENSSL_THREAD_LOCAL_URANDOM_BUF);
-  if (buf != NULL) {
-    return buf;
-  }
-
-  buf = OPENSSL_malloc(sizeof(struct rand_buffer));
-  if (buf == NULL) {
-    return NULL;
-  }
-  buf->used = BUF_SIZE;  /* To trigger a |read_full| on first use. */
-  if (!CRYPTO_set_thread_local(OPENSSL_THREAD_LOCAL_URANDOM_BUF, buf,
-                               OPENSSL_free)) {
-    OPENSSL_free(buf);
-    return NULL;
-  }
-
-  return buf;
-}
-
 /* read_full reads exactly |len| bytes from |fd| into |out| and returns 1. In
  * the case of an error it returns 0. */
 static char read_full(int fd, void *out, size_t len) {
@@ -176,45 +95,12 @@ static char read_full(int fd, void *out, size_t len) {
   return 1;
 }
 
-/* read_from_buffer reads |requested| random bytes from the buffer into |out|,
- * refilling it if necessary to satisfy the request. */
-static void read_from_buffer(struct rand_buffer *buf,
-                             void *out, size_t requested) {
-  size_t remaining = BUF_SIZE - buf->used;
-
-  while (requested > remaining) {
-    memcpy(out, &buf->rand[buf->used], remaining);
-    buf->used += remaining;
-    out = (uint8_t *)out + remaining;
-    requested -= remaining;
-
-    if (!read_full(urandom_fd, buf->rand, BUF_SIZE)) {
-      abort();
-      return;
-    }
-    buf->used = 0;
-    remaining = BUF_SIZE;
-  }
-
-  memcpy(out, &buf->rand[buf->used], requested);
-  buf->used += requested;
-}
-
 /* CRYPTO_sysrand puts |requested| random bytes into |out|. */
 void CRYPTO_sysrand(void *out, size_t requested) {
   if (requested == 0) {
     return;
   }
-
   CRYPTO_once(&once, init_once);
-  if (urandom_buffering && requested < BUF_SIZE) {
-    struct rand_buffer *buf = get_thread_local_buffer();
-    if (buf != NULL) {
-      read_from_buffer(buf, out, requested);
-      return;
-    }
-  }
-
   if (!read_full(urandom_fd, out, requested)) {
     abort();
   }
--- include/openssl/rand.h
+++ include/openssl/rand.h
@@ -35,33 +35,6 @@ OPENSSL_EXPORT void RAND_cleanup(void);
 
 /* Obscure functions. */
 
-#if !defined(OPENSSL_WINDOWS)
-/* RAND_set_urandom_fd causes the module to use a copy of |fd| for system
- * randomness rather opening /dev/urandom internally. The caller retains
- * ownership of |fd| and is at liberty to close it at any time. This is useful
- * if, due to a sandbox, /dev/urandom isn't available. If used, it must be
- * called before the first call to |RAND_bytes|, and it is mutually exclusive
- * with |RAND_enable_fork_unsafe_buffering|.
- *
- * |RAND_set_urandom_fd| does not buffer any entropy, so it is safe to call
- * |fork| at any time after calling |RAND_set_urandom_fd|. */
-OPENSSL_EXPORT void RAND_set_urandom_fd(int fd);
-
-/* RAND_enable_fork_unsafe_buffering enables efficient buffered reading of
- * /dev/urandom. It adds an overhead of a few KB of memory per thread. It must
- * be called before the first call to |RAND_bytes| and it is mutually exclusive
- * with calls to |RAND_set_urandom_fd|.
- *
- * If |fd| is non-negative then a copy of |fd| will be used rather than opening
- * /dev/urandom internally. Like |RAND_set_urandom_fd|, the caller retains
- * ownership of |fd|. If |fd| is negative then /dev/urandom will be opened and
- * any error from open(2) crashes the address space.
- *
- * It has an unusual name because the buffer is unsafe across calls to |fork|.
- * Hence, this function should never be called by libraries. */
-OPENSSL_EXPORT void RAND_enable_fork_unsafe_buffering(int fd);
-#endif
-
 #if defined(BORINGSSL_UNSAFE_FUZZER_MODE)
 /* RAND_reset_for_fuzzing resets the fuzzer-only deterministic RNG. This
  * function is only defined in the fuzzer-only build configuration. */
