a05f49fc0b325d61589b64c479f638dbbdff48de
Brian Smith <brian@briansmith.org>
2016-04-13 21:07:49 -1000

    Remove Unicode byte order marker in src/ecc_build.rs.

--- src/ecc_build.rs
+++ src/ecc_build.rs
@@ -1,4 +1,4 @@
-﻿// Copyright 2015 Brian Smith.
+// Copyright 2015-2016 Brian Smith.
 //
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
