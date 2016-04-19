c0d8b83b4462a0eb1889f32dbd7f46e83f4dbc81
David Benjamin <davidben@google.com>
2016-04-19 17:39:28 -0400

    Make SHA512_Final actually only return one.
    Rather than store md_len, factor out the common parts of SHA384_Final and
    SHA512_Final and then extract the right state. Also add a missing
    SHA384_Transform and be consistent about "1" vs "one" in comments.
    
    This also removes the NULL output special-case which no other hash function
    had.
    
    Change-Id: If60008bae7d7d5b123046a46d8fd64139156a7c5
    Reviewed-on: https://boringssl-review.googlesource.com/7720
    Reviewed-by: Adam Langley <agl@google.com>

--- include/openssl/sha.h
+++ include/openssl/sha.h
@@ -189,15 +189,15 @@ struct sha256_state_st {
 /* SHA384_DIGEST_LENGTH is the length of a SHA-384 digest. */
 #define SHA384_DIGEST_LENGTH 48
 
-/* SHA384_Init initialises |sha| and returns 1. */
+/* SHA384_Init initialises |sha| and returns one. */
 OPENSSL_EXPORT int SHA384_Init(SHA512_CTX *sha);
 
-/* SHA384_Update adds |len| bytes from |data| to |sha| and returns 1. */
+/* SHA384_Update adds |len| bytes from |data| to |sha| and returns one. */
 OPENSSL_EXPORT int SHA384_Update(SHA512_CTX *sha, const void *data, size_t len);
 
 /* SHA384_Final adds the final padding to |sha| and writes the resulting digest
  * to |md|, which must have at least |SHA384_DIGEST_LENGTH| bytes of space. It
- * returns one on success and zero on programmer error. */
+ * returns one. */
 OPENSSL_EXPORT int SHA384_Final(uint8_t *md, SHA512_CTX *sha);
 
 /* SHA384 writes the digest of |len| bytes from |data| to |out| and returns
@@ -218,15 +218,15 @@ OPENSSL_EXPORT void SHA384_Transform(SHA512_CTX *sha, const uint8_t *data);
 /* SHA512_DIGEST_LENGTH is the length of a SHA-512 digest. */
 #define SHA512_DIGEST_LENGTH 64
 
-/* SHA512_Init initialises |sha| and returns 1. */
+/* SHA512_Init initialises |sha| and returns one. */
 OPENSSL_EXPORT int SHA512_Init(SHA512_CTX *sha);
 
-/* SHA512_Update adds |len| bytes from |data| to |sha| and returns 1. */
+/* SHA512_Update adds |len| bytes from |data| to |sha| and returns one. */
 OPENSSL_EXPORT int SHA512_Update(SHA512_CTX *sha, const void *data, size_t len);
 
 /* SHA512_Final adds the final padding to |sha| and writes the resulting digest
  * to |md|, which must have at least |SHA512_DIGEST_LENGTH| bytes of space. It
- * returns one on success and zero on programmer error. */
+ * returns one. */
 OPENSSL_EXPORT int SHA512_Final(uint8_t *md, SHA512_CTX *sha);
 
 /* SHA512 writes the digest of |len| bytes from |data| to |out| and returns
@@ -245,7 +245,7 @@ struct sha512_state_st {
     uint64_t d[16];
     uint8_t p[128];
   } u;
-  unsigned num, md_len;
+  unsigned num;
 };
 
 
