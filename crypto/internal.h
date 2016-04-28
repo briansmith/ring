992c20aedc0e5206bfa2b6bc032fbff570c6deb3
David Benjamin <davidben@google.com>
2016-04-28 12:56:35 -0400

    Another OPENSSL_NO_THREADS build fix.
    GCC gets unhappy if we don't initialize the padding.
    
    Change-Id: I084ffee1717d9025dcb10d8f32de0da2339c7f01
    Reviewed-on: https://boringssl-review.googlesource.com/7797
    Reviewed-by: Steven Valdez <svaldez@google.com>
    Reviewed-by: David Benjamin <davidben@google.com>

--- crypto/internal.h
+++ crypto/internal.h
@@ -366,7 +366,7 @@ OPENSSL_EXPORT int CRYPTO_refcount_dec_and_test_zero(CRYPTO_refcount_t *count);
 struct CRYPTO_STATIC_MUTEX {
   char padding;  /* Empty structs have different sizes in C and C++. */
 };
-#define CRYPTO_STATIC_MUTEX_INIT {}
+#define CRYPTO_STATIC_MUTEX_INIT { 0 }
 #elif defined(OPENSSL_WINDOWS)
 struct CRYPTO_STATIC_MUTEX {
   CRYPTO_once_t once;
