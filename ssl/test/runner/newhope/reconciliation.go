7fcfd3b37a73ca3efde348b8bb41f37052cdd959
Adam Langley <alangley@gmail.com>
2016-05-20 11:02:50 -0700

    Add ISC license to Go files that were missing a license.
    Change-Id: I1fe3bed7d5c577748c9f4c3ccd5c1b90fec3d7d7
    Reviewed-on: https://boringssl-review.googlesource.com/8032
    Reviewed-by: David Benjamin <davidben@google.com>

--- ssl/test/runner/newhope/reconciliation.go
+++ ssl/test/runner/newhope/reconciliation.go
@@ -1,8 +1,25 @@
+// Copyright (c) 2016, Google Inc.
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+// OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+// CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+
 package newhope
 
 // This file contains the reconciliation algorithm for NewHope. This is simply a
 // monkey-see-monkey-do version of the reference code, with the exception that
 // the key resulting from reconciliation is whitened with SHA2 rather than SHA3.
+//
+// Thanks to the authors of the reference code for allowing us to release this
+// under the BoringSSL license.
 
 import (
 	"crypto/sha256"
