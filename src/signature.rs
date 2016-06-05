5f3956f923954cb9e5a34de108e0c27626696fab
Dirkjan Ochtman <dirkjan@ochtman.nl>
2016-06-05 10:04:56 +0200

    Implement signing for Ed25519 (fixes #206)
    I agree to license my contributions to each file under the terms given
    at the top of each file I changed.

--- src/signature.rs
+++ src/signature.rs
@@ -55,6 +55,7 @@ pub use ec::suite_b::ecdsa::{
 };
 
 pub use ec::eddsa::ED25519_VERIFY;
+pub use ec::eddsa::Ed25519KeyPair;
 
 #[cfg(not(feature = "no_heap"))]
 pub use rsa::{
@@ -65,6 +66,23 @@ pub use rsa::{
 
     RSA_PKCS1_3072_8192_SHA384_VERIFY,
 };
+
+/// A public key signature.
+pub struct Signature {
+    value: [u8; 64],
+}
+
+impl<'a> Signature {
+    /// Initialize signature from slice of bytes.
+    pub fn new(signature_bytes: [u8; 64]) -> Signature {
+        Signature { value: signature_bytes }
+    }
+
+    /// Returns a reference to the signature's encoded value.
+    pub fn as_slice(&'a self) -> &'a [u8] {
+        &self.value[..]
+    }
+}
 
 /// A signature verification algorithm.
 //
