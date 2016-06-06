briansmith/ring

commit 0218774094d482dd17c3212986f6702f760174b7

Verify that Ed25519 public & private keys are consistent.
The consistency check is important to avoid misuse of the API and
to protect against corruption of the key pair on disk.

Also, remove openssl/curve25519.h since it isn't used. Finally,
consistently name all the ED25519 C functions with the GFp_ prefix.

crypto/curve25519/internal.h
@@ -15,6 +15,9 @@
 #ifndef OPENSSL_HEADER_CURVE25519_INTERNAL_H
 #define OPENSSL_HEADER_CURVE25519_INTERNAL_H
 
+#include <stdint.h>
+
+
 #if defined(__cplusplus)
 extern "C" {
 #endif

crypto/curve25519/curve25519.c
@@ -19,8 +19,6 @@
  *
  * The field functions are shared by Ed25519 and X25519 where possible. */
 
-#include <openssl/curve25519.h>
-
 #include <string.h>
 
 #include <openssl/cpu.h>
@@ -4623,14 +4621,19 @@ static void sc_muladd(uint8_t *s, const uint8_t *a, const uint8_t *b,
   s[31] = s11 >> 17;
 }
 
-int ED25519_keypair(uint8_t out_private_key[64], RAND *rng) {
-  uint8_t seed[32];
-  if (!RAND_bytes(rng, seed, 32)) {
-    return 0;
-  }
 
+/* Prototype to avoid -Wmissing-prototypes warnings. */
+void GFp_ed25519_public_from_private(uint8_t out[32], const uint8_t in[32]);
+void GFp_ed25519_sign(uint8_t *out_sig, const uint8_t *message,
+                      size_t message_len, const uint8_t private_key[64]);
+int GFp_ed25519_verify(const uint8_t *message, size_t message_len,
+                       const uint8_t signature[64],
+                       const uint8_t public_key[32]);
+
+
+void GFp_ed25519_public_from_private(uint8_t out[32], const uint8_t in[32]) {
   uint8_t az[SHA512_DIGEST_LENGTH];
-  SHA512_4(az, sizeof(az), seed, 32, NULL, 0, NULL, 0, NULL, 0);
+  SHA512_4(az, sizeof(az), in, 32, NULL, 0, NULL, 0, NULL, 0);
 
   az[0] &= 248;
   az[31] &= 63;
@@ -4638,14 +4641,11 @@ int ED25519_keypair(uint8_t out_private_key[64], RAND *rng) {
 
   ge_p3 A;
   x25519_ge_scalarmult_base(&A, az);
-  ge_p3_tobytes(&out_private_key[32], &A);
-
-  memcpy(out_private_key, seed, 32);
-  return 1;
+  ge_p3_tobytes(out, &A);
 }
 
-void ED25519_sign(uint8_t *out_sig, const uint8_t *message, size_t message_len,
-                  const uint8_t private_key[64]) {
+void GFp_ed25519_sign(uint8_t *out_sig, const uint8_t *message,
+                      size_t message_len, const uint8_t private_key[64]) {
   uint8_t az[SHA512_DIGEST_LENGTH];
   SHA512_4(az, sizeof(az), private_key, 32, NULL, 0, NULL, 0, NULL, 0);
 
@@ -4670,8 +4670,9 @@ void ED25519_sign(uint8_t *out_sig, const uint8_t *message, size_t message_len,
   sc_muladd(out_sig + 32, hram, az, nonce);
 }
 
-int ED25519_verify(const uint8_t *message, size_t message_len,
-                   const uint8_t signature[64], const uint8_t public_key[32]) {
+int GFp_ed25519_verify(const uint8_t *message, size_t message_len,
+                       const uint8_t signature[64],
+                       const uint8_t public_key[32]) {
   ge_p3 A;
   if ((signature[63] & 224) != 0 ||
       x25519_ge_frombytes_vartime(&A, public_key) != 0) {

crypto/curve25519/x25519-x86_64.c
@@ -1,5 +1,3 @@
-#include <openssl/curve25519.h>
-
 #include <string.h>
 
 #include "internal.h"

include/openssl/curve25519.h
@@ -1,58 +1,0 @@
-/* Copyright (c) 2015, Google Inc.
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
- * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
- * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
-
-#ifndef OPENSSL_HEADER_CURVE25519_H
-#define OPENSSL_HEADER_CURVE25519_H
-
-#include <openssl/base.h>
-
-#if defined(__cplusplus)
-extern "C" {
-#endif
-
-
-/* Ed25519.
- *
- * Ed25519 is a signature scheme using a twisted-Edwards curve that is
- * birationally equivalent to curve25519. */
-
-#define ED25519_PRIVATE_KEY_LEN 64
-#define ED25519_PUBLIC_KEY_LEN 32
-#define ED25519_SIGNATURE_LEN 64
-
-/* ED25519_keypair sets |out_private_key| to a freshly generated
- * public–private key pair: the first 32 bytes form the private key, the second
- * 32 bytes contain the public key. It returns one on success or zero on
- * failure (to get bytes from the rng). */
-OPENSSL_EXPORT int ED25519_keypair(uint8_t out_private_key[64], RAND *rng);
-
-/* ED25519_sign sets |out_sig| to be a signature of |message_len| bytes from
- * |message| using |private_key|. */
-OPENSSL_EXPORT void ED25519_sign(uint8_t out_sig[64], const uint8_t *message,
-                                 size_t message_len,
-                                 const uint8_t private_key[64]);
-
-/* ED25519_verify returns one iff |signature| is a valid signature, by
- * |public_key| of |message_len| bytes from |message|. It returns zero
- * otherwise. */
-OPENSSL_EXPORT int ED25519_verify(const uint8_t *message, size_t message_len,
-                                  const uint8_t signature[64],
-                                  const uint8_t public_key[32]);
-
-
-#if defined(__cplusplus)
-}  /* extern C */
-#endif
-
-#endif  /* OPENSSL_HEADER_CURVE25519_H */

src/ec/eddsa.rs
@@ -16,9 +16,8 @@
 
 //! EdDSA Signatures.
 
-use {bssl, c, signature, signature_impl};
+use {bssl, c, rand, signature, signature_impl};
 use untrusted;
-use rand::{RAND, SecureRandom};
 
 struct EdDSA;
 
@@ -29,19 +28,28 @@ pub struct Ed25519KeyPair {
 
 impl<'a> Ed25519KeyPair {
     /// Generates a new key pair.
-    pub fn generate(rng: &SecureRandom) -> Result<Ed25519KeyPair, ()> {
-        let mut rand = RAND::new(rng);
+    pub fn generate(rng: &rand::SecureRandom) -> Result<Ed25519KeyPair, ()> {
         let mut pair = Ed25519KeyPair { private_public: [0; 64] };
-        try!(bssl::map_result(unsafe {
-            ED25519_keypair(pair.private_public.as_mut_ptr(), &mut rand)
-        }));
+        try!(rng.fill(&mut pair.private_public[0..32]));
+        unsafe {
+            GFp_ed25519_public_from_private(
+                pair.private_public[32..].as_mut_ptr(),
+                pair.private_public.as_ptr());
+        }
         Ok(pair)
     }
 
-    /// Copies key data from the given slices to create a new key pair.
-    /// The arguments are interpreted as little-endian-encoded key bytes.
+    /// Copies key data from the given slices to create a new key pair. The
+    /// first slice must hold the private key and the second slice must hold
+    /// the public key. Both slices must contain 32 little-endian-encoded
+    /// bytes.
     ///
     /// This is intended for use by code that deserializes key pairs.
+    ///
+    /// The private and public keys will be verified to be consistent. This
+    /// helps protect, for example, against the accidental swapping of the
+    /// public and private components of the key pair. This also detects
+    /// corruption that might have occurred during storage of the key pair.
     pub fn from_bytes(private_key: &[u8], public_key: &[u8])
                       -> Result<Ed25519KeyPair, ()> {
         if private_key.len() != 32 {
@@ -50,19 +58,23 @@ impl<'a> Ed25519KeyPair {
             return Err(());
         }
         let mut pair = Ed25519KeyPair { private_public: [0; 64] };
-        {
-            let (pair_priv, pair_pub) = pair.private_public.split_at_mut(32);
-            for i in 0..pair_priv.len() {
-                pair_priv[i] = private_key[i];
-            }
-            for i in 0..pair_pub.len() {
-                pair_pub[i] = public_key[i];
-            }
+        for i in 0..32 {
+            pair.private_public[i] = private_key[i];
+        }
+        unsafe {
+            GFp_ed25519_public_from_private(
+                pair.private_public[32..].as_mut_ptr(),
+                pair.private_public.as_ptr());
+        }
+        if &pair.private_public[32..] != public_key {
+            return Err(());
         }
         Ok(pair)
     }
 
     /// Returns a reference to the little-endian-encoded private key bytes.
+    ///
+    /// This is intended for use by code that serializes the key pair.
     pub fn private_key_bytes(&'a self) -> &'a [u8] {
         &self.private_public[..32]
     }
@@ -76,8 +88,8 @@ impl<'a> Ed25519KeyPair {
     pub fn sign(&self, msg: &[u8]) -> signature::Signature {
         let mut signature_bytes = [0u8; 64];
         unsafe {
-            ED25519_sign(signature_bytes.as_mut_ptr(), msg.as_ptr(),
-                         msg.len(), self.private_public.as_ptr());
+            GFp_ed25519_sign(signature_bytes.as_mut_ptr(), msg.as_ptr(),
+                             msg.len(), self.private_public.as_ptr());
         }
         signature::Signature::new(signature_bytes)
     }
@@ -103,32 +115,29 @@ impl signature_impl::VerificationAlgorithmImpl for EdDSA {
         let msg = msg.as_slice_less_safe();
         let signature = signature.as_slice_less_safe();
         bssl::map_result(unsafe {
-            ED25519_verify(msg.as_ptr(), msg.len(), signature.as_ptr(),
-                           public_key.as_ptr())
+            GFp_ed25519_verify(msg.as_ptr(), msg.len(), signature.as_ptr(),
+                               public_key.as_ptr())
         })
     }
 }
 
 
-#[allow(improper_ctypes)]
-extern {
-    fn ED25519_keypair(out_private_key: *mut u8/*[64]*/,
-                       rng: *mut RAND) -> c::int;
-}
-
 extern  {
-    fn ED25519_sign(out_sig: *mut u8/*[64]*/, message: *const u8,
-                    message_len: c::size_t, private_key: *const u8/*[64]*/);
+    fn GFp_ed25519_public_from_private(out: *mut u8/*[32]*/,
+                                       in_: *const u8/*[32]*/);
 
-    fn ED25519_verify(message: *const u8, message_len: c::size_t,
-                      signature: *const u8/*[64]*/,
-                      public_key: *const u8/*[32]*/) -> c::int;
+    fn GFp_ed25519_sign(out_sig: *mut u8/*[64]*/, message: *const u8,
+                        message_len: c::size_t, private_key: *const u8/*[64]*/);
+
+    fn GFp_ed25519_verify(message: *const u8, message_len: c::size_t,
+                          signature: *const u8/*[64]*/,
+                          public_key: *const u8/*[32]*/) -> c::int;
 }
 
 
 #[cfg(test)]
 mod tests {
-    use {file_test, signature};
+    use {file_test, rand, signature};
     use untrusted;
     use super::Ed25519KeyPair;
 
@@ -156,5 +165,19 @@ mod tests {
             assert!(signature::verify(&signature::ED25519_VERIFY, public_key,
                                       msg, expected_sig).is_ok());
         });
+    }
+
+    #[test]
+    fn test_ed25519_from_bytes_swapped() {
+        let rng = rand::SystemRandom::new();
+        let key_pair = Ed25519KeyPair::generate(&rng).unwrap();
+
+        assert!(Ed25519KeyPair::from_bytes(key_pair.private_key_bytes(),
+                                           key_pair.public_key_bytes())
+                                           .is_ok());
+
+        assert!(Ed25519KeyPair::from_bytes(key_pair.public_key_bytes(),
+                                           key_pair.private_key_bytes())
+                                           .is_err());
     }
 }

commit ba9a9beb284cda01ad12789735d46ab0ec1cd1d3

Fix build.
The build was broken by 0218774094d482dd17c3212986f6702f760174b7.

crypto/curve25519/internal.h
@@ -15,6 +15,8 @@
 #ifndef OPENSSL_HEADER_CURVE25519_INTERNAL_H
 #define OPENSSL_HEADER_CURVE25519_INTERNAL_H
 
+#include <openssl/base.h>
+
 #include <stdint.h>
 
 