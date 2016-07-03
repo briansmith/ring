d8c25bd7ffcc56c4810a35181a43fdb8945f6226
Brian Smith <brian@briansmith.org>
2016-07-03 10:42:22 -1000

    Remove the dependency on rustc_serialize.

--- src/digest/digest.rs
+++ src/digest/digest.rs
@@ -221,15 +221,11 @@ impl Clone for Context {
 /// # Examples:
 ///
 /// ```
-/// extern crate ring;
-/// extern crate rustc_serialize;
-///
 /// # fn main() {
-/// use ring::digest;
-/// use rustc_serialize::hex::FromHex;
+/// use ring::{digest, test};
 ///
 /// let expected_hex = "09ca7e4eaa6e8ae9c7d261167129184883644d07dfba7cbfbc4c8a2e08360d5b";
-/// let expected: Vec<u8> = expected_hex.from_hex().unwrap();
+/// let expected: Vec<u8> = test::from_hex(expected_hex).unwrap();
 /// let actual = digest::digest(&digest::SHA256, b"hello, world");
 ///
 /// assert_eq!(&expected, &actual.as_ref());
