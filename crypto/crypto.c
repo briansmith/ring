02602de552c3cd0a97eb2f47afd7768fcd39dccc
Brian Smith <brian@briansmith.org>
2016-08-23 18:06:08 -1000

    Use the "GFp_" prefix C integer type metrics & simplify them.
    They were previously functions so that they worked when *ring* was
    built as a DLL on Windows, but that's not a supported configuration, so
    thats unnecessary.

--- crypto/crypto.c
+++ crypto/crypto.c
@@ -77,29 +77,25 @@ uint32_t GFp_armcap_P = 0;
 
 #endif
 
-/* These functions allow tests in other languages to verify that their
- * understanding of the C types matches the C compiler's understanding. */
+/* These allow tests in other languages to verify that their understanding of
+ * the C types matches the C compiler's understanding. */
 
-#define DEFINE_METRICS_FUNCTIONS(ty) \
-  /* Declarations to avoid -Wmissing-prototypes warnings. */ \
-  OPENSSL_EXPORT uint16_t ring_##ty##_align(void); \
-  OPENSSL_EXPORT uint16_t ring_##ty##_size(void); \
-  \
-  OPENSSL_EXPORT uint16_t ring_##ty##_align(void) { return alignof(ty); } \
-  OPENSSL_EXPORT uint16_t ring_##ty##_size(void) { return sizeof(ty); }
+#define DEFINE_METRICS(ty) \
+  OPENSSL_EXPORT uint16_t GFp_##ty##_align = alignof(ty); \
+  OPENSSL_EXPORT uint16_t GFp_##ty##_size = sizeof(ty);
 
-DEFINE_METRICS_FUNCTIONS(int8_t)
-DEFINE_METRICS_FUNCTIONS(uint8_t)
+DEFINE_METRICS(int8_t)
+DEFINE_METRICS(uint8_t)
 
-DEFINE_METRICS_FUNCTIONS(int16_t)
-DEFINE_METRICS_FUNCTIONS(uint16_t)
+DEFINE_METRICS(int16_t)
+DEFINE_METRICS(uint16_t)
 
-DEFINE_METRICS_FUNCTIONS(int32_t)
-DEFINE_METRICS_FUNCTIONS(uint32_t)
+DEFINE_METRICS(int32_t)
+DEFINE_METRICS(uint32_t)
 
-DEFINE_METRICS_FUNCTIONS(int64_t)
-DEFINE_METRICS_FUNCTIONS(uint64_t)
+DEFINE_METRICS(int64_t)
+DEFINE_METRICS(uint64_t)
 
-DEFINE_METRICS_FUNCTIONS(int)
+DEFINE_METRICS(int)
 
-DEFINE_METRICS_FUNCTIONS(size_t)
+DEFINE_METRICS(size_t)
