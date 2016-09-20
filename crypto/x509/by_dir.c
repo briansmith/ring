1ccfb4e32d9dbc46eb7e0e27608c7f62fc280c3f
David Benjamin <davidben@google.com>
2016-09-20 17:20:59 -0400

    Shush a MinGW warning in crypto/x509.
    The MinGW setup on Android already defines this stat macro.
    
    Change-Id: Ia8e89195c06ec01d4b5a2fa7357fb8d2d500aa06
    Reviewed-on: https://boringssl-review.googlesource.com/11124
    Reviewed-by: Kenny Root <kroot@google.com>
    Reviewed-by: David Benjamin <davidben@google.com>
    Commit-Queue: David Benjamin <davidben@google.com>
    CQ-Verified: CQ bot account: commit-bot@chromium.org <commit-bot@chromium.org>

--- crypto/x509/by_dir.c
+++ crypto/x509/by_dir.c
@@ -362,7 +362,7 @@ static int get_cert_by_subject(X509_LOOKUP *xl, int type, X509_NAME *name,
                                  postfix, k);
                 }
 #ifndef OPENSSL_NO_POSIX_IO
-# ifdef _WIN32
+# if defined(_WIN32) && !defined(stat)
 #  define stat _stat
 # endif
                 {
