44c1a65760bf1c822652f9a368f475810ce1f73b
Nick Harper <nharper@chromium.org>
2017-01-23 16:28:12 -0800

    Run go fmt on bogo code.
    Change-Id: I15363a9c9ebb4e08bd9cf45ba2c95368766bb19b
    Reviewed-on: https://boringssl-review.googlesource.com/13240
    Reviewed-by: David Benjamin <davidben@google.com>

--- ssl/test/runner/cipher_suites.go
+++ ssl/test/runner/cipher_suites.go
@@ -509,7 +509,7 @@ const (
 
 // Additional cipher suite IDs, not IANA-assigned.
 const (
-	TLS_AES_128_GCM_SHA256                            uint16 = 0x1301
-	TLS_AES_256_GCM_SHA384                            uint16 = 0x1302
-	TLS_CHACHA20_POLY1305_SHA256                      uint16 = 0x1303
+	TLS_AES_128_GCM_SHA256       uint16 = 0x1301
+	TLS_AES_256_GCM_SHA384       uint16 = 0x1302
+	TLS_CHACHA20_POLY1305_SHA256 uint16 = 0x1303
 )
