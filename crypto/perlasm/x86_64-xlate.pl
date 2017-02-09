689eb3d03ac6f3d6fcf3052b8a9229195944d6a8
Adam Langley <agl@google.com>
2017-02-09 11:44:06 -0800

    x86_64-xlate.pl: import fix(?) from upstream.
    This imports the changes to x86_64-xlate from upstream's
    9c940446f614d1294fa197ffd4128206296b04da. It looks like it's a fix,
    although it doesn't alter our generated asm at all. Either way, no point
    in diverging from upstream on this point.
    
    Change-Id: Iaedf2cdb9580cfccf6380dbc3df36b0e9c148d1c
    Reviewed-on: https://boringssl-review.googlesource.com/13767
    Commit-Queue: Adam Langley <agl@google.com>
    Commit-Queue: David Benjamin <davidben@google.com>
    Reviewed-by: David Benjamin <davidben@google.com>

--- crypto/perlasm/x86_64-xlate.pl
+++ crypto/perlasm/x86_64-xlate.pl
@@ -1047,7 +1047,7 @@ my $rdrand = sub {
       my @opcode=();
       my $dst=$1;
 	if ($dst !~ /[0-9]+/) { $dst = $regrm{"%e$dst"}; }
-	rex(\@opcode,0,$1,8);
+	rex(\@opcode,0,$dst,8);
 	push @opcode,0x0f,0xc7,0xf0|($dst&7);
 	@opcode;
     } else {
@@ -1060,7 +1060,7 @@ my $rdseed = sub {
       my @opcode=();
       my $dst=$1;
 	if ($dst !~ /[0-9]+/) { $dst = $regrm{"%e$dst"}; }
-	rex(\@opcode,0,$1,8);
+	rex(\@opcode,0,$dst,8);
 	push @opcode,0x0f,0xc7,0xf8|($dst&7);
 	@opcode;
     } else {
