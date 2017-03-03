3442aa7d6c5d8918927baaa4d9116b44b1b1ee64
Brian Smith <brian@briansmith.org>
2017-03-02 15:46:54 -1000

    Implement `GFp_bn_sub_words` in terms of `limbs_sub`.

--- crypto/bn/generic.c
+++ crypto/bn/generic.c
@@ -59,6 +59,7 @@
 #include <assert.h>
 
 #include "internal.h"
+#include "../limbs/limbs.inl"
 
 
 #define mul_add(r, a, w, c)             \
@@ -204,55 +205,9 @@ BN_ULONG GFp_bn_add_words(BN_ULONG *r, const BN_ULONG *a, const BN_ULONG *b,
 
 BN_ULONG GFp_bn_sub_words(BN_ULONG *r, const BN_ULONG *a, const BN_ULONG *b,
                           int n) {
-  BN_ULONG t1, t2;
-  int c = 0;
-
   assert(n >= 0);
   if (n <= 0) {
     return (BN_ULONG)0;
   }
-
-  while (n & ~3) {
-    t1 = a[0];
-    t2 = b[0];
-    r[0] = (t1 - t2 - c) & BN_MASK2;
-    if (t1 != t2) {
-      c = (t1 < t2);
-    }
-    t1 = a[1];
-    t2 = b[1];
-    r[1] = (t1 - t2 - c) & BN_MASK2;
-    if (t1 != t2) {
-      c = (t1 < t2);
-    }
-    t1 = a[2];
-    t2 = b[2];
-    r[2] = (t1 - t2 - c) & BN_MASK2;
-    if (t1 != t2) {
-      c = (t1 < t2);
-    }
-    t1 = a[3];
-    t2 = b[3];
-    r[3] = (t1 - t2 - c) & BN_MASK2;
-    if (t1 != t2) {
-      c = (t1 < t2);
-    }
-    a += 4;
-    b += 4;
-    r += 4;
-    n -= 4;
-  }
-  while (n) {
-    t1 = a[0];
-    t2 = b[0];
-    r[0] = (t1 - t2 - c) & BN_MASK2;
-    if (t1 != t2) {
-      c = (t1 < t2);
-    }
-    a++;
-    b++;
-    r++;
-    n--;
-  }
-  return c;
+  return limbs_sub(r, a, b, (size_t)n);
 }
