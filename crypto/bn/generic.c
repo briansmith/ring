ffa901fbbe260c52b7037aa3b97781af35e2d892
Brian Smith <brian@briansmith.org>
2017-03-02 15:55:18 -1000

    Implement `GFp_bn_add_words` in terms of `limbs_add`.

--- crypto/bn/generic.c
+++ crypto/bn/generic.c
@@ -150,57 +150,11 @@ BN_ULONG GFp_bn_mul_words(BN_ULONG *rp, const BN_ULONG *ap, int num,
 
 BN_ULONG GFp_bn_add_words(BN_ULONG *r, const BN_ULONG *a, const BN_ULONG *b,
                           int n) {
-  BN_ULONG c, l, t;
-
   assert(n >= 0);
   if (n <= 0) {
     return (BN_ULONG)0;
   }
-
-  c = 0;
-  while (n & ~3) {
-    t = a[0];
-    t = (t + c) & BN_MASK2;
-    c = (t < c);
-    l = (t + b[0]) & BN_MASK2;
-    c += (l < t);
-    r[0] = l;
-    t = a[1];
-    t = (t + c) & BN_MASK2;
-    c = (t < c);
-    l = (t + b[1]) & BN_MASK2;
-    c += (l < t);
-    r[1] = l;
-    t = a[2];
-    t = (t + c) & BN_MASK2;
-    c = (t < c);
-    l = (t + b[2]) & BN_MASK2;
-    c += (l < t);
-    r[2] = l;
-    t = a[3];
-    t = (t + c) & BN_MASK2;
-    c = (t < c);
-    l = (t + b[3]) & BN_MASK2;
-    c += (l < t);
-    r[3] = l;
-    a += 4;
-    b += 4;
-    r += 4;
-    n -= 4;
-  }
-  while (n) {
-    t = a[0];
-    t = (t + c) & BN_MASK2;
-    c = (t < c);
-    l = (t + b[0]) & BN_MASK2;
-    c += (l < t);
-    r[0] = l;
-    a++;
-    b++;
-    r++;
-    n--;
-  }
-  return (BN_ULONG)c;
+  return limbs_add(r, a, b, (size_t)n);
 }
 
 BN_ULONG GFp_bn_sub_words(BN_ULONG *r, const BN_ULONG *a, const BN_ULONG *b,
