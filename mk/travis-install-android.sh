f0f584863e17583ab20018916c07ab7b77cb69b5
Pietro Monteiro <pietro@riseup.net>
2017-03-06 16:56:58 -0800

    Update Android NDK.

--- mk/travis-install-android.sh
+++ mk/travis-install-android.sh
@@ -24,7 +24,7 @@ set -ex
 ANDROID_SDK_VERSION=${ANDROID_SDK_VERSION:-24.4.1}
 ANDROID_SDK_URL=https://dl.google.com/android/android-sdk_r${ANDROID_SDK_VERSION}-linux.tgz
 
-ANDROID_NDK_VERSION=${ANDROID_NDK_VERSION:-10e}
+ANDROID_NDK_VERSION=${ANDROID_NDK_VERSION:-14}
 ANDROID_NDK_URL=https://dl.google.com/android/repository/android-ndk-r${ANDROID_NDK_VERSION}-linux-x86_64.zip
 
 ANDROID_INSTALL_PREFIX="${HOME}/android"
