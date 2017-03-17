ac8999da4d11adb414536242d677e25872434196
Brian Smith <brian@briansmith.org>
2017-03-17 11:36:25 -1000

    Remove currently-unused crypto/perlasm/ppc-xlate.pl.

--- build.rs
+++ build.rs
@@ -204,7 +204,6 @@ const RING_INCLUDES: &'static [&'static str] =
 #[cfg_attr(rustfmt, rustfmt_skip)]
 const RING_PERL_INCLUDES: &'static [&'static str] =
     &["crypto/perlasm/arm-xlate.pl",
-      "crypto/perlasm/ppc-xlate.pl",
       "crypto/sha/asm/sha-x86_64.pl",
       "crypto/sha/asm/sha-armv8.pl",
       "crypto/perlasm/x86masm.pl",
