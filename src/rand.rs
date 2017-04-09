briansmith/ring

commit d51e389673277acd6733de0b136d33e79967e4f9

Remove now-unused `GFp_RAND_bytes`.

src/rand.rs
@@ -30,9 +30,6 @@
           test))]
 use c;
 
-#[cfg(test)]
-use core;
-
 use error;
 
 
@@ -255,21 +252,6 @@ impl<'a> RAND<'a> {
     /// Wraps `rng` in a `RAND` so it can be passed to non-Rust code.
     pub fn new(rng: &'a SecureRandom) -> RAND<'a> { RAND { rng: rng } }
 }
-
-#[cfg(test)]
-#[allow(non_snake_case)]
-#[doc(hidden)]
-#[no_mangle]
-pub unsafe extern fn GFp_RAND_bytes(rng: *mut RAND, dest: *mut u8,
-                                    dest_len: c::size_t) -> c::int {
-    let dest: &mut [u8] = core::slice::from_raw_parts_mut(dest, dest_len);
-
-    match (*(*rng).rng).fill(dest) {
-        Ok(()) => 1,
-        _ => 0,
-    }
-}
-
 
 #[cfg(any(target_os = "linux", windows))]
 extern {

Cargo.toml
@@ -231,7 +231,6 @@ include = [
     "crypto/test/bn_test_util.h",
     "crypto/test/file_test.cc",
     "crypto/test/file_test.h",
-    "crypto/test/rand.h",
     "crypto/test/scoped_types.h",
     "include/openssl/aes.h",
     "include/openssl/arm_arch.h",

build.rs
@@ -193,7 +193,6 @@ const RING_INCLUDES: &'static [&'static str] =
       "crypto/test/bn_test_lib.h",
       "crypto/test/bn_test_util.h",
       "crypto/test/file_test.h",
-      "crypto/test/rand.h",
       "crypto/test/scoped_types.h",
       "include/openssl/aes.h",
       "include/openssl/arm_arch.h",

crypto/test/rand.h
@@ -1,36 +1,0 @@
-/* Copyright (c) 2014, Google Inc.
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
- * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
- * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
-
-#ifndef OPENSSL_HEADER_RAND_H
-#define OPENSSL_HEADER_RAND_H
-
-#include <openssl/base.h>
-
-#if defined(__cplusplus)
-extern "C" {
-#endif
-
-
-/* Random number generation. */
-
-/* RAND_bytes writes |len| bytes of random data to |buf|. It returns one on
- * success, zero on failure. */
-int GFp_RAND_bytes(RAND *rng, uint8_t *buf, size_t len);
-
-
-#if defined(__cplusplus)
-}  /* extern C */
-#endif
-
-#endif  /* OPENSSL_HEADER_RAND_H */

commit 2a70e772efbd74b23aecaea76c53ea16bfcfa505

Fix build failure caused by d51e389673277acd6733de0b136d33e79967e4f9.

src/rand.rs
@@ -25,9 +25,7 @@
 //! (seccomp filters on Linux in particular). See `SystemRandom`'s
 //! documentation for more details.
 
-#[cfg(any(target_os = "linux",
-          windows,
-          test))]
+#[cfg(any(target_os = "linux", windows))]
 use c;
 
 use error;