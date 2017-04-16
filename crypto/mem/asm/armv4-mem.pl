daf320bf74b15fb670ef69e1db813b348257d254
Corey Richardson <corey@octayn.net>
2017-04-16 15:57:12 -0400

    mem: remove all the old, dead code

--- crypto/mem/asm/armv4-mem.pl
+++ crypto/mem/asm/armv4-mem.pl
@@ -134,103 +134,42 @@ $code.=<<___;
 #include "arm_arch.h"
 
 .text
-.arch	armv8-a+crypto
-
-.align	5
-.globl	_armv7_neon_probe
-.type	_armv7_neon_probe,%function
-_armv7_neon_probe:
-	orr	v15.16b, v15.16b, v15.16b
-	ret
-.size	_armv7_neon_probe,.-_armv7_neon_probe
-
-.globl	_armv7_tick
-.type	_armv7_tick,%function
-_armv7_tick:
-#ifdef	__APPLE__
-	mrs	x0, CNTPCT_EL0
+#if defined(__thumb2__) && !defined(__APPLE__)
+.syntax	unified
+.thumb
 #else
-	mrs	x0, CNTVCT_EL0
+.code	32
+#undef	__thumb2__
 #endif
-	ret
-.size	_armv7_tick,.-_armv7_tick
 
-.globl	_armv8_aes_probe
-.type	_armv8_aes_probe,%function
-_armv8_aes_probe:
-	aese	v0.16b, v0.16b
-	ret
-.size	_armv8_aes_probe,.-_armv8_aes_probe
-
-.globl	_armv8_sha1_probe
-.type	_armv8_sha1_probe,%function
-_armv8_sha1_probe:
-	sha1h	s0, s0
-	ret
-.size	_armv8_sha1_probe,.-_armv8_sha1_probe
-
-.globl	_armv8_sha256_probe
-.type	_armv8_sha256_probe,%function
-_armv8_sha256_probe:
-	sha256su0	v0.4s, v0.4s
-	ret
-.size	_armv8_sha256_probe,.-_armv8_sha256_probe
-.globl	_armv8_pmull_probe
-.type	_armv8_pmull_probe,%function
-_armv8_pmull_probe:
-	pmull	v0.1q, v0.1d, v0.1d
-	ret
-.size	_armv8_pmull_probe,.-_armv8_pmull_probe
-
-.globl	OPENSSL_cleanse
-.type	OPENSSL_cleanse,%function
-.align	5
-OPENSSL_cleanse:
-	cbz	x1,.Lret	// len==0?
-	cmp	x1,#15
-	b.hi	.Lot		// len>15
-	nop
-.Little:
-	strb	wzr,[x0],#1	// store byte-by-byte
-	subs	x1,x1,#1
-	b.ne	.Little
-.Lret:	ret
-
-.align	4
-.Lot:	tst	x0,#7
-	b.eq	.Laligned	// inp is aligned
-	strb	wzr,[x0],#1	// store byte-by-byte
-	sub	x1,x1,#1
-	b	.Lot
-
-.align	4
-.Laligned:
-	str	xzr,[x0],#8	// store word-by-word
-	sub	x1,x1,#8
-	tst	x1,#-8
-	b.ne	.Laligned	// len>=8
-	cbnz	x1,.Little	// len!=0?
-	ret
-.size	OPENSSL_cleanse,.-OPENSSL_cleanse
-
-.globl	CRYPTO_memcmp
+.global	CRYPTO_memcmp
 .type	CRYPTO_memcmp,%function
 .align	4
 CRYPTO_memcmp:
-	eor	w3,w3,w3
-	cbz	x2,.Lno_data	// len==0?
-.Loop_cmp:
-	ldrb	w4,[x0],#1
-	ldrb	w5,[x1],#1
-	eor	w4,w4,w5
-	orr	w3,w3,w4
-	subs	x2,x2,#1
-	b.ne	.Loop_cmp
+	eor	ip,ip,ip
+	cmp	r2,#0
+	beq	.Lno_data
+	stmdb	sp!,{r4,r5}
 
+.Loop_cmp:
+	ldrb	r4,[r0],#1
+	ldrb	r5,[r1],#1
+	eor	r4,r4,r5
+	orr	ip,ip,r4
+	subs	r2,r2,#1
+	bne	.Loop_cmp
+
+	ldmia	sp!,{r4,r5}
 .Lno_data:
-	neg	w0,w3
-	lsr	w0,w0,#31
-	ret
+	neg	r0,ip
+	mov	r0,r0,lsr#31
+#if __ARM_ARCH__>=5
+	bx	lr
+#else
+	tst	lr,#1
+	moveq	pc,lr
+	.word	0xe12fff1e	@ bx	lr
+#endif
 .size	CRYPTO_memcmp,.-CRYPTO_memcmp
 ___
 
