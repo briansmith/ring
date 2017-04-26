afd88c27f2fe9f8f1a6d5b287cc16b1bc8f06198
David Benjamin <davidben@google.com>
2017-04-26 11:02:43 -0400

    Allow embedders to replace gtest_main.cc.
    Chromium's test infrastruction does not actually support GTest. It
    requires a custom test runner in //base. Split gtest_main.cc up into a
    gtest_main.h which defines a support function we maintain and a default
    runner. Chromium's build will swap that file out for a custom one.
    
    BUG=129
    
    Change-Id: I3e39fe3a931b3051a61d5f8eef514ca6a504f11c
    Reviewed-on: https://boringssl-review.googlesource.com/15564
    Reviewed-by: Steven Valdez <svaldez@google.com>
    Commit-Queue: Steven Valdez <svaldez@google.com>
    CQ-Verified: CQ bot account: commit-bot@chromium.org <commit-bot@chromium.org>

--- crypto/test/gtest_main.cc
+++ crypto/test/gtest_main.cc
@@ -14,58 +14,11 @@
 
 #include <gtest/gtest.h>
 
-#include <stdio.h>
+#include "gtest_main.h"
 
-#include <openssl/err.h>
-#include <openssl/crypto.h>
-
-#if defined(OPENSSL_WINDOWS)
-OPENSSL_MSVC_PRAGMA(warning(push, 3))
-#include <winsock2.h>
-OPENSSL_MSVC_PRAGMA(warning(pop))
-#endif
-
-
-namespace {
-
-class ErrorTestEventListener : public testing::EmptyTestEventListener {
- public:
-  ErrorTestEventListener() {}
-  ~ErrorTestEventListener() override {}
-
-  void OnTestEnd(const testing::TestInfo &test_info) override {
-    // If the test failed, print any errors left in the error queue.
-    if (test_info.result()->Failed()) {
-      ERR_print_errors_fp(stdout);
-    }
-
-    // Clean up the error queue for the next run.
-    ERR_clear_error();
-  }
-};
-
-}  // namespace
 
 int main(int argc, char **argv) {
-  CRYPTO_library_init();
-
-#if defined(OPENSSL_WINDOWS)
-  // Initialize Winsock.
-  WORD wsa_version = MAKEWORD(2, 2);
-  WSADATA wsa_data;
-  int wsa_err = WSAStartup(wsa_version, &wsa_data);
-  if (wsa_err != 0) {
-    fprintf(stderr, "WSAStartup failed: %d\n", wsa_err);
-    return 1;
-  }
-  if (wsa_data.wVersion != wsa_version) {
-    fprintf(stderr, "Didn't get expected version: %x\n", wsa_data.wVersion);
-    return 1;
-  }
-#endif
-
   testing::InitGoogleTest(&argc, argv);
-  testing::UnitTest::GetInstance()->listeners().Append(
-      new ErrorTestEventListener);
+  bssl::SetupGoogleTest();
   return RUN_ALL_TESTS();
 }
