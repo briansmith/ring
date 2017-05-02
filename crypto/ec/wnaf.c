c0ae51dfe467a1f2b4fae58e6042207f0d8f4e0a
David Benjamin <davidben@google.com>
2017-05-02 18:02:47 -0400

    Fix URL links in comment
    (Imported from upstream's 54538204d870b97c751d13efeefa876bd792a44b.)
    
    Change-Id: If9967b67a74ab7dea175e97ea8bda195c3cd0478
    Reviewed-on: https://boringssl-review.googlesource.com/15835
    Reviewed-by: David Benjamin <davidben@google.com>
    Commit-Queue: David Benjamin <davidben@google.com>
    CQ-Verified: CQ bot account: commit-bot@chromium.org <commit-bot@chromium.org>

--- crypto/ec/wnaf.c
+++ crypto/ec/wnaf.c
@@ -78,9 +78,10 @@
 #include "../internal.h"
 
 
-/* This file implements the wNAF-based interleaving multi-exponentation method
- * (<URL:http://www.informatik.tu-darmstadt.de/TI/Mitarbeiter/moeller.html#multiexp>);
- * */
+/* This file implements the wNAF-based interleaving multi-exponentiation method
+ * at:
+ *   http://link.springer.com/chapter/10.1007%2F3-540-45537-X_13
+ *   http://www.bmoeller.de/pdf/TI-01-08.multiexp.pdf */
 
 /* Determine the modified width-(w+1) Non-Adjacent Form (wNAF) of 'scalar'.
  * This is an array  r[]  of values that are either zero or odd with an
