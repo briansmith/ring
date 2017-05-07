15bb419df7b6fc0db209c1df680cca060fd4c42c
Brian Smith <brian@briansmith.org>
2017-05-07 10:03:39 -1000

    Add note about the ~18 special Ed25519 keys.

--- src/ec/curve25519/ed25519.rs
+++ src/ec/curve25519/ed25519.rs
@@ -87,6 +87,8 @@ impl<'a> Ed25519KeyPair {
         let pair = try!(Self::from_seed_unchecked(seed));
 
         // This implicitly verifies that `public_key` is the right length.
+        // XXX: This rejects ~18 keys when they are partially reduced, though
+        // those keys are virtually impossible to find.
         if public_key != pair.public_key_bytes() {
             return Err(error::Unspecified);
         }
