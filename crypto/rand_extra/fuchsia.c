36d59479a504adc718b42d263eae4bf7a52e86c0
Aaron Green <aarongreen@google.com>
2017-09-15 11:30:29 -0700

    Update Fuchsia symbols that have been renamed
    Fuchsia needed to rename Magenta to Zircon.  Several syscalls and status
    codes changed as a result.
    
    Change-Id: I64b5ae4537ccfb0a318452fed34040a2e8f5012e
    Reviewed-on: https://boringssl-review.googlesource.com/20324
    Reviewed-by: Adam Langley <agl@google.com>
    Commit-Queue: Adam Langley <agl@google.com>
    CQ-Verified: CQ bot account: commit-bot@chromium.org <commit-bot@chromium.org>

--- crypto/rand_extra/fuchsia.c
+++ crypto/rand_extra/fuchsia.c
@@ -19,20 +19,20 @@
 #include <limits.h>
 #include <stdlib.h>
 
-#include <magenta/syscalls.h>
+#include <zircon/syscalls.h>
 
 #include "../fipsmodule/rand/internal.h"
 
 void CRYPTO_sysrand(uint8_t *out, size_t requested) {
   while (requested > 0) {
-    size_t output_bytes_this_pass = MX_CPRNG_DRAW_MAX_LEN;
+    size_t output_bytes_this_pass = ZX_CPRNG_DRAW_MAX_LEN;
     if (requested < output_bytes_this_pass) {
       output_bytes_this_pass = requested;
     }
     size_t bytes_drawn;
-    mx_status_t status =
-        mx_cprng_draw(out, output_bytes_this_pass, &bytes_drawn);
-    if (status != MX_OK) {
+    zx_status_t status =
+        zx_cprng_draw(out, output_bytes_this_pass, &bytes_drawn);
+    if (status != ZX_OK) {
       abort();
     }
     requested -= bytes_drawn;
