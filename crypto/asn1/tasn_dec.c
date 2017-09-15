288ca7dcb44a177a1f3f7fa5be4f581edf1da668
David Benjamin <davidben@google.com>
2017-09-15 18:26:59 -0400

    Remove ASN1_template_(i2d,d2i).
    Thes are remnants of some old setup.
    
    Change-Id: I09151fda9419fbe7514f2f609f70284965694bfa
    Reviewed-on: https://boringssl-review.googlesource.com/20365
    Reviewed-by: Adam Langley <agl@google.com>
    Commit-Queue: David Benjamin <davidben@google.com>
    CQ-Verified: CQ bot account: commit-bot@chromium.org <commit-bot@chromium.org>

--- crypto/asn1/tasn_dec.c
+++ crypto/asn1/tasn_dec.c
@@ -147,15 +147,6 @@ ASN1_VALUE *ASN1_item_d2i(ASN1_VALUE **pval,
     return NULL;
 }
 
-int ASN1_template_d2i(ASN1_VALUE **pval,
-                      const unsigned char **in, long len,
-                      const ASN1_TEMPLATE *tt)
-{
-    ASN1_TLC c;
-    asn1_tlc_clear_nc(&c);
-    return asn1_template_ex_d2i(pval, in, len, tt, 0, &c);
-}
-
 /*
  * Decode an item, taking care of IMPLICIT tagging, if any. If 'opt' set and
  * tag mismatch return -1 to handle OPTIONAL
