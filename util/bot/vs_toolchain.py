24f5b18f93b88c8db553b1d2becae313c09c1419
David Benjamin <davidben@google.com>
2017-10-12 14:22:54 -0400

    Update copies of tools.
    d3868ac4d6f92e57376924a62e8d52f36d7a326561ec0bbd1d5681759a947134 sde-external-8.9.0-2017-08-06-lin.tar.bz2
    9852d57e0d6b4509accb4f9faf862327a79c18b630aac4f35c65ce7a270a9230 strawberry-perl-5.26.1.1-32bit-portable.zip
    
    I've left CMake alone for now because that involves building things and
    there's some mess between newer CMakes trying to mess with the Android
    NDK, so that needs to be tested out a bit.
    
    Change-Id: I6beea9d4774474ac62e843c8668fcef7f6a6fc04
    Reviewed-on: https://boringssl-review.googlesource.com/21824
    Commit-Queue: David Benjamin <davidben@google.com>
    CQ-Verified: CQ bot account: commit-bot@chromium.org <commit-bot@chromium.org>
    Reviewed-by: Steven Valdez <svaldez@google.com>

--- util/bot/vs_toolchain.py
+++ util/bot/vs_toolchain.py
@@ -87,8 +87,8 @@ def _GetDesiredVsToolchainHashes():
     # Update 3 final with 10.0.15063.468 SDK and no vctip.exe.
     return ['f53e4598951162bad6330f7a167486c7ae5db1e5']
   if env_version == '2017':
-    # VS 2017 Update 3.2 with 10.0.15063.468 SDK.
-    return ['9bc7ccbf9f4bd50d4a3bd185e8ca94ff1618de0b']
+    # VS 2017 Update 3.2 with 10.0.15063.468 SDK and patched setenv.cmd.
+    return ['a9e1098bba66d2acccc377d5ee81265910f29272']
   raise Exception('Unsupported VS version %s' % env_version)
 
 
