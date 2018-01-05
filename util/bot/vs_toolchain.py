9d1f96606cd7af3b8d441fbc5e8d2e69da37c506
David Benjamin <davidben@google.com>
2018-01-05 15:43:09 -0500

    Update tools.
    In particular, get the new NDK. Unfortunately, the new clang picks up
    an unfortunate change for clang-cl that we now must work around.
    
    http://llvm.org/viewvc/llvm-project?view=revision&revision=319116
    
    Bug: 109
    Change-Id: I091ca7160683e70cd79b5c2b7a4267fea258ec17
    Reviewed-on: https://boringssl-review.googlesource.com/24644
    Reviewed-by: Steven Valdez <svaldez@google.com>
    Commit-Queue: David Benjamin <davidben@google.com>
    CQ-Verified: CQ bot account: commit-bot@chromium.org <commit-bot@chromium.org>

--- util/bot/vs_toolchain.py
+++ util/bot/vs_toolchain.py
@@ -87,8 +87,9 @@ def _GetDesiredVsToolchainHashes():
     # Update 3 final with 10.0.15063.468 SDK and no vctip.exe.
     return ['f53e4598951162bad6330f7a167486c7ae5db1e5']
   if env_version == '2017':
-    # VS 2017 Update 3.2 with 10.0.15063.468 SDK and patched setenv.cmd.
-    return ['a9e1098bba66d2acccc377d5ee81265910f29272']
+    # VS 2017 Update 3.2 with 10.0.15063.468 SDK, patched setenv.cmd, and
+    # 10.0.16299.15 debuggers.
+    return ['1180cb75833ea365097e279efb2d5d7a42dee4b0']
   raise Exception('Unsupported VS version %s' % env_version)
 
 
