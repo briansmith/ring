3c92e80d7a9ff939f030f33053dac7ca9d9646b6
David Benjamin <davidben@google.com>
2018-01-09 21:44:28 +0000

    Revert "Update tools."
    This reverts commit 9d1f96606cd7af3b8d441fbc5e8d2e69da37c506.
    
    Reason for revert: aarch64 bots are breaking for some reason.
    
    Original change's description:
    > Update tools.
    > 
    > In particular, get the new NDK. Unfortunately, the new clang picks up
    > an unfortunate change for clang-cl that we now must work around.
    > 
    > http://llvm.org/viewvc/llvm-project?view=revision&revision=319116
    > 
    > Bug: 109
    > Change-Id: I091ca7160683e70cd79b5c2b7a4267fea258ec17
    > Reviewed-on: https://boringssl-review.googlesource.com/24644
    > Reviewed-by: Steven Valdez <svaldez@google.com>
    > Commit-Queue: David Benjamin <davidben@google.com>
    > CQ-Verified: CQ bot account: commit-bot@chromium.org <commit-bot@chromium.org>
    
    TBR=davidben@google.com,svaldez@google.com
    
    Change-Id: I98960f295987857c4e42c312059b6d5934bb5e43
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 109
    Reviewed-on: https://boringssl-review.googlesource.com/24747
    Reviewed-by: David Benjamin <davidben@google.com>
    Commit-Queue: David Benjamin <davidben@google.com>
    CQ-Verified: CQ bot account: commit-bot@chromium.org <commit-bot@chromium.org>

--- util/bot/update_clang.py
+++ util/bot/update_clang.py
@@ -19,7 +19,7 @@ import urllib2
 # CLANG_REVISION and CLANG_SUB_REVISION determine the build of clang
 # to use. These should be synced with tools/clang/scripts/update.py in
 # Chromium.
-CLANG_REVISION = '321529'
+CLANG_REVISION = '318667'
 CLANG_SUB_REVISION=1
 
 PACKAGE_VERSION = "%s-%s" % (CLANG_REVISION, CLANG_SUB_REVISION)
