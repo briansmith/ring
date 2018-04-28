briansmith/ring

commit eb898141209d3bde1c9e5f0b0d816fbe4d3548e1

Ensure that AES-NI is available when selecting GFp_aesni_gcm_encrypt.
This is intended to be equivalent to BoringSSL's
commit 1997ef22d7f6316a22de7b481668d27703ea4733.

crypto/fipsmodule/modes/gcm.c
@@ -399,7 +399,8 @@ static void gcm128_init_htable(u128 Htable[GCM128_HTABLE_LEN],
   gcm_init_4bit(Htable, H);
 }
 
-static void gcm128_init_gmult_ghash(GCM128_CONTEXT *ctx) {
+static void gcm128_init_gmult_ghash(GCM128_CONTEXT *ctx,
+                                    int is_aesni_encrypt) {
   /* Keep in sync with |gcm128_init_htable|. */
 
 #if defined(GHASH_ASM_X86_64) || defined(GHASH_ASM_X86)
@@ -408,8 +409,7 @@ static void gcm128_init_gmult_ghash(GCM128_CONTEXT *ctx) {
     if (((GFp_ia32cap_P[1] >> 22) & 0x41) == 0x41) { /* AVX+MOVBE */
       ctx->gmult = GFp_gcm_gmult_avx;
       ctx->ghash = GFp_gcm_ghash_avx;
-      ctx->use_aesni_gcm_crypt = 1;
-
+      ctx->use_aesni_gcm_crypt = is_aesni_encrypt ? 1 : 0;
       return;
     }
 #endif
@@ -467,7 +467,7 @@ void GFp_gcm128_init(GCM128_CONTEXT *ctx, const AES_KEY *key,
 
   memcpy(ctx->Htable, serialized_ctx, GCM128_SERIALIZED_LEN);
   ctx->block = block;
-  gcm128_init_gmult_ghash(ctx);
+  gcm128_init_gmult_ghash(ctx, GFp_aes_block_is_aesni_encrypt(block));
 }
 
 int GFp_gcm128_aad(GCM128_CONTEXT *ctx, const uint8_t *aad, size_t len) {

crypto/cipher/e_aes.c
@@ -182,11 +182,16 @@ static aes_set_key_f aes_set_key(void) {
   return GFp_AES_set_encrypt_key;
 }
 
+int GFp_aes_block_is_aesni_encrypt(aes_block_f block) {
+  return block == GFp_aesni_encrypt;
+}
+
 static aes_block_f aes_block(void) {
   /* Keep this in sync with |set_set_key| and |aes_ctr|. */
 
 #if defined(AESNI)
   if (aesni_capable()) {
+    // Keep in sync with GFp_aes_block_is_aesni_encrypt.
     return GFp_aesni_encrypt;
   }
 #endif
@@ -288,6 +293,7 @@ static int gfp_aes_gcm_init_and_aad(GCM128_CONTEXT *gcm, AES_KEY *ks,
                                     const uint8_t ad[], size_t ad_len) {
   assert(ad != NULL || ad_len == 0);
   memcpy(ks, ctx_buf, sizeof(*ks));
+
   GFp_gcm128_init(gcm, ks, aes_block(), (const uint8_t *)ctx_buf + sizeof(*ks),
                   nonce);
   if (ad_len > 0) {

crypto/fipsmodule/modes/internal.h
@@ -61,7 +61,7 @@ extern "C" {
 /* aes_block_f is a pointer to |AES_Encrypt| or a variant thereof. */
 typedef void (*aes_block_f)(const uint8_t in[16], uint8_t out[16],
                             const AES_KEY *key);
-
+int GFp_aes_block_is_aesni_encrypt(aes_block_f aes_block);
 
 /* GCM definitions */
 typedef struct { uint64_t hi,lo; } u128;

commit cbeb38e4e3bd47aa46482c449c239a938539349d

Fix non-x86_64 build breakage from eb898141209d3bde1c9e5f0b0d816fbe4d3548e1.

crypto/fipsmodule/modes/gcm.c
@@ -401,6 +401,8 @@ static void gcm128_init_htable(u128 Htable[GCM128_HTABLE_LEN],
 
 static void gcm128_init_gmult_ghash(GCM128_CONTEXT *ctx,
                                     int is_aesni_encrypt) {
+  (void)is_aesni_encrypt; // Unused
+
   /* Keep in sync with |gcm128_init_htable|. */
 
 #if defined(GHASH_ASM_X86_64) || defined(GHASH_ASM_X86)