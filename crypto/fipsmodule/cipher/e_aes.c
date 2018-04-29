briansmith/ring

commit 86ac17abcfc7f08adb19ec3c920670f452092eba

Fix more non-Intel build breakage from eb898141209d3bde1c9e5f0b0d816fbe4d3548e1.

crypto/fipsmodule/cipher/e_aes.c
@@ -174,11 +174,14 @@ static aes_set_key_f aes_set_key(void) {
   return GFp_AES_set_encrypt_key;
 }
 
-#if defined(AESNI)
+/* TODO(perf): Consider inlining this. */
 int GFp_aes_block_is_aesni_encrypt(aes_block_f block) {
+#if defined(AESNI)
   return block == GFp_aesni_encrypt;
-}
+#else
+  return 0;
 #endif
+}
 
 static aes_block_f aes_block(void) {
   /* Keep this in sync with |set_set_key| and |aes_ctr|. */

commit d2e9b2bd65fc23a9cf6a9aec1b54550da0106c03

Fix build breakage from 86ac17abcfc7f08adb19ec3c920670f452092eba.

crypto/fipsmodule/cipher/e_aes.c
@@ -179,6 +179,7 @@ int GFp_aes_block_is_aesni_encrypt(aes_block_f block) {
 #if defined(AESNI)
   return block == GFp_aesni_encrypt;
 #else
+  (void)block;
   return 0;
 #endif
 }