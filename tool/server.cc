3c37d0aba5850b10fe8aa74d20b185210a372810
David Benjamin <davidben@google.com>
2018-05-05 00:42:23 -0400

    Reland "Fix bssl client/server's error-handling."
    Rather than printing the SSL_ERROR_* constants, print the actual error.
    This should be a bit more understandable. Debugging this also uncovered
    some other issues on Windows:
    
    - We were mixing up C runtime and Winsock errors, which are separate in
      Windows.
    
    - The thread local implementation interferes with WSAGetLastError due to
      a quirk of TlsGetValue. This could affect other Windows consumers.
      (Chromium uses a custom BIO, so it isn't affected.)
    
    - SocketSetNonBlocking also interferes with WSAGetLastError.
    
    - Listen for FD_CLOSE along with FD_READ. Connection close does not
      signal FD_READ. (The select loop only barely works on Windows anyway
      due to issues with stdin and line buffering, but if we take stdin out
      of the equation, FD_CLOSE can be tested.)
    
    Change-Id: Ia8d42b5ac39ebb3045d410dd768f83a3bb88b2cb
    Reviewed-on: https://boringssl-review.googlesource.com/28186
    Reviewed-by: David Benjamin <davidben@google.com>
    Commit-Queue: Steven Valdez <svaldez@google.com>
    CQ-Verified: CQ bot account: commit-bot@chromium.org <commit-bot@chromium.org>

--- tool/server.cc
+++ tool/server.cc
@@ -185,8 +185,7 @@ static bool HandleWWW(SSL *ssl) {
         SSL_read(ssl, request + request_len, sizeof(request) - request_len);
     if (ssl_ret <= 0) {
       int ssl_err = SSL_get_error(ssl, ssl_ret);
-      fprintf(stderr, "Error while reading: %d\n", ssl_err);
-      ERR_print_errors_cb(PrintErrorCallback, stderr);
+      PrintSSLError(stderr, "Error while reading", ssl_err, ssl_ret);
       return false;
     }
     request_len += static_cast<size_t>(ssl_ret);
@@ -342,8 +341,7 @@ bool Server(const std::vector<std::string> &args) {
     int ret = SSL_accept(ssl.get());
     if (ret != 1) {
       int ssl_err = SSL_get_error(ssl.get(), ret);
-      fprintf(stderr, "Error while connecting: %d\n", ssl_err);
-      ERR_print_errors_cb(PrintErrorCallback, stderr);
+      PrintSSLError(stderr, "Error while connecting", ssl_err, ret);
       result = false;
       continue;
     }
