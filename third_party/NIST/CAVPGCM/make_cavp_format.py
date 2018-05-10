bacd670db503a64a9781c3c64e8acece07f2803c
brycx <brycx@protonmail.com>
2018-05-10 13:21:14 +0200

    Clean up make_cavp_format code

--- third_party/NIST/CAVPGCM/make_cavp_format.py
+++ third_party/NIST/CAVPGCM/make_cavp_format.py
@@ -1,32 +1,40 @@
-files_en = [
+cavp_encryption = [
     'gcmEncryptExtIV128.rsp',
     'gcmEncryptExtIV256.rsp',
 ]
 
-replacements_en = {
+cavp_decryption = [
+    'gcmDecrypt128.rsp',
+    'gcmDecrypt256.rsp',
+]
+
+keyword_replacements = {
     'Key' : 'KEY',
     'IV' : 'NONCE',
     'PT' : 'IN',
     'AAD' : 'AD',
     'Tag' : 'TAG',
+    'FAIL' : 'FAILS = FAILS_TO_DECRYPT',
 }
 
 lines = []
-lines_de = []
 
-for file in files_en:
+for file in cavp_encryption:
     with open('third_party/NIST/CAVPGCM/' + file) as infile:
         for line in infile:
-            for src, target in replacements_en.items():
+            for src, target in keyword_replacements.items():
                 line = line.replace(src, target)
             if line.endswith("= \n"):
                 line = line.replace(' = ', ' = ""')
+
             # A TAG of length 128-bits will be of length 32 in hex
-            # 'TAG = ' gives 6 in length
-            # 'NONCE = ' gives 8 in length
-            # The remaining 1 in lenght is a newline
-            # TAG = 39, NONCE = 33
-            if not line.startswith("TAG =") or (line.startswith("TAG =") and (len(line) == 39)):
+            # 'TAG = ' is 6 in length
+            # 'NONCE = ' is 8 in length
+            # The remaining 1 in length is a newline
+            # len(TAG) = 39, len(NONCE) = 33
+
+            # Check TAG length
+            if not line.startswith("TAG =") or (len(line) == 39):
                 if not line.startswith("Count = "):
                     if not line.startswith("["):
                         lines.append(line)
@@ -34,7 +42,7 @@ for file in files_en:
                         # Check if the last NONCE, in pos -5, was of valid length
                         lines.append('FAILS = WRONG_NONCE_LENGTH\n')
             else:
-                # If the TAG has invalid size, we trim the previous test case
+                # If the TAG has invalid size, we trim the test case
                 # elements which includes KEY, NONCE, etc
                 for x in range(1,7):
                     lines.pop()
@@ -46,71 +54,57 @@ for file in files_en:
     # Empty list
     lines[:] = []
 
-
-files_de = [
-    'gcmDecrypt128.rsp',
-    'gcmDecrypt256.rsp',
-]
-
-replacements_de = {
-    'Key' : 'KEY',
-    'IV' : 'NONCE',
-    'PT' : 'IN',
-    'AAD' : 'AD',
-    'Tag' : 'TAG',
-    'FAIL' : 'FAILS = FAILS_TO_DECRYPT',
-}
-
-
-for file in files_de:
+for file in cavp_decryption:
     with open('third_party/NIST/CAVPGCM/' + file) as infile:
         for line in infile:
-            for src, target in replacements_de.items():
+            for src, target in keyword_replacements.items():
                 line = line.replace(src, target)
             if line.endswith("= \n"):
                 line = line.replace(' = ', ' = ""')
-            # A TAG of length 128-bits will be of length 32 in hex
-            # 'TAG = ' gives 6 in length
-            # 'NONCE = ' gives 8 in length
-            # The remaining 1 in lenght is a newline
-            # TAG = 39, NONCE = 33
 
-            # If tag is 39 in length proceed
+            # A TAG of length 128-bits will be of length 32 in hex
+            # 'TAG = ' is 6 in length
+            # 'NONCE = ' is 8 in length
+            # The remaining 1 in length is a newline
+            # len(TAG) = 39, len(NONCE) = 33
             if not line.startswith("Count = "):
-                # 7a0c8b2ce3881518a9fd148a
-                # bb8f8fd33bb70610de13df35
                 if not line.startswith("["):
                     if line.startswith("FAILS = FAILS_TO_DECRYPT"):
-                        # Append IN, ring needs this paramter
-                        #lines_de.append('IN = ""\n')
-                        # -5 because we added the IN element
-                        if (len(lines_de[-1]) != 39):
-                            # If TAG is not valid len
+                        # Check TAG length
+                        if (len(lines[-1]) != 39):
+                            # If not valid, remove whole test case
                             for x in range(1,7):
-                                lines_de.pop()
+                                lines.pop()
                             continue
+                        # Check NONCE length
+                        if len(lines[-4]) != 33:
+                            # *ring* does not accept invalid NONCE's so the
+                            # FAILS_TO_DECRYPT can't be run at all, which is why
+                            # we replace this with WRONG_NONCE_LENGTH
+                            line = line.replace('FAILS = FAILS_TO_DECRYPT',
+                                                'FAILS = WRONG_NONCE_LENGTH')
 
-                        if len(lines_de[-4]) != 33:
-                            line = line.replace('FAILS = FAILS_TO_DECRYPT', 'FAILS = WRONG_NONCE_LENGTH')
-
-                        lines_de.append('IN = ""\n')
+                        # Add the IN paramter, since this is not an optional
+                        # argument for *ring*
+                        lines.append('IN = ""\n')
 
                     if line.startswith("IN ="):
-                        if (len(lines_de[-1]) != 39):
-                            # If TAG is not valid len
+                        # Check TAG length
+                        if (len(lines[-1]) != 39):
+                            # If not valid, remove whole test case
                             for x in range(1,7):
-                                lines_de.pop()
+                                lines.pop()
                             continue
-
-                        if (len(lines_de[-4]) != 33):
+                        # Check NONCE length
+                        if (len(lines[-4]) != 33):
                             # If NONCE is not valid len
-                            lines_de.append('FAILS = WRONG_NONCE_LENGTH\n')
+                            lines.append('FAILS = WRONG_NONCE_LENGTH\n')
 
-                    lines_de.append(line)
+                    lines.append(line)
 
     with open('third_party/NIST/CAVPGCM/' + file + '_ring.rsp', 'w') as outfile:
-        for line in lines_de:
+        for line in lines:
             outfile.write(line)
 
     # Empty list
-    lines_de[:] = []
+    lines[:] = []
