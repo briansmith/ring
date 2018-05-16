de5783ab5a8bfa44937cd72970c3ae5b3639cfcd
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-05-16 11:18:15 +0200

    Use clang on travis with Android

--- mk/update-travis-yml.py
+++ mk/update-travis-yml.py
@@ -53,7 +53,7 @@ osx_compilers = [
 
 compilers = {
     "aarch64-unknown-linux-gnu" : [ "aarch64-linux-gnu-gcc" ],
-    "armv7-linux-androideabi" : [ "arm-linux-androideabi-gcc" ],
+    "armv7-linux-androideabi" : [ "arm-linux-androideabi-clang" ],
     "arm-unknown-linux-gnueabihf" : [ "arm-linux-gnueabihf-gcc" ],
     "i686-unknown-linux-gnu" : linux_compilers,
     "x86_64-unknown-linux-gnu" : linux_compilers,
