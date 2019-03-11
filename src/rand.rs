569bb6ed6a6eceaea296a8ca9f6f77c3f2a66047
Brian Smith <brian@briansmith.org>
2019-03-11 11:17:12 -1000

    Fix Darwin builds.

--- src/rand.rs
+++ src/rand.rs
@@ -285,7 +285,7 @@ mod darwin {
 
     #[link(name = "Security", kind = "framework")]
     extern "C" {
-        static kSecRandomDefault: &SecRandomRef;
+        static kSecRandomDefault: &'static SecRandomRef;
 
         // For now `rnd` must be `kSecRandomDefault`.
         #[must_use]
