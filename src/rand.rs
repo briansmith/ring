7633902d7ca2a37be0c3e33a2c6116473b1dec56
Brian Smith <brian@briansmith.org>
2019-06-13 09:59:56 -1000

    Move `ring::rand` tests to tests/rand_tests.rs.

--- src/rand.rs
+++ src/rand.rs
@@ -306,31 +306,3 @@ mod fuchsia {
         fn zx_cprng_draw(buffer: *mut u8, length: usize);
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::rand::{self, SecureRandom};
-    use std::vec;
-
-    #[test]
-    fn test_system_random_lengths() {
-        // Test that `fill` succeeds for various interesting lengths. `256` and
-        // multiples thereof are interesting because that's an edge case for
-        // `getrandom` on Linux.
-        let lengths = [0, 1, 2, 3, 96, 255, 256, 257, 511, 512, 513, 4096];
-
-        for len in lengths.iter() {
-            let mut buf = vec![0; *len];
-
-            let rng = rand::SystemRandom::new();
-            assert!(rng.fill(&mut buf).is_ok());
-
-            // If `len` < 96 then there's a big chance of false positives, but
-            // otherwise the likelihood of a false positive is so too low to
-            // worry about.
-            if *len >= 96 {
-                assert!(buf.iter().any(|x| *x != 0));
-            }
-        }
-    }
-}
