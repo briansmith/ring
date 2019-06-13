d16b5cbcb5073f34c46fbc3ed0a3265221e4c421
Joe Richey <joerichey@google.com>
2019-06-13 13:51:37 -0700

    Use libc::SYS_getrandom
    This has been in libc for a while, so we don't need to increment the
    libc version in Cargo.toml. Note that we cannot use the `getrandom()`
    function from libc, as it isn't avalible on Android.

--- src/rand.rs
+++ src/rand.rs
@@ -115,26 +115,11 @@ use crate::sealed;
 
 #[cfg(any(target_os = "android", target_os = "linux"))]
 mod sysrand_chunk {
-    use crate::{c, error};
+    use crate::error;
 
     #[inline]
     pub fn chunk(dest: &mut [u8]) -> Result<usize, error::Unspecified> {
-        // See `SYS_getrandom` in #include <sys/syscall.h>.
-
-        #[cfg(target_arch = "aarch64")]
-        const SYS_GETRANDOM: c::long = 278;
-
-        #[cfg(target_arch = "arm")]
-        const SYS_GETRANDOM: c::long = 384;
-
-        #[cfg(target_arch = "x86")]
-        const SYS_GETRANDOM: c::long = 355;
-
-        #[cfg(target_arch = "x86_64")]
-        const SYS_GETRANDOM: c::long = 318;
-
-        let chunk_len: c::size_t = dest.len();
-        let r = unsafe { libc::syscall(SYS_GETRANDOM, dest.as_mut_ptr(), chunk_len, 0) };
+        let r = unsafe { libc::syscall(libc::SYS_getrandom, dest.as_mut_ptr(), dest.len(), 0) };
         if r < 0 {
             let errno;
 
