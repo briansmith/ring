09050cb498336655883157c6e6055db9e5542857
Adam Langley <agl@google.com>
2019-07-10 11:12:24 -0700

    Add SipHash-2-4.
    The added code is a one-shot function. A handful of instructions could
    be saved by having a context object for repeated use of the same key,
    but perhaps it's not needed.
    
    Selected the 2-4 variant to implement because it seems to be
    overwhelmingly the most commonly used.
    
    Change-Id: I1e4f699f7dd5a2d35e12245fa116bafbd3439979
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/36664
    Commit-Queue: Adam Langley <agl@google.com>
    Reviewed-by: David Benjamin <davidben@google.com>

--- sources.cmake
+++ sources.cmake
@@ -54,6 +54,7 @@ set(
   crypto/fipsmodule/rand/ctrdrbg_vectors.txt
   crypto/hmac_extra/hmac_tests.txt
   crypto/poly1305/poly1305_tests.txt
+  crypto/siphash/siphash_tests.txt
   crypto/x509/many_constraints.pem
   crypto/x509/many_names1.pem
   crypto/x509/many_names2.pem
