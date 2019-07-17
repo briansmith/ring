briansmith/ring

commit 6ba0209b1d9fc4e216d94ec54a99c0ac9f6f7680

Stop using `GFp_x25519_NEON` on iOS to match BoringSSL.

src/ec/curve25519/x25519.rs
@@ -59,15 +59,12 @@ fn x25519_public_from_private(
 ) -> Result<(), error::Unspecified> {
     let public_out = public_out.try_into()?;
 
-    #[cfg(target_arch = "arm")]
-    let cpu_features = private_key.cpu_features;
-
     let private_key: &[u8; SCALAR_LEN] = private_key.bytes_less_safe().try_into()?;
     let private_key = ops::MaskedScalar::from_bytes_masked(*private_key);
 
-    #[cfg(target_arch = "arm")]
+    #[cfg(all(not(target_os = "ios"), target_arch = "arm"))]
     {
-        if cpu::arm::NEON.available(cpu_features) {
+        if cpu::arm::NEON.available(private_key.cpu_features) {
             static MONTGOMERY_BASE_POINT: [u8; 32] = [
                 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                 0, 0, 0, 0,
@@ -100,14 +97,17 @@ fn x25519_ecdh(
     let my_private_key = ops::MaskedScalar::from_bytes_masked(*my_private_key);
     let peer_public_key: &[u8; PUBLIC_KEY_LEN] = peer_public_key.as_slice_less_safe().try_into()?;
 
-    #[cfg_attr(not(target_arch = "arm"), allow(unused_variables))]
+    #[cfg_attr(
+        not(all(not(target_os = "ios"), target_arch = "arm")),
+        allow(unused_variables)
+    )]
     fn scalar_mult(
         out: &mut ops::EncodedPoint,
         scalar: &ops::MaskedScalar,
         point: &ops::EncodedPoint,
         cpu_features: cpu::Features,
     ) {
-        #[cfg(target_arch = "arm")]
+        #[cfg(all(not(target_os = "ios"), target_arch = "arm"))]
         {
             if cpu::arm::NEON.available(cpu_features) {
                 return x25519_neon(out, scalar, point);
@@ -142,7 +142,7 @@ fn x25519_ecdh(
     Ok(())
 }
 
-#[cfg(target_arch = "arm")]
+#[cfg(all(not(target_os = "ios"), target_arch = "arm"))]
 fn x25519_neon(out: &mut ops::EncodedPoint, scalar: &ops::MaskedScalar, point: &ops::EncodedPoint) {
     extern "C" {
         fn GFp_x25519_NEON(

crypto/curve25519/asm/x25519-asm-arm.S
@@ -25,22 +25,19 @@
 
 #if !defined(OPENSSL_NO_ASM) && defined(__arm__) && !defined(__APPLE__)
 
-#if !defined(__APPLE__)
-.fpu neon
+#if defined(BORINGSSL_PREFIX)
+#include <boringssl_prefix_symbols_asm.h>
 #endif
+
+.fpu neon
 .text
 .align 4
 
 .global GFp_x25519_NEON
 
-#if defined(__APPLE__)
-.private_extern _GFp_x25519_NEON
-_GFp_x25519_NEON:
-#else
 .hidden GFp_x25519_NEON
 .type GFp_x25519_NEON, %function
 GFp_x25519_NEON:
-#endif
 
 vpush {q4,q5,q6,q7}
 mov r12,sp
@@ -56,8 +53,8 @@ mov r0,r0
 mov r1,r1
 mov r2,r2
 add r3,sp,#32
-mov r4, #0
-mov r5, #254
+ldr r4,=0
+ldr r5,=254
 vmov.i32 q0,#1
 vshr.u64 q1,q0,#7
 vshr.u64 q0,q0,#8
@@ -75,7 +72,7 @@ vst1.8 {d4-d5},[r6,: 128]!
 vst1.8 {d4-d5},[r6,: 128]!
 vst1.8 d4,[r6,: 64]
 add r6,r3,#0
-mov r7, #960
+ldr r7,=960
 sub r7,r7,#2
 neg r7,r7
 sub r7,r7,r7,LSL #7

commit bd41a21e45f2b29ea5d30080e82c7ae4d3f4adc9

Fix non-iOS ARM builds.
Commit 6ba0209b1d9fc4e216d94ec54a99c0ac9f6f7680 broke them.

src/ec/curve25519/x25519.rs
@@ -59,12 +59,15 @@ fn x25519_public_from_private(
 ) -> Result<(), error::Unspecified> {
     let public_out = public_out.try_into()?;
 
+    #[cfg(target_arch = "arm")]
+    let cpu_features = private_key.cpu_features;
+
     let private_key: &[u8; SCALAR_LEN] = private_key.bytes_less_safe().try_into()?;
     let private_key = ops::MaskedScalar::from_bytes_masked(*private_key);
 
     #[cfg(all(not(target_os = "ios"), target_arch = "arm"))]
     {
-        if cpu::arm::NEON.available(private_key.cpu_features) {
+        if cpu::arm::NEON.available(cpu_features) {
             static MONTGOMERY_BASE_POINT: [u8; 32] = [
                 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                 0, 0, 0, 0,