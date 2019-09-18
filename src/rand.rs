0a181c7bb55ce44cf86307cae022f45526885b22
Nick Hynes <nhynes@oasislabs.com>
2019-09-18 06:52:55 +0000

    Support WASI

--- src/rand.rs
+++ src/rand.rs
@@ -270,6 +270,17 @@ mod sysrand_chunk {
     }
 }
 
+#[cfg(all(target_arch = "wasm32", target_os = "wasi",))]
+mod sysrand_chunk {
+    use crate::error;
+
+    #[inline]
+    pub fn chunk(dest: &mut [u8]) -> Result<usize, error::Unspecified> {
+        wasi::wasi_unstable::random_get(dest).map_err(|_| error::Unspecified)?;
+        Ok(dest.len())
+    }
+}
+
 #[cfg(windows)]
 mod sysrand_chunk {
     use crate::{error, polyfill};
