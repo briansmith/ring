0ac6ca6cbcd7e3bbe44d7d6ba04913546c13bf3e
Brian Smith <brian@briansmith.org>
2020-01-02 15:42:19 -0600

    Switch `dev_urandom_fallback` to use `once_cell` instead of `lazy_static!`.
    This is a step towards removing the spin-rs dependency. `lazy_static!`'s
    conditial use of spin-rs based on a feature flag was particularly problematic
    because often other dependencies would enable that feature flag even in cases
    where the `std::sync::Once` implementation was preferable.

--- src/rand.rs
+++ src/rand.rs
@@ -329,18 +329,15 @@ mod sysrand_or_urandom {
 
     #[inline]
     pub fn fill(dest: &mut [u8]) -> Result<(), error::Unspecified> {
-        use lazy_static::lazy_static;
-
-        lazy_static! {
-            static ref MECHANISM: Mechanism = {
-                let mut dummy = [0u8; 1];
-                if super::sysrand_chunk::chunk(&mut dummy[..]).is_err() {
-                    Mechanism::DevURandom
-                } else {
-                    Mechanism::Sysrand
-                }
-            };
-        }
+        use once_cell::sync::Lazy;
+        static MECHANISM: Lazy<Mechanism> = Lazy::new(|| {
+            let mut dummy = [0u8; 1];
+            if super::sysrand_chunk::chunk(&mut dummy[..]).is_err() {
+                Mechanism::DevURandom
+            } else {
+                Mechanism::Sysrand
+            }
+        });
 
         match *MECHANISM {
             Mechanism::Sysrand => super::sysrand::fill(dest),
@@ -366,12 +363,10 @@ mod urandom {
     pub fn fill(dest: &mut [u8]) -> Result<(), error::Unspecified> {
         extern crate std;
 
-        use lazy_static::lazy_static;
+        use once_cell::sync::Lazy;
 
-        lazy_static! {
-            static ref FILE: Result<std::fs::File, std::io::Error> =
-                std::fs::File::open("/dev/urandom");
-        }
+        static FILE: Lazy<Result<std::fs::File, std::io::Error>> =
+            Lazy::new(|| std::fs::File::open("/dev/urandom"));
 
         match *FILE {
             Ok(ref file) => {
