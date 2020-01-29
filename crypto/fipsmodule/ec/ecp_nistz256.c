c6a0dbd1ea724fb47ef3323b74038adff1faf348
Wang Xuerui <git@xen0n.name>
2020-01-29 17:30:45 +0800

    nistz256: add generic GFp_nistz256_add and GFp_nistz256_point_double

--- crypto/fipsmodule/ec/ecp_nistz256.c
+++ crypto/fipsmodule/ec/ecp_nistz256.c
@@ -200,6 +200,143 @@ void GFp_nistz256_point_add(P256_POINT *r, const P256_POINT *a, const P256_POINT
   limbs_copy(r->Y, res_y, P256_LIMBS);
   limbs_copy(r->Z, res_z, P256_LIMBS);
 }
+
+/* Include more reference implementation code for arches lacking assembly
+ * optimizations */
+#ifdef OPENSSL_MIPS64
+
+static inline void elem_add(Limb r[P256_LIMBS], const Limb a[P256_LIMBS],
+                            const Limb b[P256_LIMBS]) {
+  LIMBS_add_mod(r, a, b, Q, P256_LIMBS);
+}
+
+static inline void elem_mul_by_3(Limb r[P256_LIMBS], const Limb a[P256_LIMBS]) {
+  Limb tmp[P256_LIMBS];
+  elem_mul_by_2(tmp, a);
+  LIMBS_add_mod(r, tmp, a, Q, P256_LIMBS);
+}
+
+static inline void elem_div_by_2(Limb r[P256_LIMBS], const Limb a[P256_LIMBS]) {
+  /* Consider the case where `a` is even. Then we can shift `a` right one bit
+   * and the result will still be valid because we didn't lose any bits and so
+   * `(a >> 1) * 2 == a (mod q)`, which is the invariant we must satisfy.
+   *
+   * The remainder of this comment is considering the case where `a` is odd.
+   *
+   * Since `a` is odd, it isn't the case that `(a >> 1) * 2 == a (mod q)`
+   * because the lowest bit is lost during the shift. For example, consider:
+   *
+   * ```python
+   * q = 2**384 - 2**128 - 2**96 + 2**32 - 1
+   * a = 2**383
+   * two_a = a * 2 % q
+   * assert two_a == 0x100000000ffffffffffffffff00000001
+   * ```
+   *
+   * Notice there how `(2 * a) % q` wrapped around to a smaller odd value. When
+   * we divide `two_a` by two (mod q), we need to get the value `2**383`, which
+   * we obviously can't get with just a right shift.
+   *
+   * `q` is odd, and `a` is odd, so `a + q` is even. We could calculate
+   * `(a + q) >> 1` and then reduce it mod `q`. However, then we would have to
+   * keep track of an extra most significant bit. We can avoid that by instead
+   * calculating `(a >> 1) + ((q + 1) >> 1)`. The `1` in `q + 1` is the least
+   * significant bit of `a`. `q + 1` is even, which means it can be shifted
+   * without losing any bits. Since `q` is odd, `q - 1` is even, so the largest
+   * odd field element is `q - 2`. Thus we know that `a <= q - 2`. We know
+   * `(q + 1) >> 1` is `(q + 1) / 2` since (`q + 1`) is even. The value of
+   * `a >> 1` is `(a - 1)/2` since the shift will drop the least significant
+   * bit of `a`, which is 1. Thus:
+   *
+   * sum  =  ((q + 1) >> 1) + (a >> 1)
+   * sum  =  (q + 1)/2 + (a >> 1)       (substituting (q + 1)/2)
+   *     <=  (q + 1)/2 + (q - 2 - 1)/2  (substituting a <= q - 2)
+   *     <=  (q + 1)/2 + (q - 3)/2      (simplifying)
+   *     <=  (q + 1 + q - 3)/2          (factoring out the common divisor)
+   *     <=  (2q - 2)/2                 (simplifying)
+   *     <=  q - 1                      (simplifying)
+   *
+   * Thus, no reduction of the sum mod `q` is necessary. */
+
+  Limb is_odd = constant_time_is_nonzero_w(a[0] & 1);
+
+  /* r = a >> 1. */
+  Limb carry = a[P256_LIMBS - 1] & 1;
+  r[P256_LIMBS - 1] = a[P256_LIMBS - 1] >> 1;
+  for (size_t i = 1; i < P256_LIMBS; ++i) {
+    Limb new_carry = a[P256_LIMBS - i - 1];
+    r[P256_LIMBS - i - 1] =
+        (a[P256_LIMBS - i - 1] >> 1) | (carry << (LIMB_BITS - 1));
+    carry = new_carry;
+  }
+
+  static const Limb Q_PLUS_1_SHR_1[P256_LIMBS] = {
+    TOBN(0x00000000, 0x00000000), TOBN(0x00000000, 0x80000000),
+    TOBN(0x80000000, 0x00000000), TOBN(0x7fffffff, 0x80000000),
+  };
+
+  Limb adjusted[P256_LIMBS];
+  BN_ULONG carry2 = limbs_add(adjusted, r, Q_PLUS_1_SHR_1, P256_LIMBS);
+#if defined(NDEBUG)
+  (void)carry2;
+#endif
+  ASSERT(carry2 == 0);
+
+  copy_conditional(r, adjusted, is_odd);
+}
+
+void GFp_nistz256_add(Limb r[P256_LIMBS], const Limb a[P256_LIMBS],
+                      const Limb b[P256_LIMBS])
+{
+  elem_add(r, a, b);
+}
+
+/* Point double: r = 2*a */
+void GFp_nistz256_point_double(P256_POINT *r, const P256_POINT *a)
+{
+  BN_ULONG S[P256_LIMBS];
+  BN_ULONG M[P256_LIMBS];
+  BN_ULONG Zsqr[P256_LIMBS];
+  BN_ULONG tmp0[P256_LIMBS];
+
+  const BN_ULONG *in_x = a->X;
+  const BN_ULONG *in_y = a->Y;
+  const BN_ULONG *in_z = a->Z;
+
+  BN_ULONG *res_x = r->X;
+  BN_ULONG *res_y = r->Y;
+  BN_ULONG *res_z = r->Z;
+
+  elem_mul_by_2(S, in_y);
+
+  elem_sqr_mont(Zsqr, in_z);
+
+  elem_sqr_mont(S, S);
+
+  elem_mul_mont(res_z, in_z, in_y);
+  elem_mul_by_2(res_z, res_z);
+
+  elem_add(M, in_x, Zsqr);
+  elem_sub(Zsqr, in_x, Zsqr);
+
+  elem_sqr_mont(res_y, S);
+  elem_div_by_2(res_y, res_y);
+
+  elem_mul_mont(M, M, Zsqr);
+  elem_mul_by_3(M, M);
+
+  elem_mul_mont(S, S, in_x);
+  elem_mul_by_2(tmp0, S);
+
+  elem_sqr_mont(res_x, M);
+
+  elem_sub(res_x, res_x, tmp0);
+  elem_sub(S, S, res_x);
+
+  elem_mul_mont(S, S, M);
+  elem_sub(res_y, S, res_y);
+}
+#endif
 #endif
 
 /* r = p * p_scalar */
