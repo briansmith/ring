d085defcabaf2cc7a4e75c41313c4750a7344061
Brian Smith <brian@briansmith.org>
2020-05-29 22:05:09 -0500

    Remove unused `LIMBS_copy`.

--- crypto/limbs/limbs.h
+++ crypto/limbs/limbs.h
@@ -27,7 +27,6 @@ typedef crypto_word Limb;
 
 Limb LIMBS_are_zero(const Limb a[], size_t num_limbs);
 Limb LIMBS_are_even(const Limb a[], size_t num_limbs);
-void LIMBS_copy(Limb r[], const Limb a[], size_t num_limbs);
 Limb LIMBS_equal(const Limb a[], const Limb b[], size_t num_limbs);
 Limb LIMBS_equal_limb(const Limb a[], Limb b, size_t num_limbs);
 void LIMBS_reduce_once(Limb r[], const Limb m[], size_t num_limbs);
