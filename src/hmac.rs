1edcb759e5a68231805590c8227c89bd7a166542
Brian Smith <brian@briansmith.org>
2020-11-11 12:36:51 -0800

    Tests: Take Clippy's about unused mut.

--- src/hmac.rs
+++ src/hmac.rs
@@ -363,7 +363,7 @@ mod tests {
     // completely wacky.
     #[test]
     pub fn hmac_signing_key_coverage() {
-        let mut rng = rand::SystemRandom::new();
+        let rng = rand::SystemRandom::new();
 
         const HELLO_WORLD_GOOD: &[u8] = b"hello, world";
         const HELLO_WORLD_BAD: &[u8] = b"hello, worle";
@@ -374,7 +374,7 @@ mod tests {
             hmac::HMAC_SHA384,
             hmac::HMAC_SHA512,
         ] {
-            let key = hmac::Key::generate(*algorithm, &mut rng).unwrap();
+            let key = hmac::Key::generate(*algorithm, &rng).unwrap();
             let tag = hmac::sign(&key, HELLO_WORLD_GOOD);
             assert!(hmac::verify(&key, HELLO_WORLD_GOOD, tag.as_ref()).is_ok());
             assert!(hmac::verify(&key, HELLO_WORLD_BAD, tag.as_ref()).is_err())
