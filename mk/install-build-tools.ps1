15c823a571b2d51c06b00a7a600e6d458513de34
Brian Smith <brian@briansmith.org>
2020-11-24 16:11:07 -0800

    Build: Replace use of Yasm with use of Nasm for Windows assembly.
    Match BoringSSL. According to the Chromium discussion about nasm, it
    is also substantially faster.

--- mk/install-build-tools.ps1
+++ mk/install-build-tools.ps1
@@ -1,30 +1,67 @@
-function Download-File {
+function Verify-Or-Delete-File {
     param (
         [Parameter(Mandatory)]
-        [string]$Url,
+        [string]$File,
         [Parameter(Mandatory)]
-        [string]$ExpectedDigest,
+        [string]$ExpectedDigest
+    )
+    $ActualDigest = ( Get-FileHash -Algorithm SHA256 $File ).Hash
+    if ( $ActualDigest -eq $ExpectedDigest )
+    {
+        return
+    }
+    rm $File
+    echo "Digest verification failed for $Url; actual $ActualDigest, expected $ExpectedDigest"
+    exit 1
+}
+
+function Download-Zip-and-Extract-File {
+    param (
+        [Parameter(Mandatory)]
+        [string]$Uri,
+        [Parameter(Mandatory)]
+        [string]$ZipExpectedDigest,
+        [Parameter(Mandatory)]
+        [string]$PathWithinZip,
+        [Parameter(Mandatory)]
+        [string]$FileExpectedDigest,
         [Parameter(Mandatory)]
         [string]$OutFile
     )
-    $TmpFile = New-TemporaryFile
-    Invoke-WebRequest -Uri $Url -OutFile $TmpFile.FullName
-    $ActualDigest = ( Get-FileHash -Algorithm SHA256 $TmpFile ).Hash
-    if ( $ActualDigest -eq $ExpectedDigest )
-    {
-        Move-Item -Force $TmpFile $OutFile
-        return
-    }
+    $TmpZip = New-TemporaryFile
+    Invoke-WebRequest -Uri $Uri -OutFile $TmpZip.FullName
+    echo $TmpZip
+    Verify-Or-Delete-File -File $TmpZip.FullName -ExpectedDigest $ZipExpectedDigest
 
-    echo "Digest verification failed for $Url; actual $ActualDigest, expected $ExpectedDigest"
-    rm $TmpFile
-    exit 1
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    $zip = [System.IO.Compression.ZipFile]::OpenRead($TmpZip)
+    $zip.Entries |
+        Where-Object { $_.FullName -eq $PathWithinZip } |
+        ForEach-Object {
+            $TmpFile = New-TemporaryFile
+            # extract the selected items from the ZIP archive
+            # and copy them to the out folder
+            $FileName = $_.Name
+            [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$TmpFile", $true)
+            Verify-Or-Delete-File -File $TmpFile -ExpectedDigest $FileExpectedDigest
+            Move-Item -Force $TmpFile $OutFile
+        }
+    $zip.Dispose()
 }
 
 $tools_dir = "target/tools"
 mkdir -Force $tools_dir
 
-Download-File `
-    -Url 'https://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win64.exe' `
-    -ExpectedDigest D160B1D97266F3F28A71B4420A0AD2CD088A7977C2DD3B25AF155652D8D8D91F `
-    -OutFile $tools_dir/yasm.exe
+# This is the file BoringSSL refers to in
+# https://boringssl.googlesource.com/boringssl/+/26f8297177ad8033cc39de84afe9c2000430a66d.
+$nasm_version = "nasm-2.13.03"
+$nasm_zip = "$nasm_version-win64.zip"
+$nasm_zip_sha256 = "B3A1F896B53D07854884C2E0D6BE7DEFBA7EBD09B864BBB9E6D69ADA1C3E989F"
+$nasm_exe = "nasm.exe"
+$nasm_exe_sha256 = "D8A933BF5CC3597C56193135CB78B225AB225E1F611D2FDB51EF6E3F555B21E3"
+Download-Zip-and-Extract-File `
+    -Uri "https://www.nasm.us/pub/nasm/releasebuilds/2.13.03/win64/$nasm_zip" `
+    -ZipExpectedDigest "$nasm_zip_sha256" `
+    -PathWithinZip "$nasm_version/$nasm_exe" `
+    -FileExpectedDigest "$nasm_exe_sha256" `
+    -OutFile "$tools_dir/$nasm_exe"
