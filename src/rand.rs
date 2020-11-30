432909280dfcb620bf23677f5d73286b345c3952
Michael Neumann <mneumann@ntecs.de>
2020-11-30 18:31:28 +0000

    Fix compilation on DragonFly
    This superseeds issue #966.
    
    Tested-on: DragonFly 5.8.1

--- src/rand.rs
+++ src/rand.rs
@@ -180,6 +180,7 @@ use self::sysrand::fill as fill_impl;
 use self::sysrand_or_urandom::fill as fill_impl;
 
 #[cfg(any(
+    target_os = "dragonfly",
     target_os = "freebsd",
     target_os = "illumos",
     target_os = "netbsd",
@@ -352,6 +353,7 @@ mod sysrand_or_urandom {
         any(target_os = "android", target_os = "linux"),
         feature = "dev_urandom_fallback"
     ),
+    target_os = "dragonfly",
     target_os = "freebsd",
     target_os = "netbsd",
     target_os = "openbsd",
