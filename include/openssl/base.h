c3f4612d83f345ef0bb0024ce6086a6a773324ca
David Benjamin <davidben@google.com>
2020-12-01 17:47:57 -0500

    Only accept little-endian ARM and MIPS variants in base.h.
    <openssl/base.h> checks for a supported platform, but we don't check
    endianness of ARM and MIPS, which are bi-endian. See
    https://crbug.com/1153312#c7.
    
    Switch this around. Documentation on which define is "official" is hard
    to come by, so I mostly mimicked Chromium. Chromium detects
    little-endian ARM and MIPS with __ARMEL__ and __MIPSEL__ respectively,
    without looking at __arm__ or __mips__. It uses __aarch64__
    instead of __AARCH64EL__, but I think that's an oversight. I can get
    Clang to output for aarch64_be and that defines __aarch64__ with
    __AARCH64EB__.
    
    <openssl/arm_arch.h> (which we should simplify and align with base.h
    once this CL sticks) also normalizes to __ARMEL__ over __BYTE_ORDER__
    and friends. Although, interestingly, arm_arch.h defines its own
    __ARMEL__ on GNUC aarch64, even though Clang does *not* define __ARMEL__
    on aarch64. (I'm guessing this aligned for the benefit of the "armx"
    bi-arch asm files.) This value is based on __BYTE_ORDER__, not
    __ARMEL__, but it assumes GNUC arm always defines __ARMEL__, so I think
    it's reasonable to assume GNUC aarch64 always defines __AARCH64EL__.
    
    Given all this, probably the simplest thing that's most likely to work
    is to use __ARMEL__, __MIPSEL__, and __AARCH64EL__. Note this does not
    change the _M_* checks. _M_* are Windows's definitions, which I think we
    can reasonably assume come with an endianness opinion. (Windows' ARM and
    ARM64 ABIs mandate little-endian.) This aligns with Chromium.
    
    Update-Note: CPU processor defines are a mess. If a little-endian ARM or
    MIPS build breaks, some of the assumptions above may be wrong. In that
    case, the output $CC -dM -E - < /dev/null on the offending toolchain
    will be useful to fix it. If a big-endian ARM or MIPS build breaks, this
    is working as intended. Any resulting binaries weren't producing the
    right outputs.
    
    Change-Id: I2a9e662d09df119a71226e91716d84e7ac3792aa
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/44324
    Commit-Queue: Adam Langley <agl@google.com>
    Reviewed-by: Adam Langley <agl@google.com>

--- include/openssl/base.h
+++ include/openssl/base.h
@@ -90,19 +90,19 @@ extern "C" {
 #elif defined(__x86) || defined(__i386) || defined(__i386__) || defined(_M_IX86)
 #define OPENSSL_32_BIT
 #define OPENSSL_X86
-#elif defined(__aarch64__) || defined(_M_ARM64)
+#elif defined(__AARCH64EL__) || defined(_M_ARM64)
 #define OPENSSL_64_BIT
 #define OPENSSL_AARCH64
-#elif defined(__arm) || defined(__arm__) || defined(_M_ARM)
+#elif defined(__ARMEL__) || defined(_M_ARM)
 #define OPENSSL_32_BIT
 #define OPENSSL_ARM
 #elif (defined(__PPC64__) || defined(__powerpc64__)) && defined(_LITTLE_ENDIAN)
 #define OPENSSL_64_BIT
 #define OPENSSL_PPC64LE
-#elif defined(__mips__) && !defined(__LP64__)
+#elif defined(__MIPSEL__) && !defined(__LP64__)
 #define OPENSSL_32_BIT
 #define OPENSSL_MIPS
-#elif defined(__mips__) && defined(__LP64__)
+#elif defined(__MIPSEL__) && defined(__LP64__)
 #define OPENSSL_64_BIT
 #define OPENSSL_MIPS64
 #elif defined(__pnacl__)
