4c3c93a24122398c28ea004cc4c19fe53bee5af4
Brian Smith <brian@briansmith.org>
2021-02-05 11:12:18 -0800

    Remove `endian::as_byte_slice()`.
    We don't need it now that `ArrayEncoding::as_byte_array()` can handle larger
    arrays.

--- src/endian.rs
+++ src/endian.rs
@@ -11,13 +11,6 @@ where
     const ZERO: Self;
 }
 
-/// Allow access to a slice of  of `Encoding<T>` as a slice of bytes.
-pub fn as_byte_slice<E: Encoding<T>, T>(x: &[E]) -> &[u8] {
-    unsafe {
-        core::slice::from_raw_parts(x.as_ptr() as *const u8, x.len() * core::mem::size_of::<E>())
-    }
-}
-
 /// Work around the inability to implement `AsRef` for arrays of `Encoding`s
 /// due to the coherence rules.
 pub trait ArrayEncoding<T> {
@@ -73,9 +66,6 @@ macro_rules! impl_array_encoding {
             for [$endian<$base>; $elems]
         {
             fn as_byte_array(&self) -> &[u8; $elems * core::mem::size_of::<$base>()] {
-                // TODO: When we can require Rust 1.47.0 or later we could avoid
-                // `as` and `unsafe` here using
-                // `as_byte_slice(self).try_into().unwrap()`.
                 let as_bytes_ptr =
                     self.as_ptr() as *const [u8; $elems * core::mem::size_of::<$base>()];
                 unsafe { &*as_bytes_ptr }
