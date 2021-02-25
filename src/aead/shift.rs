74aa79b650f8a1f751993a56046ca43166ae9934
Brian Smith <brian@briansmith.org>
2021-02-24 20:40:15 -0800

    AEAD: Refactor more of the AEAD internals to use `copy_within` style.

--- src/aead/shift.rs
+++ src/aead/shift.rs
@@ -15,18 +15,18 @@
 use super::block::{Block, BLOCK_LEN};
 
 #[cfg(target_arch = "x86")]
-pub fn shift_full_blocks<F>(in_out: &mut [u8], in_prefix_len: usize, mut transform: F)
+pub fn shift_full_blocks<F>(in_out: &mut [u8], src: core::ops::RangeFrom<usize>, mut transform: F)
 where
     F: FnMut(&[u8; BLOCK_LEN]) -> Block,
 {
     use core::convert::TryFrom;
 
-    let in_out_len = in_out.len().checked_sub(in_prefix_len).unwrap();
+    let in_out_len = in_out[src.clone()].len();
 
     for i in (0..in_out_len).step_by(BLOCK_LEN) {
         let block = {
             let input =
-                <&[u8; BLOCK_LEN]>::try_from(&in_out[(in_prefix_len + i)..][..BLOCK_LEN]).unwrap();
+                <&[u8; BLOCK_LEN]>::try_from(&in_out[(src.start + i)..][..BLOCK_LEN]).unwrap();
             transform(input)
         };
         let output = <&mut [u8; BLOCK_LEN]>::try_from(&mut in_out[i..][..BLOCK_LEN]).unwrap();
