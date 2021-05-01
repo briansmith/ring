47dea6611ca359cda8f18d2cb228692f1711d73d
Ben Noordhuis <info@bnoordhuis.nl>
2021-05-02 00:46:36 +0200

    Add Set and UTF8String DER tags.

--- src/io/der.rs
+++ src/io/der.rs
@@ -31,7 +31,9 @@ pub enum Tag {
     OctetString = 0x04,
     Null = 0x05,
     OID = 0x06,
+    UTF8String = 0x0C,
     Sequence = CONSTRUCTED | 0x10, // 0x30
+    Set = CONSTRUCTED | 0x11,      // 0x31
     UTCTime = 0x17,
     GeneralizedTime = 0x18,
 
