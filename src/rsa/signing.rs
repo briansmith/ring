0e6be9778da011a5ede91a7905f8d05af2c18977
Brian Smith <brian@briansmith.org>
2021-09-22 17:24:58 -0700

    RSA: Add a public `RsaKeyPair::from_components` function.

--- src/rsa/signing.rs
+++ src/rsa/signing.rs
@@ -43,12 +43,6 @@ derive_debug_via_field!(RsaKeyPair, stringify!(RsaKeyPair), public_key);
 impl RsaKeyPair {
     /// Parses an unencrypted PKCS#8-encoded RSA private key.
     ///
-    /// Only two-prime (not multi-prime) keys are supported. The public modulus
-    /// (n) must be at least 2047 bits. The public modulus must be no larger
-    /// than 4096 bits. It is recommended that the public modulus be exactly
-    /// 2048 or 3072 bits. The public exponent must be at least 65537 and must
-    /// be no more than 33 bits long.
-    ///
     /// This will generate a 2048-bit RSA private key of the correct form using
     /// OpenSSL's command line tool:
     ///
@@ -86,43 +80,8 @@ impl RsaKeyPair {
     ///     -in rsa-2048-private-key.pem > rsa-2048-private-key.pk8
     /// ```
     ///
-    /// The private key is validated according to [NIST SP-800-56B rev. 1]
-    /// section 6.4.1.4.3, crt_pkv (Intended Exponent-Creation Method Unknown),
-    /// with the following exceptions:
-    ///
-    /// * Section 6.4.1.2.1, Step 1: Neither a target security level nor an
-    ///   expected modulus length is provided as a parameter, so checks
-    ///   regarding these expectations are not done.
-    /// * Section 6.4.1.2.1, Step 3: Since neither the public key nor the
-    ///   expected modulus length is provided as a parameter, the consistency
-    ///   check between these values and the private key's value of n isn't
-    ///   done.
-    /// * Section 6.4.1.2.1, Step 5: No primality tests are done, both for
-    ///   performance reasons and to avoid any side channels that such tests
-    ///   would provide.
-    /// * Section 6.4.1.2.1, Step 6, and 6.4.1.4.3, Step 7:
-    ///     * *ring* has a slightly looser lower bound for the values of `p`
-    ///     and `q` than what the NIST document specifies. This looser lower
-    ///     bound matches what most other crypto libraries do. The check might
-    ///     be tightened to meet NIST's requirements in the future. Similarly,
-    ///     the check that `p` and `q` are not too close together is skipped
-    ///     currently, but may be added in the future.
-    ///     - The validity of the mathematical relationship of `dP`, `dQ`, `e`
-    ///     and `n` is verified only during signing. Some size checks of `d`,
-    ///     `dP` and `dQ` are performed at construction, but some NIST checks
-    ///     are skipped because they would be expensive and/or they would leak
-    ///     information through side channels. If a preemptive check of the
-    ///     consistency of `dP`, `dQ`, `e` and `n` with each other is
-    ///     necessary, that can be done by signing any message with the key
-    ///     pair.
-    ///
-    ///     * `d` is not fully validated, neither at construction nor during
-    ///     signing. This is OK as far as *ring*'s usage of the key is
-    ///     concerned because *ring* never uses the value of `d` (*ring* always
-    ///     uses `p`, `q`, `dP` and `dQ` via the Chinese Remainder Theorem,
-    ///     instead). However, *ring*'s checks would not be sufficient for
-    ///     validating a key pair for use by some other system; that other
-    ///     system must check the value of `d` itself if `d` is to be used.
+    /// See [`Self::from_components`] for more details on how the input is
+    /// validated.
     ///
     /// See [RFC 5958] and [RFC 3447 Appendix A.1.2] for more details of the
     /// encoding of the key.
@@ -154,6 +113,9 @@ impl RsaKeyPair {
     ///
     /// It is recommended to use `from_pkcs8()` (with a PKCS#8-encoded key)
     /// instead.
+    ///
+    /// See [`Self::from_components()`] for more details on how the input is
+    /// validated.
     ///
     /// [RFC 3447 Appendix A.1.2]:
     ///     https://tools.ietf.org/html/rfc3447#appendix-A.1.2
@@ -208,7 +170,74 @@ impl RsaKeyPair {
         Self::from_components(&components)
     }
 
-    fn from_components(
+    /// Constructs an RSA private key from its big-endian-encoded components.
+    ///
+    /// Only two-prime (not multi-prime) keys are supported. The public modulus
+    /// (n) must be at least 2047 bits. The public modulus must be no larger
+    /// than 4096 bits. It is recommended that the public modulus be exactly
+    /// 2048 or 3072 bits. The public exponent must be at least 65537 and must
+    /// be no more than 33 bits long.
+    ///
+    /// The private key is validated according to [NIST SP-800-56B rev. 1]
+    /// section 6.4.1.4.3, crt_pkv (Intended Exponent-Creation Method Unknown),
+    /// with the following exceptions:
+    ///
+    /// * Section 6.4.1.2.1, Step 1: Neither a target security level nor an
+    ///   expected modulus length is provided as a parameter, so checks
+    ///   regarding these expectations are not done.
+    /// * Section 6.4.1.2.1, Step 3: Since neither the public key nor the
+    ///   expected modulus length is provided as a parameter, the consistency
+    ///   check between these values and the private key's value of n isn't
+    ///   done.
+    /// * Section 6.4.1.2.1, Step 5: No primality tests are done, both for
+    ///   performance reasons and to avoid any side channels that such tests
+    ///   would provide.
+    /// * Section 6.4.1.2.1, Step 6, and 6.4.1.4.3, Step 7:
+    ///     * *ring* has a slightly looser lower bound for the values of `p`
+    ///     and `q` than what the NIST document specifies. This looser lower
+    ///     bound matches what most other crypto libraries do. The check might
+    ///     be tightened to meet NIST's requirements in the future. Similarly,
+    ///     the check that `p` and `q` are not too close together is skipped
+    ///     currently, but may be added in the future.
+    ///     - The validity of the mathematical relationship of `dP`, `dQ`, `e`
+    ///     and `n` is verified only during signing. Some size checks of `d`,
+    ///     `dP` and `dQ` are performed at construction, but some NIST checks
+    ///     are skipped because they would be expensive and/or they would leak
+    ///     information through side channels. If a preemptive check of the
+    ///     consistency of `dP`, `dQ`, `e` and `n` with each other is
+    ///     necessary, that can be done by signing any message with the key
+    ///     pair.
+    ///
+    ///     * `d` is not fully validated, neither at construction nor during
+    ///     signing. This is OK as far as *ring*'s usage of the key is
+    ///     concerned because *ring* never uses the value of `d` (*ring* always
+    ///     uses `p`, `q`, `dP` and `dQ` via the Chinese Remainder Theorem,
+    ///     instead). However, *ring*'s checks would not be sufficient for
+    ///     validating a key pair for use by some other system; that other
+    ///     system must check the value of `d` itself if `d` is to be used.
+    pub fn from_components<Public, Private>(
+        components: &Components<Public, Private>,
+    ) -> Result<Self, KeyRejected>
+    where
+        Public: AsRef<[u8]>,
+        Private: AsRef<[u8]>,
+    {
+        let components = Components {
+            public_key: public::Components {
+                n: components.public_key.n.as_ref(),
+                e: components.public_key.e.as_ref(),
+            },
+            d: components.d.as_ref(),
+            p: components.p.as_ref(),
+            q: components.q.as_ref(),
+            dP: components.dP.as_ref(),
+            dQ: components.dQ.as_ref(),
+            qInv: components.qInv.as_ref(),
+        };
+        Self::from_components_(&components)
+    }
+
+    fn from_components_(
         &Components {
             public_key,
             d,
