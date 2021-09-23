92e15f23dc7c12829990f5fef09062a69d54dbf7
Brian Smith <brian@briansmith.org>
2021-09-23 13:07:01 -0700

    Make `ArrayFlatMap` compatible with Rust 1.52.1 (current MSRV).

--- src/limb.rs
+++ src/limb.rs
@@ -249,7 +249,10 @@ pub fn big_endian_from_limbs(limbs: &[Limb], out: &mut [u8]) {
 /// The number of bytes returned will be a multiple of `LIMB_BYTES`.
 fn be_bytes(limbs: &[Limb]) -> impl ExactSizeIterator<Item = u8> + Clone + '_ {
     // The unwrap is safe because a slice can never be larger than `usize` bytes.
-    ArrayFlatMap::new(limbs.iter().rev().copied(), Limb::to_be_bytes).unwrap()
+    ArrayFlatMap::new(limbs.iter().rev().copied(), |limb| {
+        core::array::IntoIter::new(Limb::to_be_bytes(limb))
+    })
+    .unwrap()
 }
 
 #[cfg(feature = "alloc")]
