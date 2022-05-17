04263136676464412a297e37252117287e628c7a
Bob Beck <bbe@google.com>
2022-05-17 13:59:45 -0600

    limit the feature macro stuff to __linux__
    These definitions are to get access to getaddrinfo() and gmtime_r()
    when using glibc. This in turn conflicts with other places (which
    would have these things in their libc anyway) where using these
    feature flags turns off C11 functionality we would like to use.
    
    Bug:490
    
    Change-Id: I66fdb7292cda788df19508d99e7303ed0d4f4bdd
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/52545
    Commit-Queue: Bob Beck <bbe@google.com>
    Reviewed-by: David Benjamin <davidben@google.com>

--- crypto/asn1/time_support.c
+++ crypto/asn1/time_support.c
@@ -55,7 +55,7 @@
  * (eay@cryptsoft.com).  This product includes software written by Tim
  * Hudson (tjh@cryptsoft.com). */
 
-#if !defined(_POSIX_C_SOURCE)
+#if defined(__linux__) && !defined(_POSIX_C_SOURCE)
 #define _POSIX_C_SOURCE 201410L  /* for gmtime_r */
 #endif
 
