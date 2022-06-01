42108369f48ff91c6aecb0d97563a6389562c98d
Alvaro Figueroa <alvaro.figueroa@microsoft.com>
2022-06-01 12:31:53 -0600

    Adding RISCV from #1419

--- include/ring-core/base.h
+++ include/ring-core/base.h
@@ -91,6 +91,10 @@
 #define OPENSSL_MIPS64
 #elif defined(__wasm__)
 #define OPENSSL_32_BIT
+#elif __riscv && __riscv_xlen == 64
+#define OPENSSL_64_BIT
+#elif __riscv && __riscv_xlen == 32
+#define OPENSSL_32_BIT
 #else
 // Note BoringSSL only supports standard 32-bit and 64-bit two's-complement,
 // little-endian architectures. Functions will not produce the correct answer
