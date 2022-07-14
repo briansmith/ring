4d7b383e989eff619a60eb1da1b18f613af65daf
David Benjamin <davidben@google.com>
2022-07-14 19:02:34 -0400

    Remove PEM_X509_INFO_write_bio.
    Nothing calls this function, it doesn't support most key types, and
    accesses pkey.rsa without checking the type. Just remove it.
    
    Change-Id: I073dfe74c545c7e08578b85105c88a19bbddf58a
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/53505
    Auto-Submit: David Benjamin <davidben@google.com>
    Reviewed-by: Bob Beck <bbe@google.com>
    Commit-Queue: Bob Beck <bbe@google.com>

--- include/openssl/pem.h
+++ include/openssl/pem.h
@@ -349,10 +349,6 @@ OPENSSL_EXPORT int PEM_ASN1_write_bio(i2d_of_void *i2d, const char *name,
 
 OPENSSL_EXPORT STACK_OF(X509_INFO) *PEM_X509_INFO_read_bio(
     BIO *bp, STACK_OF(X509_INFO) *sk, pem_password_cb *cb, void *u);
-OPENSSL_EXPORT int PEM_X509_INFO_write_bio(BIO *bp, X509_INFO *xi,
-                                           EVP_CIPHER *enc, unsigned char *kstr,
-                                           int klen, pem_password_cb *cd,
-                                           void *u);
 
 OPENSSL_EXPORT int PEM_read(FILE *fp, char **name, char **header,
                             unsigned char **data, long *len);
