b95124305ab15c7523d3e21437309fa5dd717ee8
Bob Beck <bbe@google.com>
2022-07-19 14:47:58 -0600

    Mostly Revert 52426 - Drop AVCP support for 3DES
    Support is still needed for ACVP until 2023
    
    Change-Id: Ia131a85bc06e7c61c823f1b3c021e2625a8769c4
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/53465
    Reviewed-by: Corie Pressley <cpressley@google.com>
    Reviewed-by: Adam Langley <agl@google.com>
    Commit-Queue: Bob Beck <bbe@google.com>

--- util/fipstools/acvp/acvptool/subprocess/subprocess.go
+++ util/fipstools/acvp/acvptool/subprocess/subprocess.go
@@ -85,6 +85,8 @@ func NewWithIO(cmd *exec.Cmd, in io.WriteCloser, out io.ReadCloser) *Subprocess 
 		"ACVP-AES-CBC":      &blockCipher{"AES-CBC", 16, 2, true, true, iterateAESCBC},
 		"ACVP-AES-CBC-CS3":  &blockCipher{"AES-CBC-CS3", 16, 1, false, true, iterateAESCBC},
 		"ACVP-AES-CTR":      &blockCipher{"AES-CTR", 16, 1, false, true, nil},
+		"ACVP-TDES-ECB":     &blockCipher{"3DES-ECB", 8, 3, true, false, iterate3DES},
+		"ACVP-TDES-CBC":     &blockCipher{"3DES-CBC", 8, 3, true, true, iterate3DESCBC},
 		"ACVP-AES-XTS":      &xts{},
 		"ACVP-AES-GCM":      &aead{"AES-GCM", false},
 		"ACVP-AES-GMAC":     &aead{"AES-GCM", false},
