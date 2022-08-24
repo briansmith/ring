b2d3c10cdc8fb642a842db2c6061743b4604b0b5
Rebecca Chang Swee Fun <rebecca.chang@starfivetech.com>
2022-08-24 02:38:49 +0000

    Clean up header to reuse __riscv definition
    Change-Id: I3f7026b982f8503fd814be6feb99725f8e60b274
    Signed-off-by: Rebecca Chang Swee Fun <rebecca.chang@starfivetech.com>
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/54005
    Reviewed-by: David Benjamin <davidben@google.com>
    Commit-Queue: David Benjamin <davidben@google.com>

--- include/openssl/base.h
+++ include/openssl/base.h
@@ -107,6 +107,7 @@ extern "C" {
 #define OPENSSL_MIPS64
 #elif defined(__riscv) && __SIZEOF_POINTER__ == 8
 #define OPENSSL_64_BIT
+#define OPENSSL_RISCV64
 #elif defined(__riscv) && __SIZEOF_POINTER__ == 4
 #define OPENSSL_32_BIT
 #elif defined(__pnacl__)
@@ -118,9 +119,6 @@ extern "C" {
 #define OPENSSL_32_BIT
 #elif defined(__myriad2__)
 #define OPENSSL_32_BIT
-#elif defined(__riscv) && __riscv_xlen == 64
-#define OPENSSL_64_BIT
-#define OPENSSL_RISCV64
 #else
 // Note BoringSSL only supports standard 32-bit and 64-bit two's-complement,
 // little-endian architectures. Functions will not produce the correct answer
