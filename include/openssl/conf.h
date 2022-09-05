2397276eed9c2697777fcd28de23c67fb54c953f
David Benjamin <davidben@google.com>
2022-09-05 01:23:54 -0400

    Unexport CONF_parse_list.
    This doesn't seem to be used anywhere and unexporting it lets us make it
    size_t-clean.
    
    Update-Note: CONF_parse_list was removed. If parsing strings, use a
    dedicated string library.
    
    Bug: 516
    Change-Id: I86fb353bb95268f7234fddf5563ecf2a27da99bd
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/54468
    Commit-Queue: David Benjamin <davidben@google.com>
    Reviewed-by: Bob Beck <bbe@google.com>

--- include/openssl/conf.h
+++ include/openssl/conf.h
@@ -121,19 +121,6 @@ OPENSSL_EXPORT const char *NCONF_get_string(const CONF *conf,
                                             const char *name);
 
 
-// Utility functions
-
-// CONF_parse_list takes a list separated by 'sep' and calls |list_cb| giving
-// the start and length of each member, optionally stripping leading and
-// trailing whitespace. This can be used to parse comma separated lists for
-// example. If |list_cb| returns <= 0, then the iteration is halted and that
-// value is returned immediately. Otherwise it returns one. Note that |list_cb|
-// may be called on an empty member.
-int CONF_parse_list(const char *list, char sep, int remove_whitespace,
-                    int (*list_cb)(const char *elem, int len, void *usr),
-                    void *arg);
-
-
 // Deprecated functions
 
 // These defines do nothing but are provided to make old code easier to
