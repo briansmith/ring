7b2795a323b41e7b148bcd6f6318d67efccb0ce4
David Benjamin <davidben@google.com>
2022-09-08 17:40:10 -0400

    Replace even more ad-hoc bytes/integer conversions.
    Change-Id: I5e1d37106d7df8e8aaede295e8eb74c971553fd5
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/54365
    Reviewed-by: Bob Beck <bbe@google.com>
    Auto-Submit: David Benjamin <davidben@google.com>
    Commit-Queue: David Benjamin <davidben@google.com>
    Commit-Queue: Bob Beck <bbe@google.com>

--- ssl/ssl_lib.cc
+++ ssl/ssl_lib.cc
@@ -2822,13 +2822,6 @@ int SSL_get_ivs(const SSL *ssl, const uint8_t **out_read_iv,
   return 1;
 }
 
-static uint64_t be_to_u64(const uint8_t in[8]) {
-  return (((uint64_t)in[0]) << 56) | (((uint64_t)in[1]) << 48) |
-         (((uint64_t)in[2]) << 40) | (((uint64_t)in[3]) << 32) |
-         (((uint64_t)in[4]) << 24) | (((uint64_t)in[5]) << 16) |
-         (((uint64_t)in[6]) << 8) | ((uint64_t)in[7]);
-}
-
 uint64_t SSL_get_read_sequence(const SSL *ssl) {
   // TODO(davidben): Internally represent sequence numbers as uint64_t.
   if (SSL_is_dtls(ssl)) {
@@ -2836,11 +2829,11 @@ uint64_t SSL_get_read_sequence(const SSL *ssl) {
     assert(ssl->d1->r_epoch == (ssl->d1->bitmap.max_seq_num >> 48));
     return ssl->d1->bitmap.max_seq_num;
   }
-  return be_to_u64(ssl->s3->read_sequence);
+  return CRYPTO_load_u64_be(ssl->s3->read_sequence);
 }
 
 uint64_t SSL_get_write_sequence(const SSL *ssl) {
-  uint64_t ret = be_to_u64(ssl->s3->write_sequence);
+  uint64_t ret = CRYPTO_load_u64_be(ssl->s3->write_sequence);
   if (SSL_is_dtls(ssl)) {
     assert((ret >> 48) == 0);
     ret |= ((uint64_t)ssl->d1->w_epoch) << 48;
