bd5f1c41054ddee7c54030b689f8ff70b7cfaa8a
Brian Smith <brian@briansmith.org>
2022-10-19 16:37:10 -0700

    Use `llvm-nm` for checking symbol prefixes for targets for which we use clang.

--- mk/check-symbol-prefixes.sh
+++ mk/check-symbol-prefixes.sh
@@ -17,6 +17,32 @@
 set -eux -o pipefail
 IFS=$'\n\t'
 
+llvm_version=14
+
+for arg in $*; do
+  case $arg in
+    --target=*)
+      target=${arg#*=}
+      ;;
+    *)
+      ;;
+  esac
+done
+
+nm_exe=nm
+case $target in
+--target=aarch64-unknown-linux-gnu|*-musl*|wasm32-*)
+  nm_exe=llvm-nm-$llvm_version
+  ;;
+--target=*-android*)
+    ndk_version=25.1.8937393
+    ANDROID_NDK_ROOT=${ANDROID_NDK_ROOT:-${ANDROID_HOME}/ndk/$ndk_version}
+    android_tools=${ANDROID_NDK_ROOT}/toolchains/llvm/prebuilt/linux-x86_64/bin
+    nm_exe=$android_tools/llvm-nm
+  ;;
+esac
+
+
 # TODO: This should only look in one target directory.
 # TODO: This isn't as strict as it should be.
 #
@@ -25,12 +51,12 @@ IFS=$'\n\t'
 #
 # This is very liberal in filtering out symbols that "look like"
 # Rust-compiler-generated symbols.
-find target -type f -name libring-*.rlib | while read -r infile; do
+find target/$target -type f -name libring-*.rlib | while read -r infile; do
   tmpfile=$infile-without-lib-rmeta
   cp "$infile" "$tmpfile"
   # `nm` doesn't understand `lib.rmeta`.
   ar d "$tmpfile" lib.rmeta
-  bad=$(nm --defined-only --extern-only --print-file-name "$tmpfile" \
+  bad=$($nm_exe --defined-only --extern-only --print-file-name "$tmpfile" \
     | ( grep -v -E " . _?(ring_core_|__rustc|_ZN|DW.ref.rust_eh_personality)" || [[ $? == 1 ]] ))
   rm "$tmpfile"
   if [ ! -z "${bad-}" ]; then
