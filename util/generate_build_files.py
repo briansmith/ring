0e68520eb27b1f37038e9d0772cfee1d015b50c3
David Benjamin <davidben@google.com>
2023-02-04 20:06:30 -0500

    Specify -Iinclude with the crypto target.
    It's unclear to me whether doing it target-by-target is an improvement
    in crypto/fipsmodule, but this otherwise does seem a bit tidier. This
    aligns with CMake's documentation and "modern CMake" which prefers this
    pattern.
    
    Change-Id: I36c81842bff8b36eeaaf5dd3e0695fb45f3376c9
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/56585
    Commit-Queue: David Benjamin <davidben@google.com>
    Reviewed-by: Bob Beck <bbe@google.com>

--- util/generate_build_files.py
+++ util/generate_build_files.py
@@ -487,8 +487,6 @@ if(BUILD_SHARED_LIBS)
   set(CMAKE_POSITION_INDEPENDENT_CODE TRUE)
 endif()
 
-include_directories(src/include)
-
 '''
 
   def PrintLibrary(self, out, name, files):
@@ -543,6 +541,7 @@ endif()
 
       self.PrintLibrary(cmake, 'crypto',
           files['crypto'] + ['${CRYPTO_SOURCES_ASM_USED}'])
+      cmake.write('target_include_directories(crypto PUBLIC src/include)\n')
       self.PrintLibrary(cmake, 'ssl', files['ssl'])
       self.PrintExe(cmake, 'bssl', files['tool'], ['ssl', 'crypto'])
 
