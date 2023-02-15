9a4c7597ca127e8cbb00f5e27e65fe12b3f47ae0
Brian Smith <brian@briansmith.org>
2023-02-15 10:36:49 -0800

    CI: Install the latest (currently) version of the LTS NDK.

--- mk/install-build-tools.sh
+++ mk/install-build-tools.sh
@@ -27,8 +27,22 @@ function install_packages {
 use_clang=
 case $target in
 --target*android*)
+  # https://blog.rust-lang.org/2023/01/09/android-ndk-update-r25.html says
+  # "Going forward the Android platform will target the most recent LTS NDK,
+  # allowing Rust developers to access platform features sooner. These updates
+  # should occur yearly and will be announced in release notes." Assume that
+  # means that we should always prefer to be using the latest 25.x.y version of
+  # the NDK until the Rust project announces that we should use a higher major
+  # version number.
+  #
+  # TODO: This should probably be implemented as a map of Rust toolchain version
+  # to NDK version; e.g. our MSRV might (only) support an older NDK than the
+  # latest stable Rust toolchain.
+  #
+  # Keep the following line in sync with the corresponding line in cargo.sh.
+  ndk_version=25.2.9519653
+
   mkdir -p "${ANDROID_HOME}/licenses"
-  ndk_version=25.1.8937393
   android_license_file="${ANDROID_HOME}/licenses/android-sdk-license"
   accept_android_license=24333f8a63b6825ea9c5514f83c2829b004d1fee
   grep --quiet --no-messages "$accept_android_license" "$android_license_file" \
