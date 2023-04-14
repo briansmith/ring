3d592b27bfe76002da7766e72c5475dad53c0194
ZHAI Xiaojuan <zhaixiaojuan@loongson.cn>
2023-04-14 12:40:30 +0800

    Add support for LoongArch64

--- include/ring-core/target.h
+++ include/ring-core/target.h
@@ -34,6 +34,9 @@
 #elif defined(__ARMEL__) || defined(_M_ARM)
 #define OPENSSL_32_BIT
 #define OPENSSL_ARM
+#elif defined(__loongarch_lp64)
+#define OPENSSL_64_BIT
+#define OPENSSL_LOONGARCH64
 #elif defined(__MIPSEL__) && !defined(__LP64__)
 #define OPENSSL_32_BIT
 #define OPENSSL_MIPS
