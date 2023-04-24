5748eb8a3314281151af1edc63ba58bfd7097d7d
David Benjamin <davidben@google.com>
2023-04-24 16:09:23 -0400

    Move convert_wycheproof into its own package
    This can still be run like go run ../../util/convert_wycheproof. This is
    part of an attempt to reland 54b04fdc21d540a6e24f9ddb7ddc3e583518e24f,
    which ran into an issue with internal tooling that could not handle
    standalone Go files.
    
    Since the only such target we actually needed to run in that repository
    is convert_wycheproof, just promote it into its own package. Then we can
    stop trying to import util.
    
    Change-Id: I6237777dad09e5c81ad961816ce14a287ab2d46a
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/59185
    Reviewed-by: Adam Langley <agl@google.com>
    Commit-Queue: Adam Langley <agl@google.com>
    Auto-Submit: David Benjamin <davidben@google.com>

--- util/convert_wycheproof/convert_wycheproof.go
+++ util/convert_wycheproof/convert_wycheproof.go
@@ -12,8 +12,8 @@
 // OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
 // CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-// convert_wycheproof.go converts Wycheproof test vectors into a format more
-// easily consumed by BoringSSL.
+// convert_wycheproof converts Wycheproof test vectors into a format more easily
+// consumed by BoringSSL.
 package main
 
 import (
