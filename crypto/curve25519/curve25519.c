da757e601005704a00e58ac04f9dfeac184f0dd2
David Benjamin <davidben@google.com>
2023-05-24 14:21:07 -0400

    Add constant-time validation for curve25519
    Also add some tests for X25519_public_from_private, as we apparently
    weren't directly testing it with test vectors.
    
    Change-Id: I1b73a9655323d507a8e022c62530ddd4610db4b9
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/60109
    Auto-Submit: David Benjamin <davidben@google.com>
    Commit-Queue: Adam Langley <agl@google.com>
    Reviewed-by: Adam Langley <agl@google.com>

--- crypto/curve25519/curve25519.c
+++ crypto/curve25519/curve25519.c
@@ -2116,7 +2116,8 @@ int X25519(uint8_t out_shared_key[32], const uint8_t private_key[32],
   static const uint8_t kZeros[32] = {0};
   x25519_scalar_mult(out_shared_key, private_key, peer_public_value);
   // The all-zero output results when the input is a point of small order.
-  return CRYPTO_memcmp(kZeros, out_shared_key, 32) != 0;
+  return constant_time_declassify_int(
+             CRYPTO_memcmp(kZeros, out_shared_key, 32)) != 0;
 }
 
 void X25519_public_from_private(uint8_t out_public_value[32],
@@ -2147,4 +2148,5 @@ void X25519_public_from_private(uint8_t out_public_value[32],
   fe_loose_invert(&zminusy_inv, &zminusy);
   fe_mul_tlt(&zminusy_inv, &zplusy, &zminusy_inv);
   fe_tobytes(out_public_value, &zminusy_inv);
+  CONSTTIME_DECLASSIFY(out_public_value, 32);
 }
