a08b71194f7787549f6e0b47db9eea2a88c88f7a
David Benjamin <davidben@google.com>
2023-06-24 20:34:37 -0400

    Temporarily add sk_new_null, etc., wrappers in bssl-sys
    Android have not updated their bindgen (see b/279198502), so they cannot
    yet pick up inline functions automatically.
    
    Bug: 596
    Change-Id: I49d5adaaa3537ada545c9c6fce98ea2dbf2f40ae
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/61165
    Reviewed-by: Bob Beck <bbe@google.com>
    Commit-Queue: Bob Beck <bbe@google.com>
    Auto-Submit: David Benjamin <davidben@google.com>

--- rust/bssl-sys/src/lib.rs
+++ rust/bssl-sys/src/lib.rs
@@ -5,6 +5,7 @@
 // Set in build.rs
 include!(env!("BINDGEN_RS_FILE"));
 
+// TODO(crbug.com/boringssl/596): Remove these wrappers.
 pub fn ERR_GET_LIB(packed_error: u32) -> i32 {
     unsafe { ERR_GET_LIB_RUST(packed_error) }
 }
@@ -16,6 +17,13 @@ pub fn ERR_GET_REASON(packed_error: u32) -> i32 {
 pub fn ERR_GET_FUNC(packed_error: u32) -> i32 {
     unsafe { ERR_GET_FUNC_RUST(packed_error) }
 }
+
+pub use OPENSSL_sk_free as sk_free;
+pub use OPENSSL_sk_new_null as sk_new_null;
+pub use OPENSSL_sk_num as sk_num;
+pub use OPENSSL_sk_pop as sk_pop;
+pub use OPENSSL_sk_push as sk_push;
+pub use OPENSSL_sk_value as sk_value;
 
 pub fn init() {
     unsafe {
