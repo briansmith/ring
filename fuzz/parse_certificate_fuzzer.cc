4d70016a62bf8d4c1856aff89d6769ad4f71e77f
Bob Beck <bbe@google.com>
2023-07-18 17:05:02 +0000

    Make the parse_certificate_fuzzer from libpki work inside boring.
    Seed the corpus from cert_corpus. As part of that, check in the result of minimizing all the corpora.
    
    Note this is just making one of the fuzzers build, I'll adapt
    the others and follow on by updating the IMPORT process to do it
    in a follow on cl.
    
    Bug: chromium:1322914
    
    Change-Id: Iea1b89f8fee938fa99c0a4d8134bcd0e7023d149
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/61765
    Commit-Queue: Bob Beck <bbe@google.com>
    Reviewed-by: David Benjamin <davidben@google.com>

---
+++ fuzz/parse_certificate_fuzzer.cc
@@ -0,0 +1,25 @@
+// Copyright 2016 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "../pki/cert_errors.h"
+#include "../pki/parsed_certificate.h"
+#include <openssl/pool.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  bssl::CertErrors errors;
+  std::shared_ptr<const bssl::ParsedCertificate> cert =
+      bssl::ParsedCertificate::Create(
+          bssl::UniquePtr<CRYPTO_BUFFER>(
+              CRYPTO_BUFFER_new(data, size, nullptr)),
+          {}, &errors);
+
+  // Severe errors must be provided iff the parsing failed.
+  CHECK_EQ(errors.ContainsAnyErrorWithSeverity(bssl::CertError::SEVERITY_HIGH),
+           cert == nullptr);
+
+  return 0;
+}
