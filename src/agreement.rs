c7d949c2ada31259f0e1c37872bebb758aa586d7
Brian Smith <brian@briansmith.org>
2023-10-26 12:50:18 -0700

    Agreement docs: Eliminate redundant nested `peer_public_key` in example.

--- src/agreement.rs
+++ src/agreement.rs
@@ -32,17 +32,17 @@
 //! // message.
 //! let my_public_key = my_private_key.compute_public_key()?;
 //!
-//! let peer_public_key = {
+//! let peer_public_key_bytes = {
 //!     // In a real application, the peer public key would be parsed out of a
 //!     // protocol message. Here we just generate one.
-//!     let peer_public_key = {
-//!         let peer_private_key =
-//!             agreement::EphemeralPrivateKey::generate(&agreement::X25519, &rng)?;
-//!         peer_private_key.compute_public_key()?
-//!     };
-//!
-//!     agreement::UnparsedPublicKey::new(&agreement::X25519, peer_public_key)
+//!     let peer_private_key =
+//!         agreement::EphemeralPrivateKey::generate(&agreement::X25519, &rng)?;
+//!     peer_private_key.compute_public_key()?
 //! };
+//!
+//! let peer_public_key = agreement::UnparsedPublicKey::new(
+//!     &agreement::X25519,
+//!     peer_public_key_bytes);
 //!
 //! agreement::agree_ephemeral(
 //!     my_private_key,
