970df5e4563fd0cd0f2ae8cdb910225979363bab
David Benjamin <davidben@google.com>
2023-12-22 13:24:07 -0500

    Add a missing error check for sk_X509_push
    Change-Id: I4cf8af50d7355b447d3d7f9f92259febfc844b79
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/65127
    Reviewed-by: Bob Beck <bbe@google.com>
    Commit-Queue: David Benjamin <davidben@google.com>

--- crypto/x509/x509_vfy.c
+++ crypto/x509/x509_vfy.c
@@ -400,7 +400,10 @@ int X509_verify_cert(X509_STORE_CTX *ctx) {
       }
       ctx->current_cert = x;
     } else {
-      sk_X509_push(ctx->chain, chain_ss);
+      if (!sk_X509_push(ctx->chain, chain_ss)) {
+        ctx->error = X509_V_ERR_OUT_OF_MEM;
+        goto end;
+      }
       num++;
       ctx->last_untrusted = num;
       ctx->current_cert = chain_ss;
