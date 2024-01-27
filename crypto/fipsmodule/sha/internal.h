469118ca637887427f9727f1aac13c3e5bdee068
David Benjamin <davidben@google.com>
2024-01-27 18:54:15 -0500

    Move capability checks in sha256-586.pl to C
    This took a little restructuring because they were previously all one
    big function. Some notes:
    
    - label() and set_label() in x86 perlasm default to function-scoped. But
      static_label() marks a label as file-scoped, which is why "pic_point"
      and "K256" work.
    
    - There's a pretty sizeable common preamble. I just copied it to each
      for simplicity. I'm pretty sure some of it is wasted, but it's
      definitely not all wasted, between loading parameters, setting up
      stack alignment, and saving the old stack location. But I'm not sure
      if all those 16 bytes are actually used.
    
    Bug: 673
    Change-Id: I6e8671d05d07cb4676ecf117dd56e2ed355c5d19
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/65874
    Commit-Queue: David Benjamin <davidben@google.com>
    Reviewed-by: Bob Beck <bbe@google.com>

--- crypto/fipsmodule/sha/internal.h
+++ crypto/fipsmodule/sha/internal.h
@@ -79,6 +79,7 @@ OPENSSL_INLINE int sha512_hw_capable(void) {
 #elif !defined(OPENSSL_NO_ASM) && defined(OPENSSL_X86)
 
 #define SHA1_ASM_NOHW
+#define SHA256_ASM_NOHW
 
 #define SHA1_ASM_SSSE3
 OPENSSL_INLINE int sha1_ssse3_capable(void) {
@@ -103,11 +104,31 @@ OPENSSL_INLINE int sha1_avx_capable(void) {
 void sha1_block_data_order_avx(uint32_t state[5], const uint8_t *data,
                                size_t num);
 
+#define SHA256_ASM_SSSE3
+OPENSSL_INLINE int sha256_ssse3_capable(void) {
+  // TODO(davidben): Do we need to check the FXSR bit? The Intel manual does not
+  // say to.
+  return CRYPTO_is_SSSE3_capable() && CRYPTO_is_FXSR_capable();
+}
+void sha256_block_data_order_ssse3(uint32_t state[8], const uint8_t *data,
+                                   size_t num);
+
+#define SHA256_ASM_AVX
+OPENSSL_INLINE int sha256_avx_capable(void) {
+  // Pre-Zen AMD CPUs had slow SHLD/SHRD; Zen added the SHA extension; see the
+  // discussion in sha1-586.pl.
+  //
+  // TODO(davidben): Should we enable SHAEXT on 32-bit x86?
+  // TODO(davidben): Do we need to check the FXSR bit? The Intel manual does not
+  // say to.
+  return CRYPTO_is_AVX_capable() && CRYPTO_is_intel_cpu() &&
+         CRYPTO_is_FXSR_capable();
+}
+void sha256_block_data_order_avx(uint32_t state[8], const uint8_t *data,
+                                 size_t num);
+
 // TODO(crbug.com/boringssl/673): Move the remaining CPU dispatch to C.
-#define SHA256_ASM
 #define SHA512_ASM
-void sha256_block_data_order(uint32_t state[8], const uint8_t *data,
-                             size_t num_blocks);
 void sha512_block_data_order(uint64_t state[8], const uint8_t *data,
                              size_t num_blocks);
 
