ffb7fe3e04815be37e150bc07a99688a2991d843
Brian Smith <brian@briansmith.org>
2024-02-29 17:50:31 -0800

    mk/package.sh: Avoid using wc(1).
    The test for empty output can be done directly using bash.

--- mk/package.sh
+++ mk/package.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 # Make sure the current tree isn't dirty.
 # https://stackoverflow.com/a/5737794
-if [[ $(git status --porcelain | wc -c) -ne 0 ]]; then
+if [[ -n "$(git status --porcelain)" ]]; then
   echo Repository is dirty.
   exit 1
 fi
