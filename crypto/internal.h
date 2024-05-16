146f5f68b73f4a29f56923291183e18b9cb5de1b
Brian Smith <brian@briansmith.org>
2024-05-16 11:53:03 -0700

    aes: Oxidize aes_nohw [un]compact_{block,word}.

--- crypto/internal.h
+++ crypto/internal.h
@@ -343,18 +343,6 @@ static inline int constant_time_declassify_int(int v) {
 static inline uint32_t CRYPTO_bswap4(uint32_t x) {
   return __builtin_bswap32(x);
 }
-
-static inline uint64_t CRYPTO_bswap8(uint64_t x) {
-  return __builtin_bswap64(x);
-}
-#elif defined(_MSC_VER)
-#pragma warning(push, 3)
-#include <stdlib.h>
-#pragma warning(pop)
-#pragma intrinsic(_byteswap_ulong)
-static inline uint32_t CRYPTO_bswap4(uint32_t x) {
-  return _byteswap_ulong(x);
-}
 #endif
 
 #if !defined(RING_CORE_NOSTDLIBINC)
