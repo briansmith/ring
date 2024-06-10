731ca3b23b00101661932a6448d67c49404aeb01
Brian Smith <brian@briansmith.org>
2024-06-10 15:35:39 -0700

    bssl::Result doesn't need to be Copy/Clone/Debug.
    It is immediately converted into a `Result` whenever created.

--- src/bssl.rs
+++ src/bssl.rs
@@ -17,7 +17,6 @@ use crate::{c, error};
 /// An `int` returned from a foreign function containing **1** if the function
 /// was successful or **0** if an error occurred. This is the convention used by
 /// C code in `ring`.
-#[derive(Clone, Copy, Debug)]
 #[must_use]
 #[repr(transparent)]
 pub struct Result(c::int);
