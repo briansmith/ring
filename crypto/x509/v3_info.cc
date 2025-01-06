508d5b3483e31ee1e72e7a29ab2a9fee61ce7d4d
Bob Beck <bbe@google.com>
2025-01-06 23:28:10 +0000

    replace manual alloction math and srlcats with asprintf
    Change-Id: I94077581037372ea658e60b86b05fa977e1c3ac6
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/74868
    Commit-Queue: Bob Beck <bbe@google.com>
    Reviewed-by: Adam Langley <agl@google.com>

--- crypto/x509/v3_info.cc
+++ crypto/x509/v3_info.cc
@@ -80,7 +80,6 @@ static STACK_OF(CONF_VALUE) *i2v_AUTHORITY_INFO_ACCESS(
   const AUTHORITY_INFO_ACCESS *ainfo =
       reinterpret_cast<const AUTHORITY_INFO_ACCESS *>(ext);
   ACCESS_DESCRIPTION *desc;
-  int name_len;
   char objtmp[80], *name;
   CONF_VALUE *vtmp;
   STACK_OF(CONF_VALUE) *tret = ret;
@@ -97,14 +96,9 @@ static STACK_OF(CONF_VALUE) *i2v_AUTHORITY_INFO_ACCESS(
     vtmp = sk_CONF_VALUE_value(tret, i);
     i2t_ASN1_OBJECT(objtmp, sizeof objtmp, desc->method);
 
-    name_len = strlen(objtmp) + 3 + strlen(vtmp->name) + 1;
-    name = reinterpret_cast<char *>(OPENSSL_malloc(name_len));
-    if (name == NULL) {
+    if (OPENSSL_asprintf(&name, "%s - %s", objtmp, vtmp->name) == -1) {
       goto err;
     }
-    OPENSSL_strlcpy(name, objtmp, name_len);
-    OPENSSL_strlcat(name, " - ", name_len);
-    OPENSSL_strlcat(name, vtmp->name, name_len);
     OPENSSL_free(vtmp->name);
     vtmp->name = name;
   }
