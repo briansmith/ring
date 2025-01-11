622389ff17259e92a00077630cf36786f9604acf
Paul H. Liu <paul@thev.net>
2025-01-10 19:34:56 -0800

    Separate features for wasm32_unknown_unknown_custom, wasm32_unknown_unknown_js, and wasm32_unknown_unknown_custom_or_js.

--- src/lib.rs
+++ src/lib.rs
@@ -43,12 +43,17 @@
 //! <tr><td><code>std</code>
 //!     <td>Enable features that use libstd, in particular
 //!         <code>std::error::Error</code> integration. Implies `alloc`.
-//! <tr><td><code>wasm32_unknown_unknown_js</code>
+//! <tr><td><code>wasm32_unknown_unknown_custom_or_js</code>
 //!     <td>When this feature is enabled, for the wasm32-unknown-unknown target,
 //!         Web APIs will be used to implement features like `ring::rand` that
 //!         require an operating environment of some kind. This has no effect
-//!         for any other target. This enables the `getrandom` crate's `js`
-//!         feature.
+//!         for any other target.
+//! <tr><td><code>wasm32_unknown_unknown_js</code>
+//!     <td>Enable both `wasm32_unknown_unknown_custom_or_js` and the `getrandom`
+//!         crate's `js` feature.
+//! <tr><td><code>wasm32_unknown_unknown_custom</code>
+//!     <td>Enable both `wasm32_unknown_unknown_custom_or_js` and the `getrandom`
+//!         crate's `custom` feature.
 //! </table>
 
 // When running mk/package.sh, don't actually build any code.
