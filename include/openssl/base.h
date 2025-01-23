15bab61253c035c3953160e32a0ae08442db73a5
Bob Beck <bbe@google.com>
2025-01-23 19:46:41 +0000

    Bump API version to 34
    Change-Id: I6fc3367377d8c4ec6e75e23c1b7e3056ff31b90f
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/75627
    Reviewed-by: David Benjamin <davidben@google.com>
    Auto-Submit: Bob Beck <bbe@google.com>
    Commit-Queue: David Benjamin <davidben@google.com>

--- include/openssl/base.h
+++ include/openssl/base.h
@@ -66,7 +66,7 @@ extern "C" {
 // A consumer may use this symbol in the preprocessor to temporarily build
 // against multiple revisions of BoringSSL at the same time. It is not
 // recommended to do so for longer than is necessary.
-#define BORINGSSL_API_VERSION 33
+#define BORINGSSL_API_VERSION 34
 
 #if defined(BORINGSSL_SHARED_LIBRARY)
 
