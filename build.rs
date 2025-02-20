8dd9172e173d40aa314f3f2fa91635529895238d
dave horner <daveydave1234@gmail.com>
2025-02-19 22:41:28 -0500

    3000

--- build.rs
+++ build.rs
@@ -151,7 +151,7 @@ fn cpp_flags(compiler: &cc::Tool) -> &'static [&'static str] {
             "/Zc:wchar_t",
             "/Zc:forScope",
             "/Zc:inline",
-            "/Zm5000",
+            "/Zm3000",
             // Warnings.
             "/Wall",
             "/wd4127", // C4127: conditional expression is constant
