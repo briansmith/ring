0a65776ae8f40a22e8f2491f700e82341c865e12
Brian Smith <brian@briansmith.org>
2025-06-04 17:40:06 -0700

    wip

---
+++ mk/pregenerate.sh
@@ -0,0 +1,23 @@
+# This only works on Windows, using MinGW.
+set -eux -o pipefail
+IFS=$'\n\t'
+
+# Make sure the current tree isn't dirty.
+# https://stackoverflow.com/a/5737794
+if [[ -n "$(git status --porcelain)" ]]; then
+  echo Repository is dirty.
+  exit 1
+fi
+
+msrv=1.66.0
+cargo clean --target-dir=target/pregenerate_asm
+cp build_settings_packaged.rs build_settings.rs
+RING_PREGENERATE_ASM=1 CC_AARCH64_PC_WINDOWS_MSVC=clang \
+  cargo +${msrv} build -p ring --target-dir=target/pregenerate_asm
+if [[ -n "$(git status --porcelain -- ':(exclude,top)build_settings.rs' ':(exclude,top)pregenerated/')" ]]; then
+  echo Repository is dirty.
+  exit 1
+fi
+(cd pregenerated && git add . && git commit -m "Update pregenerated files.")
+git add build_settings.rs pregenerated
+git commit -m "Prepare packaged release."
