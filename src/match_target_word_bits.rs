7efd058c50111b76890cd1a36cc949a695ce36b1
Brian Smith <brian@briansmith.org>
2025-06-08 17:21:09 -0700

    limb tests: Eliminate direct use of `cfg(target_pointer_width)`.

--- src/match_target_word_bits.rs
+++ src/match_target_word_bits.rs
@@ -1,23 +1,29 @@
 macro_rules! match_target_word_bits {
     {
-        64 => { $( $if_64:item )* },
-        32 => { $( $if_32:item )* },
+        64 => { $( $if_64:tt )* },
+        32 => { $( $if_32:tt )* },
+        $( _ => { $( $otherwise:tt )* } )?
     } => {
         cfg_if::cfg_if! {
             if #[cfg(target_pointer_width = "64")] {
                 $( $if_64 )*
             } else if #[cfg(target_pointer_width = "32")] {
                 $( $if_32 )*
+            } else {
+                $( $( $otherwise )* )?
             }
         }
     };
 
     {
         64 | 32 => { $( $if_64_or_32:item )* },
+        $( _ => { $( $otherwise:tt )* } )?
     } => {
         cfg_if::cfg_if! {
             if #[cfg(any(target_pointer_width = "32", target_pointer_width = "64"))] {
                 $( $if_64_or_32 )*
+            } else {
+                $( $( $otherwise )* )?
             }
         }
     };
