83b5871f63a8a5aa50ff4cc006245d9b344d56de
Brian Smith <brian@briansmith.org>
2025-06-08 19:41:38 -0700

    aarch64/x86_64: Use native word size for ILP32 ABIs.

--- src/match_target_word_bits.rs
+++ src/match_target_word_bits.rs
@@ -5,7 +5,10 @@ macro_rules! match_target_word_bits {
         $( _ => { $( $otherwise:tt )* } )?
     } => {
         cfg_if::cfg_if! {
-            if #[cfg(target_pointer_width = "64")] {
+            // Use 64-bit words on AArch64 ILP32 and x86-64 x32.
+            if #[cfg(any(target_arch = "aarch64",
+                         target_arch = "x86_64",
+                         target_pointer_width = "64"))] {
                 $( $if_64 )*
             } else if #[cfg(target_pointer_width = "32")] {
                 $( $if_32 )*
