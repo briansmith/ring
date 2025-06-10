3f102b5bb0858a947ed422f5da3719ab348b2383
Brian Smith <brian@briansmith.org>
2025-06-10 11:49:48 -0700

    poly1305 fallback: Transliterate `CRYPTO_poly1305_init` to Rust.

--- src/aead/poly1305/ffi_fallback.rs
+++ src/aead/poly1305/ffi_fallback.rs
@@ -13,9 +13,13 @@
 // OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
 // CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-use super::{Key, Tag, BLOCK_LEN, KEY_LEN, TAG_LEN};
-use crate::c;
-use core::num::NonZeroUsize;
+use super::{Key, Tag, BLOCK_LEN, TAG_LEN};
+use crate::{c, polyfill};
+use core::num::{NonZeroUsize, Wrapping};
+
+type W32 = Wrapping<u32>;
+
+const _5: W32 = Wrapping(5);
 
 // XXX/TODO(MSRV): change to `pub(super)`.
 pub(in super::super) struct State {
@@ -25,49 +29,68 @@ pub(in super::super) struct State {
 // Keep in sync with `poly1305_state_st` in poly1305.c
 #[repr(C, align(64))]
 struct poly1305_state_st {
-    r0: u32,
-    r1: u32,
-    r2: u32,
-    r3: u32,
-    r4: u32,
-    s1: u32,
-    s2: u32,
-    s3: u32,
-    s4: u32,
-    h0: u32,
-    h1: u32,
-    h2: u32,
-    h3: u32,
-    h4: u32,
+    r0: W32,
+    r1: W32,
+    r2: W32,
+    r3: W32,
+    r4: W32,
+    s1: W32,
+    s2: W32,
+    s3: W32,
+    s4: W32,
+    h0: W32,
+    h1: W32,
+    h2: W32,
+    h3: W32,
+    h4: W32,
     key: [u8; BLOCK_LEN],
 }
 
 impl State {
     pub(super) fn new_context(Key { key }: Key) -> super::Context {
-        prefixed_extern! {
-            fn CRYPTO_poly1305_init(state: &mut poly1305_state_st, key: &[u8; KEY_LEN]);
-        }
-        let mut r = Self {
+        let (t, key) = key.split_at(BLOCK_LEN);
+        let t: &[u8; BLOCK_LEN] = t.try_into().unwrap();
+        let key: &[u8; BLOCK_LEN] = key.try_into().unwrap();
+
+        let (t, _) = polyfill::slice::as_chunks(&t[..]);
+
+        let mut t1 = Wrapping(u32::from_le_bytes(t[1]));
+        let mut t2 = Wrapping(u32::from_le_bytes(t[2]));
+        let mut t3 = Wrapping(u32::from_le_bytes(t[3]));
+        let mut t0 = Wrapping(u32::from_le_bytes(t[0]));
+
+        let r0 = t0 & Wrapping(0x3ffffff);
+        t0 >>= 26;
+        t0 |= t1 << 6;
+        let r1 = t0 & Wrapping(0x3ffff03);
+        t1 >>= 20;
+        t1 |= t2 << 12;
+        let r2 = t1 & Wrapping(0x3ffc0ff);
+        t2 >>= 14;
+        t2 |= t3 << 18;
+        let r3 = t2 & Wrapping(0x3f03fff);
+        t3 >>= 8;
+        let r4 = t3 & Wrapping(0x00fffff);
+
+        super::Context::Fallback(Self {
             state: poly1305_state_st {
-                r0: 0,
-                r1: 0,
-                r2: 0,
-                r3: 0,
-                r4: 0,
-                s1: 0,
-                s2: 0,
-                s3: 0,
-                s4: 0,
-                h0: 0,
-                h1: 0,
-                h2: 0,
-                h3: 0,
-                h4: 0,
-                key: [0u8; BLOCK_LEN],
+                r0,
+                r1,
+                r2,
+                r3,
+                r4,
+                s1: r1 * _5,
+                s2: r2 * _5,
+                s3: r3 * _5,
+                s4: r4 * _5,
+                h1: Wrapping(0),
+                h2: Wrapping(0),
+                h3: Wrapping(0),
+                h4: Wrapping(0),
+                h0: Wrapping(0),
+                key: *key,
             },
-        };
-        unsafe { CRYPTO_poly1305_init(&mut r.state, &key) }
-        super::Context::Fallback(r)
+        })
     }
 
     // `input.len % BLOCK_LEN == 0` must be true for every call except the
