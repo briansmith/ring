ea387fcf071ec32f92825738ca212aaad5f34157
Brian Smith <brian@briansmith.org>
2025-07-30 11:35:35 -0700

    polyfill: Clarify casts.

--- src/polyfill/atomic.rs
+++ src/polyfill/atomic.rs
@@ -10,6 +10,7 @@ pub trait AtomicPolyfills {
 impl AtomicPolyfills for AtomicU32 {
     type NonAtomic = u32;
 
+    // TODO(MSRV-1.70): drop this in favor of `AtomicU32::as_ptr()`.
     #[inline(always)]
     fn as_ptr(&self) -> *mut Self::NonAtomic {
         // SAFETY: "This type has the same size and bit validity as
@@ -17,6 +18,8 @@ impl AtomicPolyfills for AtomicU32 {
         // this type is always equal to its size, even on targets where
         // usize has a lesser alignment."
         const _ALIGNMENT_COMPATIBLE: () = assert!(align_of::<AtomicU32>() % align_of::<u32>() == 0);
-        super::ptr::from_ref(self).cast_mut().cast::<u32>()
+        super::ptr::from_ref(self)
+            .cast::<Self::NonAtomic>()
+            .cast_mut()
     }
 }
