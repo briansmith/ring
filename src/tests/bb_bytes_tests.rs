a862e22040da4a92f49876179a1c37d2c42cbef9
Brian Smith <brian@briansmith.org>
2026-02-27 14:22:22 -0800

    Testing: Always test both valid `BoolMask` values.
    There is no need to randomize the value of `b` in these tests.

--- src/tests/bb_bytes_tests.rs
+++ src/tests/bb_bytes_tests.rs
@@ -18,7 +18,6 @@ use crate::{
     bb::{xor_assign_at_start_bytes, BoolMask},
     rand,
 };
-use core::hint::black_box;
 
 #[test]
 fn constant_time_conditional_memcpy() {
@@ -63,23 +62,19 @@ fn test_constant_time_conditional_mem_x<const LEN: usize>(
     ref_f: impl Fn(bool, &mut [u8; LEN], &[u8; LEN]),
 ) {
     let rng = rand::SystemRandom::new();
-    for _ in 0..256 {
-        let mut out = rand::generate::<[u8; LEN]>(&rng).unwrap().expose();
-        let input = rand::generate::<[u8; LEN]>(&rng).unwrap().expose();
+    for b in [BoolMask::false_(), BoolMask::true_()] {
+        for _ in 0..256 {
+            let mut out = rand::generate::<[u8; LEN]>(&rng).unwrap().expose();
+            let input = rand::generate::<[u8; LEN]>(&rng).unwrap().expose();
 
-        let mut ref_out: [u8; LEN] = out;
-        let ref_in: [u8; LEN] = input;
+            let mut ref_out: [u8; LEN] = out;
+            let ref_in: [u8; LEN] = input;
 
-        // Mask to 16 bits to make zero more likely than it would otherwise be.
-        let b = if (rand::generate::<[u8; 1]>(&rng).unwrap().expose()[0] & 0x0f) == 0 {
-            BoolMask::true_()
-        } else {
-            BoolMask::false_()
-        };
+            f(b, &mut out, &input);
+            ref_f(b.leak(), &mut ref_out, &ref_in);
 
-        f(b, &mut out, &input);
-        ref_f(black_box(b).leak(), &mut ref_out, &ref_in);
-        assert_eq!(ref_in, input);
-        assert_eq!(&ref_out, &out);
+            assert_eq!(ref_in, input);
+            assert_eq!(&ref_out, &out);
+        }
     }
 }
