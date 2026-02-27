76482c584daed2fe80c1012779f2d83bdb9e6eaf
Brian Smith <brian@briansmith.org>
2026-02-27 14:15:13 -0800

    Testing: Factor out common logic of constant_time_conditional_mem* tests.

--- src/tests/bb_bytes_tests.rs
+++ src/tests/bb_bytes_tests.rs
@@ -16,77 +16,70 @@
 
 use crate::{
     bb::{xor_assign_at_start_bytes, BoolMask},
-    error, rand,
+    rand,
 };
+use core::hint::black_box;
 
 #[test]
-fn constant_time_conditional_memcpy() -> Result<(), error::Unspecified> {
-    let rng = rand::SystemRandom::new();
-    for _ in 0..100 {
-        let mut out = rand::generate::<[u8; 256]>(&rng)?.expose();
-        let input = rand::generate::<[u8; 256]>(&rng)?.expose();
-
-        // Mask to 16 bits to make zero more likely than it would otherwise be.
-        let b = (rand::generate::<[u8; 1]>(&rng)?.expose()[0] & 0x0f) == 0;
-
-        let ref_in = input;
-        let ref_out = if b { input } else { out };
-
-        prefixed_extern! {
-            fn bssl_constant_time_test_conditional_memcpy(dst: &mut [u8; 256], src: &[u8; 256], b: BoolMask);
-        }
-        unsafe {
-            bssl_constant_time_test_conditional_memcpy(
-                &mut out,
-                &input,
-                if b {
-                    BoolMask::true_()
-                } else {
-                    BoolMask::false_()
-                },
-            )
-        }
-        assert_eq!(ref_in, input);
-        assert_eq!(ref_out, out);
-    }
-
-    Ok(())
+fn constant_time_conditional_memcpy() {
+    test_constant_time_conditional_mem_x(
+        |b, out, input| {
+            prefixed_extern! {
+                fn bssl_constant_time_test_conditional_memcpy(dst: &mut [u8; 256], src: &[u8; 256], b: BoolMask);
+            }
+            unsafe { bssl_constant_time_test_conditional_memcpy(out, input, b) }
+        },
+        |b, out, input| {
+            if b {
+                *out = *input;
+            }
+        },
+    )
 }
 
 #[test]
-fn constant_time_conditional_memxor() -> Result<(), error::Unspecified> {
+fn constant_time_conditional_memxor() {
+    test_constant_time_conditional_mem_x(
+        |b, out, input| {
+            prefixed_extern! {
+                fn bssl_constant_time_test_conditional_memxor(dst: &mut [u8; 256], src: &[u8; 256], b: BoolMask);
+            }
+            unsafe {
+                bssl_constant_time_test_conditional_memxor(out, input, b);
+            }
+        },
+        |b, out, input| {
+            if b {
+                xor_assign_at_start_bytes(out, input)
+            }
+        },
+    )
+}
+
+// `f`: The implementation being tested.
+// `ref_f`: The reference implementation.
+fn test_constant_time_conditional_mem_x<const LEN: usize>(
+    f: impl Fn(BoolMask, &mut [u8; LEN], &[u8; LEN]),
+    ref_f: impl Fn(bool, &mut [u8; LEN], &[u8; LEN]),
+) {
     let rng = rand::SystemRandom::new();
     for _ in 0..256 {
-        let mut out = rand::generate::<[u8; 256]>(&rng)?.expose();
-        let input = rand::generate::<[u8; 256]>(&rng)?.expose();
+        let mut out = rand::generate::<[u8; LEN]>(&rng).unwrap().expose();
+        let input = rand::generate::<[u8; LEN]>(&rng).unwrap().expose();
+
+        let mut ref_out: [u8; LEN] = out;
+        let ref_in: [u8; LEN] = input;
 
         // Mask to 16 bits to make zero more likely than it would otherwise be.
-        let b = (rand::generate::<[u8; 1]>(&rng)?.expose()[0] & 0x0f) != 0;
-
-        let ref_in = input;
-        let mut ref_out = out;
-        if b {
-            xor_assign_at_start_bytes(&mut ref_out, &ref_in)
+        let b = if (rand::generate::<[u8; 1]>(&rng).unwrap().expose()[0] & 0x0f) == 0 {
+            BoolMask::true_()
+        } else {
+            BoolMask::false_()
         };
 
-        prefixed_extern! {
-            fn bssl_constant_time_test_conditional_memxor(dst: &mut [u8; 256], src: &[u8; 256], b: BoolMask);
-        }
-        unsafe {
-            bssl_constant_time_test_conditional_memxor(
-                &mut out,
-                &input,
-                if b {
-                    BoolMask::true_()
-                } else {
-                    BoolMask::false_()
-                },
-            );
-        }
-
+        f(b, &mut out, &input);
+        ref_f(black_box(b).leak(), &mut ref_out, &ref_in);
         assert_eq!(ref_in, input);
-        assert_eq!(ref_out, out);
+        assert_eq!(&ref_out, &out);
     }
-
-    Ok(())
 }
