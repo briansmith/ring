e212ca339690cd06bb31ca8c22f73e2628751b67
Brian Smith <brian@briansmith.org>
2026-03-08 22:28:00 -0700

    Remove unused `static mut` case of `prefixed_extern!`.

--- src/prefixed.rs
+++ src/prefixed.rs
@@ -62,23 +62,6 @@ macro_rules! prefixed_extern {
             }
         }
     };
-
-    // A `static mut` global variable.
-    {
-        $( #[$meta:meta] )*
-        $vis:vis static mut $name:ident: $typ:ty;
-    } => {
-        extern "C" {
-            prefixed_item! {
-                link_name
-                $name
-                {
-                    $( #[$meta] )*
-                    $vis static mut $name: $typ;
-                }
-            }
-        }
-    };
 }
 
 #[deprecated = "`#[export_name]` creates problems and we will stop doing it."]
